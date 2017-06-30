54f292fdca4384421ba4917789467e31162a9516
Steven Estes <estess@yottadb.com>
2017-06-30 12:35:44 -0400

    1. Update SUITE to add r100 2. Update copyrights to YottaDB 3. Improve test to run in both M mode and UTF8 mode.

--- r100/inref/zlen2arg.m
+++ r100/inref/zlen2arg.m
@@ -1,7 +1,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;								;
-; Copyright (c) 2017 Finxact, LLC. and/or its subsidiaries.     ;
-; All rights reserved.                                          ;
+; Copyright (c) 2017 YottaDB LLC. and/or its subsidiaries.	;
+; All rights reserved.	     	  	     			;
 ;								;
 ;	This source code contains the intellectual property	;
 ;	of its copyright holder(s), and is made available	;
@@ -20,6 +20,7 @@
 ;   are no cache problems.
 ;
 zlen2arg()
+	write "Test two argument $[Z]LENGTH() interaction with $[Z]PIECE() cache",!
 	set strbase=""
 	set delim="/"
 	set errcnt=0
@@ -53,6 +54,26 @@ zlen2arg()
 	;
 	if (0=errcnt) write !,"PASS - no errors detected",!!
 	else  write !,"FAIL -- total of ",errcnt," error(s) detected",!!
+	;
+	; This next test is primarily a UTF8 test (and will run differently in UTF8 and M modes) but we'll run in both anyway to
+	; be consistent. Start with a string that exceeds the number of pieces that can be kept in cache - the strbase string
+	; created in the previous step is handy but we need to do 2 things:
+	;   1. Turn off BADCHAR so we can have illegal UTF8 chars.
+	;   2. Translate the current '/' delimiter to $zchar(132).
+	; This allows us to append $char(196) to the end of the string. Note this is a 2 byte UTF8 character $zchar(195,132). Note the
+	; 2nd byte is the same as the delimiter but should not be recognized as such by $LENGTH() since it is part of a valid
+	; character.
+	;
+	write "Testing trailing UTF8 char whose last char is same as delimiter",!!
+	view "NOBADCHAR"
+	set newdelim=$zchar(132)
+	set str1=$translate(strbase,delim,newdelim)
+	set len1=$length(str1,newdelim)
+	set str2=str1_$zchar(196)
+	set len2=$length(str2,newdelim)
+	write:(len1=len2) "PASS - trailing UTF8 char was not taken as a delimiter",!
+	write:(len1'=len2) "FAIL - lengths of strings not as expected - length(str1): ",len1,"  length(str2): ",len2,!
+	write !
 	quit
 
 ;
--- r100/u_inref/zlen2arg.csh
+++ r100/u_inref/zlen2arg.csh
@@ -1,7 +1,7 @@
 #!/usr/local/bin/tcsh -f
 #################################################################
 #								#
-# Copyright (c) 2017 Finxact, LLC. and/or its subsidiaries.     #
+# Copyright (c) 2017 YottaDB LLC. and/or its subsidiaries.	#
 # All rights reserved.                                          #
 #								#
 #	This source code contains the intellectual property	#
@@ -12,6 +12,11 @@
 #################################################################
 #
 # Test for two argument $[Z]LENGTH() and its new support to use/create/update the piece cache also
-# used/created/updated by $[Z]PIECE().
+# used/created/updated by $[Z]PIECE(). Run in both UTF8 and M modes
 #
+$switch_chset "M" >&! switch_m.log
 $gtm_dist/mumps -run zlen2arg
+#
+# Now run in UTF8 mode
+#
+$switch_chset "UTF-8" >&! switch_utf8.log
