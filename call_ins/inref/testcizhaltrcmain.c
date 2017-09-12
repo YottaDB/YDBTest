e3b36e395940eb23f3764116c235649954feb4af
Steven Estes <estess@users.noreply.github.com>
2017-09-12 19:50:12 -0400

    [#59] Test changes for call-ins changes plus Simple API changes
    General change for all new tests - change gtm_* types and routine names to ydb_*.
    
    call_ins/inref/drivecirtn.c
      - gtm_* -> ydb_*
    
    call_ins/inref/testcizhaltrc.m
      - Houses call-in entryrefs for new test_ci_z_halt_rc subtest
    
    call_ins/inref/testcizhaltrcmain.c
      - Main C driver routine for new test_ci_z_halt_rc subtest
    
    call_ins/instream.csh
      - Add test_ci_z_halt_rc subtest
    
    call_ins/outref/outref.txt
      - Add new test_ci_z_halt_rc subtest completion message to reference file
    
    call_ins/outref/test_ci_z_halt_rc.txt
      - Reference file for new test_ci_z_halt_rc subtes
    
    call_ins/u_inref/32args.csh
      - Remove blank line
    
    call_ins/u_inref/ctomctom.csh
      - Remove blank lines
    
    call_ins/u_inref/test_ci_z_halt_rc.csh
      - Driver script for new test_ci_z_halt_rc subtest

---
+++ call_ins/inref/testcizhaltrcmain.c
@@ -0,0 +1,206 @@
+/****************************************************************
+ *								*
+ * Copyright (c) 2017 YottaDB LLC. and/or its subsidiaries.	*
+ * All rights reserved.						*
+ *								*
+ *	This source code contains the intellectual property	*
+ *	of its copyright holder(s), and is made available	*
+ *	under a license.  If you do not know the terms of	*
+ *	the license, please stop and do not read further.	*
+ *								*
+ ****************************************************************/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "ydbxc_types.h"
+
+#define ERRBUF_SIZE	1024
+
+enum {
+	useHALT = 1,
+	useZHALT,
+	useZGOTO0,
+	useQUITretval
+};
+
+/*
+ * See test_ci_z_halt_rc.csh for test description. Series of call-ins that terminate in various ways. We are testing to make sure
+ * they termination in an acceptable fashion (i.e. lack of explosions and correct errors generated). Note the return value is
+ * pre-set before each call so we can see under which conditions the return value is being left unset.
+ */
+int main(void)
+{
+	ydb_status_t		status;
+	ydb_int_t		retintval;
+	ydb_string_t		retstrval;
+	char			errbuf[ERRBUF_SIZE];
+	char			retbuf[32];
+
+	status = ydb_init();		/* Initialize call-in environment */
+	if (status)
+	{
+		ydb_zstatus(errbuf, ERRBUF_SIZE);
+		printf("%s\n", errbuf);
+		fflush(stdout);
+		return 0;
+	}
+
+	/* First is a series of integer return value tests */
+
+	printf("main: Initializing return value to -99 for HALT test\n");
+	retintval = -99;
+	status = ydb_ci("testcizhaltrcint", &retintval, useHALT);
+	if (status)
+	{
+		ydb_zstatus(errbuf, ERRBUF_SIZE);
+		printf("Return code: %d\n", status);
+		printf("%s\n", errbuf);
+		fflush(stdout);
+	}
+	printf("main: After HALT, return value is %d\n\n", retintval);
+
+	printf("main: Initializing return value to -99 for ZHALT test\n");
+	retintval = -99;
+	status = ydb_ci("testcizhaltrcint", &retintval, useZHALT);
+	if (status)
+	{
+		printf("Return code: %d\n", status);
+		ydb_zstatus(errbuf, ERRBUF_SIZE);
+		printf("%s\n", errbuf);
+		fflush(stdout);
+	}
+	printf("main: After ZHALT, return value is %d\n\n", retintval);
+
+	printf("main: Initializing return value to -99 for ZGOTO 0 test\n");
+	retintval = -99;
+	status = ydb_ci("testcizhaltrcint", &retintval, useZGOTO0);
+	if (status)
+	{
+		printf("Return code: %d\n", status);
+		ydb_zstatus(errbuf, ERRBUF_SIZE);
+		printf("%s\n", errbuf);
+		fflush(stdout);
+	}
+	printf("main: After ZGOTO 0, return value is %d\n\n", retintval);
+
+	printf("main: Initializing return value to -99 for QUIT with no return value (but one expected) test\n");
+	retintval = -99;
+	status = ydb_ci("testcizhaltrcint", &retintval, useQUITretval);
+	if (status)
+	{
+		printf("Return code: %d\n", status);
+		ydb_zstatus(errbuf, ERRBUF_SIZE);
+		printf("%s\n", errbuf);
+		fflush(stdout);
+	}
+	printf("main: After QUIT with no return value (but one expected), return value is %d\n\n", retintval);
+
+	printf("main: Setting up for QUIT with (an unexpected integer) return value test\n");
+	status = ydb_ci("testcizhaltnoretv",1);
+	if (status)
+	{
+		printf("Return code: %d\n", status);
+		ydb_zstatus(errbuf, ERRBUF_SIZE);
+		printf("%s\n", errbuf);
+		fflush(stdout);
+	}
+	printf("main: Return from QUIT with (an unexpected integer) return value\n\n");
+
+	/* Now the same treatment for a string return */
+
+	printf("main: Initializing return value to 'snarf' for the HALT test\n");
+	retstrval.address = retbuf;
+	strcpy(retbuf, "snarf");
+	retstrval.length = sizeof("snarf") - 1;
+	status = ydb_ci("testcizhaltrcstr", &retstrval, useHALT);
+	if (status)
+	{
+		ydb_zstatus(errbuf, ERRBUF_SIZE);
+		printf("Return code: %d\n", status);
+		printf("%s\n", errbuf);
+		fflush(stdout);
+	}
+	printf("main: After HALT, return value is %.*s\n\n", (int)retstrval.length, retstrval.address);
+
+	printf("main: Initializing return value to 'snarf' for the ZHALT test\n");
+	retstrval.address = retbuf;
+	strcpy(retbuf, "snarf");
+	retstrval.length = sizeof("snarf") - 1;
+	status = ydb_ci("testcizhaltrcstr", &retstrval, useZHALT);
+	if (status)
+	{
+		ydb_zstatus(errbuf, ERRBUF_SIZE);
+		printf("Return code: %d\n", status);
+		printf("%s\n", errbuf);
+		fflush(stdout);
+	}
+	printf("main: After ZHALT, return value is %.*s\n\n", (int)retstrval.length, retstrval.address);
+
+	printf("main: Initializing return value to 'snarf' for the ZGOTO 0 test\n");
+	retstrval.address = retbuf;
+	strcpy(retbuf, "snarf");
+	retstrval.length = sizeof("snarf") - 1;
+	status = ydb_ci("testcizhaltrcstr", &retstrval, useZGOTO0);
+	if (status)
+	{
+		ydb_zstatus(errbuf, ERRBUF_SIZE);
+		printf("Return code: %d\n", status);
+		printf("%s\n", errbuf);
+		fflush(stdout);
+	}
+	printf("main: After ZGOTO 0, return value is %.*s\n\n", (int)retstrval.length, retstrval.address);
+
+	printf("main: Initializing return value to 'snarf' for the QUIT with no return value (but one expected) test\n");
+	retstrval.address = retbuf;
+	strcpy(retbuf, "snarf");
+	retstrval.length = sizeof("snarf") - 1;
+	status = ydb_ci("testcizhaltrcstr", &retstrval, useQUITretval);
+	if (status)
+	{
+		ydb_zstatus(errbuf, ERRBUF_SIZE);
+		printf("Return code: %d\n", status);
+		printf("%s\n", errbuf);
+		fflush(stdout);
+	}
+	printf("main: After QUIT with (an unexpected) return value, return value is %.*s\n\n",
+	       (int)retstrval.length, retstrval.address);
+
+	printf("main: Setting up for QUIT with (an unexpected string) return value test\n");
+	status = ydb_ci("testcizhaltnoretv",0);
+	if (status)
+	{
+		ydb_zstatus(errbuf, ERRBUF_SIZE);
+		printf("Return code: %d\n", status);
+		printf("%s\n", errbuf);
+		fflush(stdout);
+	}
+	printf("main: Return from QUIT with (an unexpected string) return value\n\n");
+
+	/* Now make a call with arguments to a routine that isn't expecting any - expect error */
+
+	printf("main: Initializing return value to -99 for the call with args to routine with no parms test\n");
+	retintval = -99;
+	status = ydb_ci("testcizhaltnoargs", &retintval, 42);
+	if (status)
+	{
+		ydb_zstatus(errbuf, ERRBUF_SIZE);
+		printf("Return code: %d\n", status);
+		printf("%s\n", errbuf);
+		fflush(stdout);
+	}
+	printf("main: Returned from testcizhaltnoargs\n\n");
+
+	/* Test complete - Close up shop */
+	status = ydb_exit();
+	if (status)
+	{
+		ydb_zstatus(errbuf, ERRBUF_SIZE);
+		printf("Return code: %d\n", status);
+		printf("%s\n", errbuf);
+		fflush(stdout);
+	}
+
+	return 0;
+}
---
+++ call_ins/u_inref/test_ci_z_halt_rc.csh
@@ -0,0 +1,47 @@
+#################################################################
+#								#
+# Copyright (c) 2017 YottaDB LLC. and/or its subsidiaries.	#
+# All rights reserved.                                          #
+#								#
+#	This source code contains the intellectual property	#
+#	of its copyright holder(s), and is made available	#
+#	under a license.  If you do not know the terms of	#
+#	the license, please stop and do not read further.	#
+#								#
+#################################################################
+#
+# Test conditions:
+#   1. C main routine does call-in to routine that expects a return value (test both int and string returns)
+#   2. Test various types of exits the routine does:
+#        a. HALT but caller expects a return value.
+#        b. ZHALT with return value which should be returned to caller.
+#        c. ZGOTO 0 but caller expects a return value.
+#        d. QUIT no return value when one is expected (expect QUITARGREQD error).
+#        e. QUIT with a return value when one is NOT expected (expect NOTEXTRINSIC error).
+#   3. Test call-in to routine supplying args when none are expected (expect FMLLSTMISSING error).
+#
+
+#
+# Define the set of call-in routines we are testing
+#
+setenv GTMCI testcizhaltrc.xc
+cat >> $GTMCI << EOF
+testcizhaltrcint:  ydb_int_t    *testcizhaltrcint^testcizhaltrc(I:ydb_int_t)
+testcizhaltrcstr:  ydb_string_t *testcizhaltrcstr^testcizhaltrc(I:ydb_int_t)
+testcizhaltnoargs: ydb_int_t    *testcizhaltnoargs^testcizhaltrc(I:ydb_int_t)
+testcizhaltnoretv: void          testcizhaltnoretval^testcizhaltrc(I:ydb_int_t)
+EOF
+#
+# Compile/build C main routine
+#
+$gt_cc_compiler $gtt_cc_shl_options $gtm_tst/$tst/inref/testcizhaltrcmain.c -I$gtm_dist
+$gt_ld_linker $gt_ld_option_output testcizhaltrcmain $gt_ld_options_common testcizhaltrcmain.o $gt_ld_sysrtns $ci_ldpath$gtm_dist -L$gtm_dist $tst_ld_gtmshr $gt_ld_syslibs >& link.map
+if( $status != 0 ) then
+    cat link.map
+endif
+#
+# Drive main test
+#
+testcizhaltrcmain
+#
+echo "End of test_ci_z_halt_rc subtest"
