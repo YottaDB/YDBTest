954804e670866f186f3c2dcc25780a8232ebffa5
Narayanan Iyer <nars@yottadb.com>
2017-09-29 14:07:16 -0400

    Revert "Fix basic test to pass in case it is run with a developmental version i.e. V9xx_Ryyy"
    This reverts commit 3d6716c2808fc72c89965ad7cbbf952aa618278c.

--- basic/inref/testver.m
+++ basic/inref/testver.m
@@ -17,10 +17,6 @@ testver	;
 	set zyre=$translate($piece($zyrelease," ",2),".-")
 	set testver=$zconvert(zv_"_"_zyre,"U")
 	set gtmverno=$ztrnlnm("gtm_verno")
-	; If running test version is V9xx_Ryyy, then skip comparing V9xx against $ZVERSION (which would be V6*)
-	set testzv=$piece(gtmverno,"_",1)
-	set testzyre=$piece(gtmverno,"_",2,99)
-	set:(testzv["V9") gtmverno=$zconvert(zv_"_"_testzyre,"U")
 	if testver=gtmverno write "Passed the version test",!
 	else                write "Should be ",gtmverno," but is ",testver,!
 	quit
