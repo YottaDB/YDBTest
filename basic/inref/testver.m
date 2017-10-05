43eb6c271b04ac18ea38008719032358d588b110
Narayanan Iyer <nars@yottadb.com>
2017-10-05 12:44:54 -0400

    Fix basic test to pass in case it is run with a developmental version i.e. V9xx

--- basic/inref/testver.m
+++ basic/inref/testver.m
@@ -13,10 +13,12 @@ testver	;
 	;
 	; Construct version from $ZVERSION & $ZYRELEASE
 	;
-	set zv=$translate($piece($zversion," ",2),".-")
-	set zyre=$translate($piece($zyrelease," ",2),".-")
-	set testver=$zconvert(zv_"_"_zyre,"U")
 	set gtmverno=$ztrnlnm("gtm_verno")
+	if gtmverno'["V9" do
+	.	set zv=$translate($piece($zversion," ",2),".-")
+	.	set zyre=$translate($piece($zyrelease," ",2),".-")
+	.	set testver=$zconvert(zv_"_"_zyre,"U")
+	else  set testver=gtmverno
 	if testver=gtmverno write "Passed the version test",!
 	else                write "Should be ",gtmverno," but is ",testver,!
 	quit
