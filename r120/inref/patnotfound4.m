8cfdad646d51b0ab2debef18b5183e7c50e01551
Narayanan Iyer <nars@yottadb.com>
2017-11-14 16:56:23 -0500

    [NARS1] [estess] Add r120/patnotfound subtest to test PATNOTFOUND error handling (see YottaDB/YottaDB#90)
    See YottaDB/YottaDB#90 for a description of the issue and corresponding test cases. Those test cases are folded into this automated test.
    There are 4 M programs. And each of them are included because they show different failure symptoms with R110 pro and dbg. Below is the output of those test programs with the older code. All of them run correctly with the code fix.
    
    R110 pro build output
    ---------------------
    ```
     --> Running test with patnotfound1.m <---
    %GTM-E-PATNOTFOUND, Current pattern table has no characters with pattern code B
    %GTM-E-PATNOTFOUND, Current pattern table has no characters with pattern code
    
     --> Running test with patnotfound2.m <---
    %GTM-E-PATNOTFOUND, Current pattern table has no characters with pattern code B
    %GTM-E-ZLINKFILE, Error while zlinking "/usr/library/gtm_test/T998/r120/inref/patnotfound2.m"
    %GTM-E-ZLNOOBJECT, No object module was produced
    
     --> Running test with patnotfound3.m <---
    %GTM-E-PATNOTFOUND, Current pattern table has no characters with pattern code B
    %GTM-F-KILLBYSIGSINFO1, GT.M process 14353 has been killed by a signal 11 at address 0x00007F90F6C0E8A3 (vaddr 0x0000000000000000)
    %GTM-F-SIGMAPERR, Signal was caused by an address not mapped to an object
    
     --> Running test with patnotfound4.m <---
    %GTM-E-PATNOTFOUND, Current pattern table has no characters with pattern code B
    %GTM-F-KILLBYSIGSINFO1, GT.M process 14355 has been killed by a signal 11 at address 0x00007F7CDB8D700F (vaddr 0x00007F7CDB8D700F)
    %GTM-F-SIGMAPERR, Signal was caused by an address not mapped to an object
    ```
    
    R110 dbg build output
    ---------------------
    ```
     --> Running test with patnotfound1.m <---
    %GTM-E-PATNOTFOUND, Current pattern table has no characters with pattern code B
    %GTM-F-GTMASSERT, GT.M V6.3-002 Linux x86_64 - Assert failed /Distrib/GT.M/V63002/sr_port/chktchain.c line 28
    
     --> Running test with patnotfound2.m <---
    %GTM-E-PATNOTFOUND, Current pattern table has no characters with pattern code B
    %GTM-F-ASSERT, Assert failed in /Distrib/GT.M/V63002/sr_port/zlcompile.c line 81 for expression ((FALSE == run_time) && (TRUE == TREF(compile_time)))
    
     --> Running test with patnotfound3.m <---
    %GTM-E-PATNOTFOUND, Current pattern table has no characters with pattern code B
    %GTM-F-ASSERT, Assert failed in /Distrib/GT.M/V63002/sr_port/code_gen.c line 51 for expression (0 == pending_errtriplecode)
    
     --> Running test with patnotfound4.m <---
    %GTM-E-PATNOTFOUND, Current pattern table has no characters with pattern code B
    %GTM-F-GTMASSERT, GT.M V6.3-002 Linux x86_64 - Assert failed /Distrib/GT.M/V63002/sr_port/chktchain.c line 28
    ```

---
+++ r120/inref/patnotfound4.m
@@ -0,0 +1,20 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;								;
+; Copyright (c) 2017 YottaDB LLC. and/or its subsidiaries.	;
+; All rights reserved.	     	  	     			;
+;								;
+;	This source code contains the intellectual property	;
+;	of its copyright holder(s), and is made available	;
+;	under a license.  If you do not know the terms of	;
+;	the license, please stop and do not read further.	;
+;								;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+main	;
+	do good
+	quit
+bad	;
+	if 1?1B
+	quit
+good	;
+	write "hello",!
+	quit
