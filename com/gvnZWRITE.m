7e8f50d90aa5e461875a002be7ddcd2521c983de
Narayanan Iyer <nars@yottadb.com>
2017-12-12 13:34:38 -0500

    [#59] Enhance simpleapi/stresstest subtest to do lots of gvn sets

---
+++ com/gvnZWRITE.m
@@ -0,0 +1,23 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;								;
+; Copyright (c) 2017 YottaDB LLC. and/or its subsidiaries.	;
+; All rights reserved.						;
+;								;
+;	This source code contains the intellectual property	;
+;	of its copyright holder(s), and is made available	;
+;	under a license.  If you do not know the terms of	;
+;	the license, please stop and do not read further.	;
+;								;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;
+; Routine to list all nodes in the database files accessible through the gld pointed to by $gtmgbldir
+;
+gvnZWRITE ;
+	set x="^%" for  set x=$order(@x,1) quit:x=""  set y=x  do
+	. if $data(@y) do print(y)
+	. for  set y=$query(@y) quit:y=""  do print(y)
+	quit
+
+print(node);
+	write node,"=",$zwrite($get(@node)),!
+	quit
