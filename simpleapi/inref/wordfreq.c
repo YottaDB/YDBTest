83f87c528bcb01d7940db0aaed4871fdf782ab81
Narayanan Iyer <nars@yottadb.com>
2017-12-27 16:27:22 -0500

    [#59] New simpleapi/wordfreq subtest to test simpleAPI use to count word frequencies

---
+++ simpleapi/inref/wordfreq.c
@@ -0,0 +1,121 @@
+/****************************************************************
+ *								*
+ * Copyright (c) 2017 YottaDB LLC. and/or its subsidiaries.	*
+ * All rights reserved.						*
+ *								*
+ *	This source code contains the intellectual property	*
+ *	of its copyright holder(s), and is made available	*
+ *	under a license.  If you do not know the terms of	*
+ *	the license, please stop and do not read further.	*
+ *								*
+ ****************************************************************/
+
+#include "libyottadb.h"	/* for ydb_* macros/prototypes/typedefs */
+
+#include <stdio.h>	/* for "printf" */
+#include <string.h>	/* for "strtok" */
+#include <ctype.h>	/* for "toupper" */
+
+#include <sys/types.h>	/* needed for "kill" in assert */
+#include <signal.h>	/* needed for "kill" in assert */
+#include <unistd.h>	/* needed for "getpid" in assert */
+
+/* Use SIGILL below to generate a core when an assertion fails */
+#define assert(x) ((x) ? 1 : (fprintf(stderr, "Assert failed at %s line %d : %s\n", __FILE__, __LINE__, #x), kill(getpid(), SIGILL)))
+
+/* count and report word frequencies for http://www.cs.duke.edu/csed/code/code2007/ */
+int main()
+{
+	ydb_buffer_t	tmp1, tmp2, index, words, words_tmp1, null;
+	ydb_buffer_t	index_subscr[2], value;
+	char		tmp1buff[64], tmp2buff[64], valuebuff[64], words_tmp1buff[64], linebuff[1024], *lineptr, *ptr, *tmp_ptr;
+	int		status;
+
+	/* Initialize all array variable names we are planning to use. Randomly use locals vs globals to store word frequencies. */
+	if (getpid() % 2)
+	{
+		YDB_STRLIT_TO_BUFFER(&words, "words");
+		YDB_STRLIT_TO_BUFFER(&index, "index");
+	} else
+	{
+		YDB_STRLIT_TO_BUFFER(&words, "^words");
+		YDB_STRLIT_TO_BUFFER(&index, "^index");
+	}
+	value.buf_addr = &valuebuff[0];
+	value.len_used = 0;
+	value.len_alloc = sizeof(valuebuff);
+	do
+	{
+		lineptr = fgets(linebuff, sizeof(linebuff), stdin);
+		if (NULL == lineptr)
+			break;
+		ptr = strtok(lineptr, " ");
+		while (NULL != ptr)
+		{	/* convert word pointed to by "ptr" to lowercase */
+			tmp_ptr = ptr;
+			while (*tmp_ptr)
+			{
+				*tmp_ptr = tolower(*tmp_ptr);
+				tmp_ptr++;
+			}
+			/* */
+			tmp1.buf_addr = ptr;
+			tmp1.len_used = tmp_ptr - ptr;
+			if (tmp_ptr[-1] == '\n')
+				tmp1.len_used--;	/* - 1 to remove trailing newline */
+			if (tmp1.len_used)
+			{
+				tmp1.len_alloc = tmp1.len_used;
+				status = ydb_incr_s(&words, 1, &tmp1, NULL, &value);	/* M line : set value=$incr(words(tmp1)) */
+				assert(YDB_OK == status);
+			}
+			ptr = strtok(NULL, " ");
+		}
+	} while (1);
+	tmp1.buf_addr = tmp1buff;			/* M line : set tmp1="" */
+	tmp1.len_used = 0;
+	tmp1.len_alloc = sizeof(tmp1buff);
+	words_tmp1.buf_addr = &words_tmp1buff[0];
+	words_tmp1.len_used = 0;
+	words_tmp1.len_alloc = sizeof(words_tmp1buff);
+	null.buf_addr = NULL;
+	null.len_used = 0;
+	null.len_alloc = 0;
+	do
+	{
+		status = ydb_subscript_next_s(&words, 1, &tmp1, &tmp1);	/* M line : set tmp1=$order(words(tmp1)) */
+		assert(YDB_OK == status);
+		if (0 == tmp1.len_used)
+			break;
+		status = ydb_get_s(&words, 1, &tmp1, &words_tmp1);	/* M line : set words_tmp1=words(tmp1) */
+		assert(YDB_OK == status);
+		index_subscr[0] = words_tmp1;
+		index_subscr[1] = tmp1;
+		status = ydb_set_s(&index, 2, index_subscr, &null);	/* M line : set index(words_tmp1,tmp1)="" */
+		assert(YDB_OK == status);
+	} while (1);
+	do
+	{
+		status = ydb_subscript_previous_s(&index, 1, &tmp1, &tmp1);	/* M line : set tmp1=$order(index(tmp1),-1) */
+		assert(YDB_OK == status);
+		if (0 == tmp1.len_used)
+			break;
+		tmp2.buf_addr = tmp2buff;			/* M line : set tmp2="" */
+		tmp2.len_used = 0;
+		tmp2.len_alloc = sizeof(tmp2buff);
+		index_subscr[0] = tmp1;
+		index_subscr[1] = tmp2;
+		do
+		{
+			status = ydb_subscript_next_s(&index, 2, index_subscr, &tmp2); /* M line : set tmp2=$order(index(tmp1,tmp2)) */
+			assert(YDB_OK == status);
+			if (0 == tmp2.len_used)
+				break;
+			tmp1.buf_addr[tmp1.len_used] = '\0';
+			tmp2.buf_addr[tmp2.len_used] = '\0';
+			printf("%s\t%s\n", tmp1.buf_addr, tmp2.buf_addr);
+			index_subscr[1] = tmp2;
+		} while (1);
+	} while (1);
+	return YDB_OK;
+}
--- simpleapi/u_inref/wordfreq.csh
+++ simpleapi/u_inref/wordfreq.csh
@@ -11,12 +11,12 @@
 #								#
 #################################################################
 #
-# Use simpleAPI to find the maximum number of steps for the 3n+1 problem for all integers through two input integers.
+# Use simpleAPI to find the frequency of words in an input text file
 #
 
 $gtm_tst/com/dbcreate.csh mumps 1
 
-set file = "threen1g.c"
+set file = "wordfreq.c"
 cp $gtm_tst/$tst/inref/$file .
 
 echo " --> Running $file <---"
@@ -24,10 +24,17 @@ set exefile = $file:r
 $gt_cc_compiler $gtt_cc_shl_options -I$gtm_tst/com -I$gtm_dist $file
 $gt_ld_linker $gt_ld_option_output $exefile $gt_ld_options_common $exefile.o $gt_ld_sysrtns $ci_ldpath$gtm_dist -L$gtm_dist $tst_ld_gtmshr $gt_ld_syslibs >& $exefile.map
 if (0 != $status) then
-	echo "THREEN1G-E-LINKFAIL : Linking $exefile failed. See $exefile.map for details"
+	echo "WORDFREQ-E-LINKFAIL : Linking $exefile failed. See $exefile.map for details"
 	exit -1
 endif
-./$exefile 10 20
+./$exefile < $gtm_tst/$tst/outref/wordfreq_input.txt >& wordfreq.out
+echo "# Running : diff $gtm_tst/$tst/outref/wordfreq_output.txt wordfreq.out"
+diff $gtm_tst/$tst/outref/wordfreq_output.txt wordfreq.out
+if ($status) then
+	echo "WORDFREQ-E-FAIL : diff failed"
+else
+	echo "  --> PASS from wordfreq"
+endif
 echo ""
 
 $gtm_tst/com/dbcheck.csh
