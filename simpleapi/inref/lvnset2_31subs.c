ff9094e30044d4b93d2d785c2152933f64f6548d
Narayanan Iyer <nars@yottadb.com>
2017-12-29 12:16:07 -0500

    [#59] Restructure simpleapi/lvnset subtest (will serve as model for other simpleapi subtests)

--- simpleapi/inref/lvnset2_31subs.c
+++ simpleapi/inref/lvnset2_31subs.c
@@ -24,7 +24,6 @@
 #define VALUE2	"One less than 43"
 #define VALUE3 	"Life, the universe, and everything"
 
-/* Test 32-level (max-deep) subscripts can be set in Local Variables */
 int main()
 {
 	int		status, subs;
@@ -32,6 +31,8 @@ int main()
 	char		errbuf[ERRBUF_SIZE], subsstrlit[MAX_SUBS][3];	/* 3 to hold 2 digit decimal # + trailing null char */
 	ydb_string_t	zwrarg;
 
+	printf("# Test 31-level (max-deep) subscripts can be set using ydb_set_s() of Local Variables\n"); fflush(stdout);
+	printf("# Also test that passing in a NULL value results in null string value\n"); fflush(stdout);
 	/* Initialize varname, subscript, and value buffers */
 	YDB_STRLIT_TO_BUFFER(&basevar, BASEVAR);
 	YDB_STRLIT_TO_BUFFER(&subscr[0], SUBSCR1);
@@ -39,39 +40,15 @@ int main()
 	YDB_STRLIT_TO_BUFFER(&value1, VALUE1);
 	YDB_STRLIT_TO_BUFFER(&value2, VALUE2);
 	YDB_STRLIT_TO_BUFFER(&value3, VALUE3);
-	/* Set a base variable, no subscripts */
-	status = ydb_set_s(&basevar, 0, NULL, &value1);
-	if (YDB_OK != status)
-	{
-		ydb_zstatus(errbuf, ERRBUF_SIZE);
-		printf("ydb_set_s() [1]: %s\n", errbuf);
-		fflush(stdout);
-		return YDB_OK;
-	}
-	/* Set single subscript value */
-	status = ydb_set_s(&basevar, 1, subscr, &value2);
-	if (YDB_OK != status)
-	{
-		ydb_zstatus(errbuf, ERRBUF_SIZE);
-		printf("ydb_set_s() [2]: %s\n", errbuf);
-		fflush(stdout);
-		return YDB_OK;
-	}
-	/* Set two subscript value */
-	status = ydb_set_s(&basevar, 2, subscr, &value3);
-	if (YDB_OK != status)
-	{
-		ydb_zstatus(errbuf, ERRBUF_SIZE);
-		printf("ydb_set_s() [3]: %s\n", errbuf);
-		fflush(stdout);
-		return YDB_OK;
-	}
-	/* Set 32-level deep subscripts */
 	for (subs = 0; subs < MAX_SUBS; subs++)
 	{
-		subsbuff[subs].len_used = sprintf(subsstrlit[subs], "%d", subs);
+		printf("Set a local variable with %d subscripts\n", subs); fflush(stdout);
+		subsbuff[subs].len_used = subsbuff[subs].len_alloc = sprintf(subsstrlit[subs], "%d", subs);
 		subsbuff[subs].buf_addr = subsstrlit[subs];
-		status = ydb_set_s(&basevar, subs, subsbuff, &subsbuff[subs]);
+		if (subs % 2)
+			status = ydb_set_s(&basevar, subs, subsbuff, &subsbuff[subs]);
+		else
+			status = ydb_set_s(&basevar, subs, subsbuff, NULL);
 		if (YDB_OK != status)
 		{
 			ydb_zstatus(errbuf, ERRBUF_SIZE);
@@ -80,7 +57,7 @@ int main()
 			return YDB_OK;
 		}
 	}
-	/* Demonstrate our progress by executing a ZWRITE in a call-in */
+	printf("Demonstrate our progress by executing a ZWRITE in a call-in\n"); fflush(stdout);
 	zwrarg.address = NULL;
 	zwrarg.length = 0;
 	status = ydb_ci("driveZWRITE", &zwrarg);
---
+++ simpleapi/inref/lvnset3_errors.c
@@ -0,0 +1,132 @@
+/****************************************************************
+ *								*
+ * Copyright (c) 2017 YottaDB LLC. and/or its subsidiaries.	*
+ * All rights reserved.						*
+ *								*
+ *	This source code contains the intellectual property	*
+ *	of its copyright holder(s), and is made available	*
+ *	under a license.  If you do not know the terms of	*
+ *	the license, please stop and do not read further.	*
+ *								*
+ ****************************************************************/
+
+#include "libyottadb.h"
+
+#include <stdio.h>
+
+#define ERRBUF_SIZE	1024
+
+#define BADBASEVAR1 "B%dbasevarInvChar"
+#define BADBASEVAR2 "Verylongbasevarthatexceedsmaxlength"
+#define BADBASEVAR3 "1namestartswithdigit"
+#define BASEVAR "baselv"
+#define SUBSCR32 "x"
+#define VALUE1	"A question"
+
+int main()
+{
+	int		i, status;
+	ydb_buffer_t	subscr32[32], value1, badbasevar, basevar;
+	char		errbuf[ERRBUF_SIZE];
+
+	printf("# Test error scenarios in ydb_set_s() of Local Variables\n"); fflush(stdout);
+	/* Initialize varname and value buffers */
+	YDB_STRLIT_TO_BUFFER(&basevar, BASEVAR);
+	YDB_STRLIT_TO_BUFFER(&value1, VALUE1);
+
+	/* Now for a few error cases - first up, bad basevar names */
+	printf("Attempting set of bad basevar (%% in middle of name) %s\n", BADBASEVAR1);
+	fflush(stdout);
+	YDB_STRLIT_TO_BUFFER(&badbasevar, BADBASEVAR1);
+	status = ydb_set_s(&badbasevar, 0, NULL, &value1);
+	if (YDB_OK != status)
+	{
+		ydb_zstatus(errbuf, ERRBUF_SIZE);
+		printf("ydb_set_s() [a]: %s\n", errbuf);
+		fflush(stdout);
+	}
+	printf("Attempting set of bad basevar (> 31 characters) %s\n", BADBASEVAR2);
+	YDB_STRLIT_TO_BUFFER(&badbasevar, BADBASEVAR2);
+	status = ydb_set_s(&badbasevar, 0, NULL, &value1);
+	if (YDB_OK != status)
+	{
+		ydb_zstatus(errbuf, ERRBUF_SIZE);
+		printf("ydb_set_s() [b]: %s\n", errbuf);
+		fflush(stdout);
+	}
+	printf("Attempting set of bad basevar (first letter in name is digit) %s\n", BADBASEVAR3);
+	YDB_STRLIT_TO_BUFFER(&badbasevar, BADBASEVAR3);
+	status = ydb_set_s(&badbasevar, 0, NULL, &value1);
+	if (YDB_OK != status)
+	{
+		ydb_zstatus(errbuf, ERRBUF_SIZE);
+		printf("ydb_set_s() [c]: %s\n", errbuf);
+		fflush(stdout);
+	}
+	/* Now try setting a non-existent subscript */
+	printf("Attempting set of basevar with NULL subscript address parameter where basevar is undefined\n");
+	status = ydb_set_s(&basevar, 1, NULL, &value1);
+	if (YDB_OK != status)
+	{
+		ydb_zstatus(errbuf, ERRBUF_SIZE);
+		printf("ydb_set_s() [d]: %s\n", errbuf);
+		fflush(stdout);
+	}
+	printf("Attempting set of basevar with NULL subscript address parameter where basevar is defined\n");
+	/* Set a base variable, and then retry the set of a non-existent subscript */
+	status = ydb_set_s(&basevar, 0, NULL, &value1);
+	if (YDB_OK != status)
+	{
+		ydb_zstatus(errbuf, ERRBUF_SIZE);
+		printf("ydb_set_s() [1]: %s\n", errbuf);
+		fflush(stdout);
+	}
+	status = ydb_set_s(&basevar, 1, NULL, &value1);
+	if (YDB_OK != status)
+	{
+		ydb_zstatus(errbuf, ERRBUF_SIZE);
+		printf("ydb_set_s() [d]: %s\n", errbuf);
+		fflush(stdout);
+	}
+	/* Now try setting > 31 subscripts */
+	printf("Attempting set of basevar with 32 subscripts\n");
+	for (i = 0; i < 32; i++)
+		YDB_STRLIT_TO_BUFFER(&subscr32[i], SUBSCR32);
+	status = ydb_set_s(&basevar, 32, subscr32, &value1);
+	if (YDB_OK != status)
+	{
+		ydb_zstatus(errbuf, ERRBUF_SIZE);
+		printf("ydb_set_s() [e]: %s\n", errbuf);
+		fflush(stdout);
+	}
+	/* Now try setting < 0 subscripts */
+	printf("Attempting set of basevar with -1 subscripts\n");
+	status = ydb_set_s(&basevar, -1, NULL, &value1);
+	if (YDB_OK != status)
+	{
+		ydb_zstatus(errbuf, ERRBUF_SIZE);
+		printf("ydb_set_s() [f]: %s\n", errbuf);
+		fflush(stdout);
+	}
+	/* Test of NORETBUFFER */
+	printf("Attempting set with value->len_alloc == 0\n");
+	value1.len_alloc = 0;
+	status = ydb_set_s(&basevar, 0, NULL, &value1);
+	if (YDB_OK != status)
+	{
+		ydb_zstatus(errbuf, ERRBUF_SIZE);
+		printf("ydb_set_s() [g]: %s\n", errbuf);
+		fflush(stdout);
+	}
+	printf("Attempting set with value->buf_addr == NULL\n");
+	YDB_STRLIT_TO_BUFFER(&value1, VALUE1);
+	value1.buf_addr = NULL;
+	status = ydb_set_s(&basevar, 0, NULL, &value1);
+	if (YDB_OK != status)
+	{
+		ydb_zstatus(errbuf, ERRBUF_SIZE);
+		printf("ydb_set_s() [h]: %s\n", errbuf);
+		fflush(stdout);
+	}
+	return YDB_OK;
+}
