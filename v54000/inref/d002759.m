bcb29154eb0c2de58b0d17f664477888165f5a83
Narayanan Iyer <nars@yottadb.com>
2018-01-02 11:38:36 -0500

    [#59] Add 2018 copyright to all modules till now changed in this project

--- v54000/inref/d002759.m
+++ v54000/inref/d002759.m
@@ -1,7 +1,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;								;
-; Copyright (c) 2017 YottaDB LLC. and/or its subsidiaries.	;
-; All rights reserved.	     	  	     			;
+; Copyright (c) 2017-2018 YottaDB LLC. and/or its subsidiaries.	;
+; All rights reserved.						;
 ;								;
 ;	This source code contains the intellectual property	;
 ;	of its copyright holder(s), and is made available	;
@@ -32,7 +32,7 @@
 	. . Set error=0
 	. . Do prime4
 	. . Do SimpleTest(.xdawarr)
-	. . If ('error) Do	; Did not get an error - check if this is expected or not
+	. . If ('error) Do	 ; Did not get an error - check if this is expected or not
 	. . . If (expect(def,subs)'="") Write "FAIL -- Case ",def,"/",subs," VAR was defined",!
 	. . . Else  Write "PASS -- Case ",def,"/",subs," VAR was defined",!
 	. . Write !
