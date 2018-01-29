c1493152fa7e837341debbd5b0f94ccc3095560f
Narayanan Iyer <nars@yottadb.com>
2018-01-29 10:17:52 -0500

    [#141] Add r120/zstepoveroutof subtest (tests YottaDB/YottaDB#141)

---
+++ r120/inref/zstover2.m
@@ -0,0 +1,19 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;								;
+; Copyright (c) 2018 YottaDB LLC. and/or its subsidiaries.	;
+; All rights reserved.	     	  	     			;
+;								;
+;	This source code contains the intellectual property	;
+;	of its copyright holder(s), and is made available	;
+;	under a license.  If you do not know the terms of	;
+;	the license, please stop and do not read further.	;
+;								;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+zstover2	;
+	do ^zstepover
+	set x1=$$y
+	quit
+y()
+	set xx="x2",x2=""
+	quit @xx
