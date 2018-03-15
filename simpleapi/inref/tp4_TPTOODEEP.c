9f5ee74c0644eeae96089b4e291f62343fd64be6
Narayanan Iyer <nars@yottadb.com>
2018-03-15 09:05:53 -0400

    [#59] Enhance simpleapi/tp subtest to test TPTOODEEP error

---
+++ simpleapi/inref/tp4_TPTOODEEP.c
@@ -0,0 +1,47 @@
+/****************************************************************
+ *								*
+ * Copyright (c) 2018 YottaDB LLC. and/or its subsidiaries.	*
+ * All rights reserved.						*
+ *								*
+ *	This source code contains the intellectual property	*
+ *	of its copyright holder(s), and is made available	*
+ *	under a license.  If you do not know the terms of	*
+ *	the license, please stop and do not read further.	*
+ *								*
+ ****************************************************************/
+
+#include "libyottadb.h"
+
+#include <stdio.h>
+
+#define	ERRBUF_SIZE	1024
+
+char		errbuf[ERRBUF_SIZE];
+int		gvnset();
+int		level;
+ydb_tpfnptr_t	tpfn;
+
+/* Function to do a simple set of 3 Global variable nodes (done in "gvnset()") inside of a TP transaction */
+int main()
+{
+	int		status;
+
+	tpfn = &gvnset;
+	status = ydb_tp_s(tpfn, NULL, NULL, 0, NULL);
+	if (YDB_OK != status)
+	{
+		ydb_zstatus(errbuf, ERRBUF_SIZE);
+		printf("ydb_tp_s() exit status = %d : %s\n", status, errbuf);
+		fflush(stdout);
+	}
+	return status;
+}
+
+/* Function to set a global variable */
+int gvnset()
+{
+	int		status;
+
+	printf("Entering ydb_tp_s() : Level = %d\n", level++);
+	return ydb_tp_s(tpfn, NULL, NULL, 0, NULL);
+}
