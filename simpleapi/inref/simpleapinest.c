8b785bfd65c9307875a508c30885817d49a86660
Narayanan Iyer <nars@yottadb.com>
2018-03-18 09:28:12 -0400

    [#59] New simpleapi/simpleapinest subtest to test SIMPLEAPINEST error

---
+++ simpleapi/inref/simpleapinest.c
@@ -0,0 +1,39 @@
+/****************************************************************
+ *								*
+ * Copyright (c) 2018 YottaDB LLC. and/or its subsidiaries.	*
+ * All rights reserved.						*
+ *								*
+ *	This source code contains the intellectual property	*
+ *	of its copyright holder(s), and is made available	*
+ *	under a license.  If you do not know the terms of	*
+ *	the license, please stop and do not read further.	*
+ *								*
+ ****************************************************************/
+
+#include "libyottadb.h"
+
+#include <stdio.h>
+
+#define ERRBUF_SIZE	1024
+
+#define BASEVAR "^basevar"
+#define VALUE1	"value"
+
+char		errbuf[ERRBUF_SIZE];
+ydb_buffer_t	basevar, value1, badbasevar;
+
+int main()
+{
+	int		status;
+
+	printf("### Test of SIMPLEAPINEST error###\n"); fflush(stdout);
+
+	YDB_LITERAL_TO_BUFFER(BASEVAR, &basevar);
+	YDB_LITERAL_TO_BUFFER(VALUE1, &value1);
+
+	printf("# Do SET of global that invokes a trigger###\n"); fflush(stdout);
+	status = ydb_set_s(&basevar, 0, NULL, &value1);
+	YDB_ASSERT(YDB_OK == status);
+	return YDB_OK;
+}
+
