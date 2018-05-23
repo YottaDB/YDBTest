8ea1bc8b270386878354bdc40a71c7fa4cba6190
Jake Rezac <jake@yottadb.com>
2018-05-23 15:24:06 -0400

    Scaffolding fro gtm8922

--- v63004/inref/gtm8922.m
+++ v63004/inref/gtm8922.m
@@ -10,135 +10,72 @@
 ;								;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;
-testA
-	WRITE "----------",!
-	WRITE "testA",!
-	WRITE "----------",!!
+gtm8922
 
-	do checkRegion()
+	WRITE $VIEW("DBFLUSH")
+	WRITE "VIEW ""DBFLUSH"":""AREG,BREG"""
+	VIEW "DBFLUSH":"AREG,BREG"
+	WRITE $VIEW("DBFLUSH")
 
-	WRITE !,"VIEW ""STATSHARE"" ",!
-	VIEW "STATSHARE"
-	do checkRegion()
-	WRITE !
+	WRITE $VIEW("DBSYNC")
+	WRITE "VIEW ""DBSYNC"":""AREG,BREG"""
+	VIEW "DBSYNC":"AREG,BREG"
+	WRITE $VIEW("DBSYNC")
 
-	WRITE !,"VIEW ""NOSTATSHARE"" ",!
-	VIEW "NOSTATSHARE"
-	do checkRegion()
-	WRITE !
+	;WRITE "VIEW ""STATSHARE"":""AREG"""
+	;VIEW "STATSHARE":"AREG"
+	;WRITE !
+	;do checkRegion()
+	;WRITE !
 
-	WRITE "VIEW ""STATSHARE"":""AREG"""
-	VIEW "STATSHARE":"AREG"
-	WRITE !
-	do checkRegion()
-	WRITE !
-
-	WRITE "VIEW ""STATSHARE"":""DEFAULT""",!
-	VIEW "STATSHARE":"DEFAULT"
-	WRITE !
-	do checkRegion()
-	WRITE !
-	WRITE "VIEW ""NOSTATSHARE"":""AREG""",!
-	VIEW "NOSTATSHARE":"AREG"
-	WRITE !
-	do checkRegion()
-	WRITE !
-
-	WRITE "VIEW ""STATSHARE"""
-	VIEW "STATSHARE"
-	WRITE !
-	do checkRegion()
-	WRITE !
-
-	WRITE "VIEW ""NOSTATSHARE"""
-	VIEW "NOSTATSHARE"
-	WRITE !
-	do checkRegion()
-	WRITE !
+	;WRITE "VIEW ""STATSHARE"":""DEFAULT""",!
+	;VIEW "STATSHARE":"DEFAULT"
+	;WRITE !
+	;do checkRegion()
+	;WRITE !
+	;WRITE "VIEW ""NOSTATSHARE"":""AREG""",!
+	;VIEW "NOSTATSHARE":"AREG"
+	;WRITE !
+	;do checkRegion()
+	;WRITE !
 
 	quit
 
 
-testB
-	WRITE "----------",!
-	WRITE "testB",!
-	WRITE "----------",!!
 
-	WRITE "VIEW ""STATSHARE"""
-	VIEW "STATSHARE"
-	WRITE !
-	do checkRegion()
-	WRITE !
-
-
-	quit
-
-
-testC
-	WRITE "----------",!
-	WRITE "testC ("_$ZCMDLINE_")",!
-	WRITE "----------",!!
-
-	WRITE "SET $ZGBLDIR=""otherA.gld"" ",!
-	SET $ZGBLDIR="otherA.gld"
-
-	WRITE "VIEW ""STATSHARE"":""DEFAULT"" ",!
-	VIEW "STATSHARE":"DEFAULT"
-	WRITE !
-	do checkRegion()
-	WRITE !
-
-	WRITE "VIEW ""NOSTATSHARE"":""DEFAULT"" ",!
-	VIEW "NOSTATSHARE":"DEFAULT"
-	WRITE !
-	do checkRegion()
-	WRITE !
-
-	VIEW $ZCMDLINE	;Either "NOSTATSHARE or STATSHARE
-	WRITE "SET $ZGBLDIR=""otherB.gld"" ",!
-	SET $ZGBLDIR="otherB.gld"
-
-	set x=$get(^x)
-	WRITE !
-	do checkRegion()
-	WRITE !
-
-	quit
-
-
-checkRegion()
-
-	WRITE "Check for open region files:",!
-
-	; Filters the file names from the ls output
-	ZSYSTEM "ls -l /proc/"_$job_"/fd | $grep '.dat$' | awk -F '/' '{print $(NF)}' | sort"
-
-
-	; .gst files start with an RNG number, so the file names themselves require additional filtering
-
-	set line1="ls -l /proc/"_$job_"/fd | $grep '.gst$' | "
-	set line2="awk -F '/' '{print $(NF)}'  | "
-	set line3="awk -F '.' 'BEGIN { ORS="""" }; {print ""xxx""; for (i = 2; i <= NF ; i++) { print ""."" ; print $i } print ""\n""}' | "
-	set line4=" sort"
-
-	ZSYSTEM line1_line2_line3_line4
-
-	WRITE !,"Check Sharing Status:",!
-
-	set reg=""
-	for  set reg=$view("GVNEXT",reg) quit:reg=""  DO
-	. WRITE "STATSHARE "_reg_": "
-	. WRITE $VIEW("STATSHARE",reg),!
-
-	WRITE "STATSHARE: "
-	WRITE $VIEW("STATSHARE"),!
-
-	WRITE "^%YGS : ",!
-	IF $data(^%YGS)  DO
-	. ZWR ^%YGS
-
-	ELSE  DO
-	. WRITE "NO REGION"
-
-	quit
-
+;checkRegion()
+;
+;	WRITE "Check for open region files:",!
+;
+;	; Filters the file names from the ls output
+;	ZSYSTEM "ls -l /proc/"_$job_"/fd | $grep '.dat$' | awk -F '/' '{print $(NF)}' | sort"
+;
+;
+;	; .gst files start with an RNG number, so the file names themselves require additional filtering
+;
+;	set line1="ls -l /proc/"_$job_"/fd | $grep '.gst$' | "
+;	set line2="awk -F '/' '{print $(NF)}'  | "
+;	set line3="awk -F '.' 'BEGIN { ORS="""" }; {print ""xxx""; for (i = 2; i <= NF ; i++) { print ""."" ; print $i } print ""\n""}' | "
+;	set line4=" sort"
+;
+;	ZSYSTEM line1_line2_line3_line4
+;
+;	WRITE !,"Check Sharing Status:",!
+;
+;	set reg=""
+;	for  set reg=$view("GVNEXT",reg) quit:reg=""  DO
+;	. WRITE "STATSHARE "_reg_": "
+;	. WRITE $VIEW("STATSHARE",reg),!
+;
+;	WRITE "STATSHARE: "
+;	WRITE $VIEW("STATSHARE"),!
+;
+;	WRITE "^%YGS : ",!
+;	IF $data(^%YGS)  DO
+;	. ZWR ^%YGS
+;
+;	ELSE  DO
+;	. WRITE "NO REGION"
+;
+;	quit
+;
--- v63004/u_inref/gtm8922.csh
+++ v63004/u_inref/gtm8922.csh
@@ -14,71 +14,19 @@
 echo '# Testing VIEW "[NO]STATSHARE"[:<region-list>]'
 
 echo '#### testA ####'
-echo '# enables or disables database statistics sharing for listed regions which permit such sharing. '
-echo '#'
-echo '# Without the region-list, the command acts on all regions enabled for sharing. '
-echo '#'
-echo '# When a targeted region has sharing disabled, STATSHARE has no immediate effect, '
-echo ''
 
 echo "# Create a 3 region DB with gbl_dir mumps.gld and regions DEFAULT, AREG, and BREG"
 $gtm_tst/com/dbcreate.csh mumps 3 >>& dbcreate_log_1.txt
 
-echo ''
-echo '# Disable sharing for BREG'
-$MUPIP set -NOSTAT  -reg "BREG" #>>& dbcreate_log.txt
+#echo ''
+#echo '# Disable sharing for BREG'
+#$MUPIP set -NOSTAT  -reg "BREG" #>>& dbcreate_log.txt
 
-echo '# Run testA of gtm8874.m to test basic VIEW functionality and disabled regions'
-$ydb_dist/mumps -run testA^gtm8874
+echo '# Run gtm8874.m to ...'
+$ydb_dist/mumps -run gtm8922
 
-echo '# Shut down the DB and backup necessary files to sub directory'
+echo '# Shut down the DB '#and backup necessary files to sub directory'
 $gtm_tst/com/dbcheck.csh >>& dbcreate_log_1.txt
-$gtm_tst/com/backup_dbjnl.csh dbbkup1 "*.gld *.mjl* *.mjf *.dat" cp nozip
+#$gtm_tst/com/backup_dbjnl.csh dbbkup1 "*.gld *.mjl* *.mjf *.dat" cp nozip
 echo ''
 
-echo '#### testB ####'
-echo '# the $gtm_statshare environment variable applies to databases as the application first uses them'
-echo ''
-
-echo '# setenv gtm_statshare "TRUE"'
-setenv gtm_statshare "TRUE"
-echo '# Recreate the 3 region DB with gbl_dir mumps.gld'
-$gtm_tst/com/dbcreate.csh mumps 3 >>& dbcreate_log_2.txt
-
-echo ''
-echo '# Disable sharing for BREG'
-$MUPIP set -NOSTAT  -reg "BREG" #>>& dbcreate_log.txt
-
-echo '# Run testB1 of gtm8874.m to V[IEW] "STATSHARE" and run $VIEW for each region with gtm_statshare="TRUE"'
-$ydb_dist/mumps -run testB^gtm8874
-
-echo 'unsetenv gtm_statshare'
-unsetenv gtm_statshare
-
-echo '# Shut down the DB and backup necessary files to sub directory'
-$gtm_tst/com/dbcheck.csh >>& dbcreate_log_2.txt
-$gtm_tst/com/backup_dbjnl.csh dbbkup2 "*.gld *.mjl* *.mjf *.dat" cp nozip
-echo ''
-
-echo '#### testC ####'
-echo '# VIEW "[NO]STATSHARE" with no region sub-argument opens any unopened mapped regions'
-echo '# and any enabled associated statsDB regions'
-echo '#'
-echo '# When the last VIEW "[NO]STATSHARE" had no region sub-argument, regions implicitly share '
-echo '# when the process first references them, but after a VIEW specifies selective sharing, '
-echo '# regions do not implicitly share as they open'
-echo ''
-
-echo "# Create a 1 region DB with gbl_dir otherA.gld"
-$gtm_tst/com/dbcreate.csh otherA >>& dbcreate_log_3.txt
-echo "# Backup otherA.dat DB"
-$gtm_tst/com/backup_dbjnl.csh dbbkup3 "*.gld *.mjl* *.mjf *.dat" cp nozip
-echo "# Create a 1 region DB with gbl_dir otherB.gld"
-$gtm_tst/com/dbcreate.csh otherB >>& dbcreate_log_3.txt
-echo "# Move otherA.dat DB files back to current directory"
-
-mv dbbkup3/* ./
-
-echo '# Run testC of gtm8874.m to test implicit sharing of VIEW "STATSHARE"'
-$ydb_dist/mumps -run testC^gtm8874 "NOSTATSHARE"
-$ydb_dist/mumps -run testC^gtm8874 "STATSHARE"
