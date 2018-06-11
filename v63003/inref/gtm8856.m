0a13afd0d0cb9341a8a36d968ff750a1b331ad19
Vinay Ekambaram <vinay@yottadb.com>
2018-06-11 11:03:20 -0400

    New v63003/gtm8856 subtest (tests GTM-8856 in V63003)

---
+++ v63003/inref/gtm8856.m
@@ -0,0 +1,22 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;								;
+; Copyright (c) 2018 YottaDB LLC. and/or its subsidiaries.	;
+; All rights reserved.						;
+;								;
+;	This source code contains the intellectual property	;
+;	of its copyright holder(s), and is made available	;
+;	under a license.  If you do not know the terms of	;
+;	the license, please stop and do not read further.	;
+;								;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;
+
+gtm8856
+        view "PATLOAD":"patcode.txt"
+        Write "PATCODE status = ",$$set^%PATCODE("NEWLANGUAGE"),!
+        write "Need to see "
+        xecute:0 "write 1?1K"
+        write "this statement in full",!
+        quit
+
+
