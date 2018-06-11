bdb9d8a4f7a2009aa98aaf41f5526b8248632188
VinayYottaDB <39492500+VinayYottaDB@users.noreply.github.com>
2018-06-11 10:25:57 -0400

    New v63003/gtm8854 subtest (tests GTM-8854 in V63003)
    Tests the following release note:
    
    The compiler appropriately handles a syntax error in the argument of a postconditional command when the postconditional is a literal that evaluates to FALSE. Due to a regression associated with GTM-8573 in rm inV6.3-001[A], this combination caused an abnormal termination with a segmentation violation (SIG-11). (GTM-8854)

---
+++ v63003/inref/gtm8854.m
@@ -0,0 +1,22 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;								;
+; Copyright (c) 2018 YottaDB LLC. and/or its subsidiaries.	;
+; All rights reserved.						;
+;								;
+;	This source code contains the intellectual property	;
+;	of its copyright holder(s), and is made available	;
+;	under a license.  If you do not know the terms of	;
+;	the license, please stop and do not read further.	;
+;								;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;
+;
+;
+
+test1
+	hang:0
+	quit
+
+test2
+	hang:0 0,
+	quit
