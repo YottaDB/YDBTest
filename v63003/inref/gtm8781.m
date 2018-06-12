621f92a068f87076a61fe54a8d961100718bf08e
Vinay Ekambaram <vinay@yottadb.com>
2018-06-12 14:47:06 -0400

    New v63003/gtm8781 subtest (Tests GTM-8781 in V63003)

---
+++ v63003/inref/gtm8781.m
@@ -0,0 +1,24 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;								;
+; Copyright (c) 2018 YottaDB LLC. and/or its subsidiaries.	;
+; All rights reserved.						;
+;								;
+;	This source code contains the intellectual property	;
+;	of its copyright holder(s), and is made available	;
+;	under a license.  If you do not know the terms of	;
+;	the license, please stop and do not read further.	;
+;								;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;
+
+gtm8781
+	zsystem "echo 'gtm8781 subtest'"
+	write "Memory Allocated: ",$zrealstor,!
+	set mem=$zrealstor
+	set pass=1
+	set halt=0
+	for i=1:1:100 do  quit:halt=1
+	. zsystem "echo 'hello' > /dev/null"
+	. if mem'=$zrealstor  set pass=0  write "Memory Leak Detected, Test Failed"  set halt=1
+	write:pass "No Memory Leaks, Test Passed"
+	quit
