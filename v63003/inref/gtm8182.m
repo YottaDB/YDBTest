5568d3cfdbfd8df110db6b2bd8417f8a9e801e57
Jake Rezac <jake@yottadb.com>
2018-06-27 12:49:13 -0400

    Scaffolding for gtm8182

--- v63003/inref/gtm8182.m
+++ v63003/inref/gtm8182.m
@@ -16,27 +16,15 @@ gtm8182
 	SET INST1gbldir=INST1path_"/mumps.gld"
 	SET INST1repl=INST1path_"/mumps.repl"
 
-	SET INST2path=$ZTRNLNM("path_INST2","","","","","VALUE")
-	SET INST2gbldir=INST2path_"/mumps.gld"
-
 	SET INST3path=$ZTRNLNM("path_INST3","","","","","VALUE")
 	SET INST3gbldir=INST3path_"/mumps.gld"
 	SET INST3repl=INST3path_"/mumps.repl"
 
-	SET INST4path=$ZTRNLNM("path_INST4","","","","","VALUE")
-	SET INST4gbldir=INST4path_"/mumps.gld"
-
 	WRITE "INST1gbldir: "
 	WRITE INST1gbldir,!
 
-	WRITE "INST2gbldir: "
-	WRITE INST2gbldir,!
-
 	WRITE "INST3gbldir: "
 	WRITE INST3gbldir,!
-
-	WRITE "INST4gbldir: "
-	WRITE INST4gbldir,!
 
 	WRITE "ZGBLDIR: ",$ZGBLDIR,!
 	; Update the DB of INST1
@@ -45,19 +33,18 @@ gtm8182
 	; Switch to global directory for INST3
 	SET $ZGBLDIR=INST3gbldir
 	WRITE "ZGBLDIR: ",$ZGBLDIR,!
-	DO setenv^%POSIX("ydb_gbldir",INST3gbldir,1) ; setenv gbl dir to INST3's
-	;WRITE:$$setenv^%POSIX("ydb_gbldir",INST3gbldir,1)
-
-	ZSYSTEM "$GDE CHANGE -INSTANCE -FILE_NAME="""_INST3repl_""""
+	;DO setenv^%POSIX("ydb_gbldir",INST3gbldir,1) ; setenv gbl dir to INST3's
+	;ZSYSTEM "$GDE CHANGE -INSTANCE -FILE_NAME="""_INST3repl_""""
+	ZSYSTEM "setenv ydb_gbldir """_INST3gbldir_""" ; "_"$GDE CHANGE -INSTANCE -FILE_NAME="""_INST1repl_""""
 
 	; Update the DB of INST3
 	SET ^zack="oldest"
 
 	; Switch back to global directory for INST1
-	DO setenv^%POSIX("ydb_gbldir",INST1gbldir,1) ; setenv gbl dir back to INST1's
-	;WRITE:$$setenv^%POSIX("ydb_gbldir",INST1gbldir,1)
 	SET $ZGBLDIR=INST1gbldir
 	WRITE "ZGBLDIR: ",$ZGBLDIR,!
-	ZSYSTEM "$GDE CHANGE -INSTANCE -FILE_NAME="""_INST1repl_""""
+	;DO setenv^%POSIX("ydb_gbldir",INST1gbldir,1) ; setenv gbl dir back to INST1's
+	;ZSYSTEM "$GDE CHANGE -INSTANCE -FILE_NAME="""_INST1repl_""""
+	ZSYSTEM "setenv ydb_gbldir """_INST1gbldir_""" ; "_"$GDE CHANGE -INSTANCE -FILE_NAME="""_INST1repl_""""
 
 	quit
