98031a936254baf23d710660d907e7340420cf89
JakeYottaDB <38507582+JakeYottaDB@users.noreply.github.com>
2018-07-11 13:48:52 -0400

    New v63003/gtm8182 subtest (tests GTM-8182 in V63003) (#276)
    Tests the following release note:
    
    GT.M allows updating globals belonging to a different source instance using extended global references
    or SET $ZGBLDIR. While the replication setup remains the same, these are the main considerations:
    
    1) Use one of two ways to identify the current instance as specified by a replication instance file:
    
       a) A global directory can define a mapping to a replication instance file as specified with a GDE
          CHANGE -INSTANCE -FILE_NAME=<replication_instance_file> command. When a global directory is use, if
          it has a mapping of an instance file, that mapping overrides any setting of the gtm_repl_instance
          environment variable. GDE CHANGE -INSTANCE -FILE_NAME="" removes any global directory mapping
          for an instance file.
    
       b) The gtm_repl_instance environment variable specifies a replication instance file for utilities,
          and, as the default, whenever a user processes relies on a global directory with no instance
          file specification.
    
    2) In order to use multiple instances, at least one global directory must have an instance mapping.
    
    3) A replication instance file cannot share any region with another instance file.
    
    4) The Source Servers of all the instances have properly set up Replication Journal Pools.
    
    5) A TP transaction or a trigger, as it always executes within a TP transaction, must always restrict
       updates to globals in one replicating instance.
    
    Notes
    
    * Like other mapping specified by a global directory, a process determines any instance mapping 
      by a global directory at the time a process first uses uses the global directory. Processes other
      than MUPIP CREATE ignore other (non-mapping) global directory database characteristics, except for
      collation, which interacts with mapping.
    
    * When Instance Freeze is enabled (gtm_custom_errors is appropriately defined), a process
      attaches a region to an instance at the first access to the region; the access may be a read or a
      VIEW/$VIEW(). Otherwise, the process attaches to a region at the first update to that region. When
      the mappings are correct, this difference does not matter.
    
    * A process can always update globals that are not in a replicated region.
    
    * Use $VIEW("JNLPOOL") to determine the state of the current Journal Pool. $VIEW("JNLPOOL") returns
      the replication instance file name for the current Journal Pool and an empty string when there is no
      Journal Pool. Note that the current Journal Pool may not be associated with the last global accessed
      by an extended reference.

---
+++ v63003/inref/gtm8182.m
@@ -0,0 +1,102 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;								;
+; Copyright (c) 2018 YottaDB LLC. and/or its subsidiaries.	;
+; All rights reserved.						;
+;								;
+;	This source code contains the intellectual property	;
+;	of its copyright holder(s), and is made available	;
+;	under a license.  If you do not know the terms of	;
+;	the license, please stop and do not read further.	;
+;								;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;
+
+gtm8182
+	DO getPaths
+
+	WRITE "    Update the INST1 DB",!
+	SET ^jake="youngest"
+	WRITE "    Update the INST3 DB with an extended reference",!
+	SET ^|INST3gbldir|zack="oldest"
+
+	WRITE "    Switch to INST3 gbldir",!
+	SET $ZGBLDIR=INST3gbldir
+
+	WRITE "    Update the INST3 DB",!
+	SET ^jim="father"
+	WRITE "    Update the INST1 DB with an extended reference",!
+	SET ^|INST1gbldir|wendy="mother"
+
+	WRITE "    Switch back to INST1 gbldir",!
+	SET $ZGBLDIR=INST1gbldir
+
+	WRITE !
+
+	quit
+
+TP1
+	DO getPaths
+
+	TSTART
+	WRITE "    Update the INST1 DB",!
+	SET ^mike="uncle"
+	TCOMMIT
+
+	TSTART
+	WRITE "    Update the INST1 DB",!
+	SET ^edd="uncle"
+	WRITE "    Update the INST3 DB with an extended reference",!
+	SET ^|INST3gbldir|grace="grandmother"
+	TCOMMIT
+
+	quit
+
+TP2
+	DO getPaths
+
+	TSTART
+	WRITE "    Update the INST1 DB",!
+	SET ^edd="uncle"
+	WRITE "    Update the INST3 DB with an extended reference",!
+	SET ^|INST3gbldir|harley="cousing"
+	TCOMMIT
+
+	quit
+
+testPool
+	DO getPaths
+
+	; A variable in the DB  must be accessed to open the JNL POOL for the process
+	SET jake=^jake
+	SET ^jake=jake
+	WRITE "    JNL POOL INST1: ",!
+	WRITE "    ",$VIEW("JNLPOOL"),!
+	WRITE !
+
+	WRITE "    Switch to INST3 gbldir and update DB",!,!
+	SET $ZGBLDIR=INST3gbldir
+
+	; A variable in the DB  must be accessed to open the JNL POOL for the process
+	SET jim=^jim
+	SET ^jim=jim
+	WRITE "    JNL POOL INST3: ",!
+	WRITE "    ",$VIEW("JNLPOOL"),!
+	WRITE !
+
+	WRITE "    Update the INST1 DB with an extended reference ",!
+	SET jake=^|INST1gbldir|jake
+	SET ^|INST1gbldir|jake=jake
+	WRITE "    JNL POOL INST3: ",!
+	WRITE "    ",$VIEW("JNLPOOL"),!
+
+	quit
+
+getPaths
+
+	SET INST1path=$ZTRNLNM("path_INST1","","","","","VALUE")
+	SET INST1gbldir=INST1path_"/mumps.gld"
+
+	SET INST3path=$ZTRNLNM("path_INST3","","","","","VALUE")
+	SET INST3gbldir=INST3path_"/mumps.gld"
+
+	quit
