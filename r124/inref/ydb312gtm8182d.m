2346fe9a9411e4d98c8b055b71f5c06312fd3461
Jake Rezac <jake@yottadb.com>
2018-07-20 14:17:07 -0400

    New r124/ydb312_gtm8182d subtest (tests YottaDB/YottaDB#312 in r1.24)

---
+++ r124/inref/ydb312gtm8182d.m
@@ -0,0 +1,30 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;								;
+; Copyright (c) 2018 YottaDB LLC. and/or its subsidiaries.	;
+; All rights reserved.						;
+;								;
+;	This source code contains the intellectual property	;
+;	of its copyright holder(s), and is made available	;
+;	under a license.  If you do not know the terms of	;
+;	the license, please stop and do not read further.	;
+;								;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;
+
+gtm8182
+	DO getPaths
+
+	SET $ZGBLDIR=INST1gbldir
+	SET x=$get(^a)
+	; ZSYSTEM "$ydb_dist/mupip replic -source -start -log=source.log -buf=1 -instsecondary=DUMMY -passive -updok"
+	ZSYSTEM "$MSR STARTSRC INST1 INST2"
+	SET ^a=1
+
+	quit
+
+getPaths
+
+	SET INST1path=$ZTRNLNM("path_INST1","","","","","VALUE")
+	SET INST1gbldir=INST1path_"/mumps.gld"
+
+	quit
