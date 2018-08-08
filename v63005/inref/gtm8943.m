06bd479cddcc97190f3fa37786c8f08640505257
Vinay Ekambaram <vinay@yottadb.com>
2018-08-08 12:29:17 -0400

    New v63005/gtm8943 subtest (Tests GTM-8943 in V63005)

---
+++ v63005/inref/gtm8943.m
@@ -0,0 +1,26 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;								;
+; Copyright (c) 2018 YottaDB LLC. and/or its subsidiaries.	;
+; All rights reserved.						;
+;								;
+;	This source code contains the intellectual property	;
+;	of its copyright holder(s), and is made available	;
+;	under a license.  If you do not know the terms of	;
+;	the license, please stop and do not read further.	;
+;								;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;
+gtm8943
+	write "Entering Call In M Function",!
+	do ^sstep
+	do child1
+	quit
+
+child1
+	do child2
+	quit
+
+child2
+	ZGOTO 0
+	write "ZGOTO 0 failed",!
+	quit
