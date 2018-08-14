bc00302701aa5e4e81eef64efaf5081c47076961
Narayanan Iyer <nars@yottadb.com>
2018-08-14 17:40:43 -0400

    [#341] New r124/ydb341 subtest (tests YottaDB/YottaDB#341) (#375)

---
+++ r124/inref/ydb341.m
@@ -0,0 +1,36 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;								;
+; Copyright (c) 2018 YottaDB LLC. and/or its subsidiaries.	;
+; All rights reserved.						;
+;								;
+;	This source code contains the intellectual property	;
+;	of its copyright holder(s), and is made available	;
+;	under a license.  If you do not know the terms of	;
+;	the license, please stop and do not read further.	;
+;								;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;
+	set jrisave=$$FUNC^%HD($$^%PEEKBYNAME("sgmnt_addrs.gvstats_rec.n_jrec_epoch_idle","DEFAULT"))
+	write "Perform update SET ^x=1",!
+        set ^x=1
+	write "Sleep for 8 seconds to ensure an idle epoch gets written",!
+	; "hang 8" below takes into account 1 second for flush timer + 5 seconds for idle epoch timer + 2 seconds for buffer
+	hang 8
+	write "Confirm an idle epoch did get written : "
+	set jridelta=$$FUNC^%HD($$^%PEEKBYNAME("sgmnt_addrs.gvstats_rec.n_jrec_epoch_idle","DEFAULT"))-jrisave
+	write "JRI gvstat increased by ",(jridelta>0),!
+	write "Perform update SET ^x=2",!
+        set ^x=2
+	set jresave=$$FUNC^%HD($$^%PEEKBYNAME("sgmnt_addrs.gvstats_rec.n_jrec_epoch_regular","DEFAULT"))
+	write "Sleep for anywhere from 1 to 5 seconds",!
+	hang 1+$random(5)
+	write "Perform update SET ^x=3",!
+        set ^x=3
+	write "Sleep for 1 second",!
+	hang 1
+	write "Perform update SET ^x=4",!
+        set ^x=4
+	write "Confirm a regular epoch got written in between updates ^x=2 and ^x=4 : "
+	set jredelta=$$FUNC^%HD($$^%PEEKBYNAME("sgmnt_addrs.gvstats_rec.n_jrec_epoch_regular","DEFAULT"))-jresave
+	write "JRE gvstat increased by ",(jredelta>0),!
+        quit
--- r124/instream.csh
+++ r124/instream.csh
@@ -31,6 +31,7 @@
 # ydb315           [jake]  Tests that the ZCOMPILE operation will not display warning if $ZCOMPILE contains "-nowarnings"
 # ydb324           [nars]  Tests that Error inside indirection usage in direct mode using $ETRAP (not $ZTRAP) does not terminate process
 # ydb321           [nars]  Tests that journal records fed to external filters include timestamps
+# ydb341           [nars]  Tests that epoch_interval setting is honored even if an idle epoch is written
 #----------------------------------------------------------------------------------------------------------------------------------------------------------
 
 echo "r124 test starts..."
@@ -39,7 +40,7 @@ echo "r124 test starts..."
 setenv subtest_list_common     ""
 setenv subtest_list_non_replic ""
 setenv subtest_list_non_replic "$subtest_list_non_replic readonly ydb275socketpass ydb280socketwait jnlunxpcterr ydb297 ydb315"
-setenv subtest_list_non_replic "$subtest_list_non_replic ydb324"
+setenv subtest_list_non_replic "$subtest_list_non_replic ydb324 ydb341"
 setenv subtest_list_replic     ""
 setenv subtest_list_replic     "$subtest_list_replic ydb282srcsrvrerr ydb293 ydb312_gtm8182a ydb312_gtm8182b  ydb312_gtm8182c"
 setenv subtest_list_replic     "$subtest_list_replic ydb312_gtm8182d ydb312_gtm8182e ydb312_gtm8182f ydb312_gtm8182g ydb321"
