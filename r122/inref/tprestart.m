311caa6e26bc1c1631a37e33627b2db0e6107c07
Narayanan Iyer <nars@yottadb.com>
2018-08-29 11:28:55 -0400

    Fix occasional r122/tprestart test failure by making it tn-based instead of time-based (!394)
    We had a failure of this subtest on a slow box with the following diff.
    
    > No TPRESTART messages in the syslog from this test run. The test is not serving its purpose. Signal failure.
    
    In this case, in the 10 seconds that the subtest waited for 5 mumps processes to do TP updates,
    only 5 TSTART/TCOMMIT updates happened. This was not enough to create the contention needed to
    get any TPRESTART messages in the syslog.
    
    The test has now been reworked to be tn-based. That is, each child waits for at least 100 TP
    transactions to be done before being ready to terminate. Once all 5 children are done 100 TP
    transactions, all children will terminate. This should be enough to reliably produce TPRESTART
    messages that this test cares about. On faster systems, this test will take less than 10 seconds
    and on slower systems this test might sometimes take more than 10 seconds.
    
    Also ran the revised test with r120 (the version which did not have the particular code fix this
    test is testing) and verified it saw TPRESTART messages where global names did not match the
    mapped db file name that was reported. So the revised test does serve its purpose too.

--- r122/inref/tprestart.m
+++ r122/inref/tprestart.m
@@ -11,19 +11,18 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 tprestart	;
-	set ^stop=0,jmaxwait=0			; signal child processes to proceed
-	do ^job("child^tprestart",5,"""""")	; start 5 jobs
-	hang 10					; let child run for 10 seconds
-	set ^stop=1				; signal child processes to stop
-	do wait^job				; wait for child processes to die
+	set ^stop=0,^njobs=5		 	 ; signal child processes to proceed
+	do ^job("child^tprestart",^njobs,"""""") ; start 5 jobs
         quit
 
 child  ;
-	for i=1:1  quit:^stop=1  do
+	for i=1:1  quit:^stop=^njobs  do
 	.	tstart ():serial
 	.	set x=$incr(^c)
 	.	if $r(2) set ^a($j,x)=$j(1,220)
 	.	if $r(2) set ^b($j,x)=$j(2,220)
 	.	if $r(2) set ^c($j,x)=$j(3,220)
 	.	tcommit
+	.	if i=100 set x=$incr(^stop)	; once this child has done 100 TP transactions, signal it is okay to stop
+	.					; as we expect at least some of those TP transactions to have seen a TPRESTART
 	quit
