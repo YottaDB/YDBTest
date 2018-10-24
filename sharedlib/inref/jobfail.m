93e1108b58e67008f6f4cdb80bf98f612877f42a
Narayanan Iyer <nars@yottadb.com>
2018-10-24 09:35:16 -0400

    Fix rare sharedlib/gtm7905 subtest failure with TEST-E-LSOF error

--- sharedlib/inref/jobfail.m
+++ sharedlib/inref/jobfail.m
@@ -1,6 +1,9 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;								;
-;	Copyright 2013 Fidelity Information Services, Inc	;
+; Copyright 2013 Fidelity Information Services, Inc		;
+;								;
+; Copyright (c) 2018 YottaDB LLC. and/or its subsidiaries.	;
+; All rights reserved.						;
 ;								;
 ;	This source code contains the intellectual property	;
 ;	of its copyright holder(s), and is made available	;
@@ -11,6 +14,12 @@
 jobfail;
 	view "JOBPID":1
 	job label^jobfail:(output="op":error="err")
+	; $zroutines would have been set to "jobfail.so" by caller to test that stdout/stderr are not modified
+	; by middle process created during JOB command execution.
+	; But we need to wait for jobbed off process to finish before returning (to avoid TEST-E-LSOF errors from test framework)
+	; so reset $zroutines to what it should have been (derive it from the "ydb_routines" env var) and wait for job to die.
+	set $zroutines=$ztrnlnm("ydb_routines")
+	do ^waitforproctodie($zjob,300)
 	quit
 label
 	quit
