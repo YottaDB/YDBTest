5bb5c9bcd0e4285271f3bbdf577bed3d76c4548b
Narayanan Iyer <nars@yottadb.com>
2019-02-06 09:36:38 -0500

    [#417] Enhance r124/ydb394 subtest to better test https://gitlab.com/YottaDB/DB/YDB/issues/417

--- r124/inref/ydb394.c
+++ r124/inref/ydb394.c
@@ -17,6 +17,24 @@
 
 #define BASEVAR "basevar"
 #define SUBSCR 	"1"
+#define	LEN_USED_RANDOM_VALUE	10
+
+void	check_ret_value(char *fn_name, int status, ydb_buffer_t *ret_value)
+{
+	if (YDB_ERR_NODEEND == status)
+		printf("%s() returned YDB_ERR_NODEEND as expected\n", fn_name);
+	else
+		printf("%s() returned the wrong value: %d : Expecting YDB_ERR_NODEEND (%d) \n", fn_name, status, YDB_ERR_NODEEND);
+	if (LEN_USED_RANDOM_VALUE == ret_value->len_used)
+		printf("%s() left ret_value->len_used untouched as expected\n", fn_name);
+	else
+	{
+		ret_value->buf_addr[ret_value->len_used] = '\0';
+		printf("%s() incorrectly modified ret_value->len_used : Actual = %d : Expected = %d : ret_value->buf_addr = : %s\n",
+				fn_name, ret_value->len_used, LEN_USED_RANDOM_VALUE, ret_value->buf_addr);
+	}
+	fflush(stdout);
+}
 
 int main(void)
 {
@@ -28,111 +46,23 @@ int main(void)
 	YDB_LITERAL_TO_BUFFER(SUBSCR, &subscr);
 
 	ret_value.buf_addr = &retvaluebuff[0];
-	ret_value.len_used = 0;
+	ret_value.len_used = LEN_USED_RANDOM_VALUE;	/* a random value */
 	ret_value.len_alloc = sizeof(retvaluebuff);
 
-	printf("\n# Test that ydb_subscript_next_s() with nonexistent local variable with 1 subscript returns an empty string\n"); fflush(stdout);
+	printf("\n# Test that ydb_subscript_next_s() with nonexistent local variable with 1 subscript returns an empty string\n");
 	status = ydb_subscript_next_s(&basevar, 1, &subscr, &ret_value);
-	if (YDB_OK != status)
-	{
-		ret_value.buf_addr[ret_value.len_used] = '\0';
-		if (ret_value.len_used == 0)
-		{
-			printf("ydb_subscript_next_s() returned an empty string\n");
-			fflush(stdout);
-		} else
-		{
-			printf("ydb_subscript_next_s() returned the wrong value: %s\n", ret_value.buf_addr);
-			fflush(stdout);
-		}
+	check_ret_value("ydb_subscript_next_s", status, &ret_value);
 
-		if (YDB_ERR_NODEEND == status)
-		{
-			printf("ydb_subscript_next_s() returned YDB_ERR_NODEEND\n");
-			fflush(stdout);
-		} else
-		{
-			printf("ydb_subscript_next_s() returned the wrong value: %d\n", status);
-			fflush(stdout);
-		}
-	}
-
-	printf("\n# Test that ydb_subscript_previous_s() with nonexistent local variable with 1 subscript returns an empty string\n"); fflush(stdout);
+	printf("\n# Test that ydb_subscript_previous_s() with nonexistent local variable with 1 subscript returns an empty string\n");
 	status = ydb_subscript_previous_s(&basevar, 1, &subscr, &ret_value);
-	if (YDB_OK != status)
-	{
-		ret_value.buf_addr[ret_value.len_used] = '\0';
-		if (ret_value.len_used == 0)
-		{
-			printf("ydb_subscript_previous_s() returned an empty string\n");
-			fflush(stdout);
-		} else
-		{
-			printf("ydb_subscript_previous_s() returned the wrong value: %s\n", ret_value.buf_addr);
-			fflush(stdout);
-		}
+	check_ret_value("ydb_subscript_previous_s", status, &ret_value);
 
-		if (YDB_ERR_NODEEND == status)
-		{
-			printf("ydb_subscript_previous_s() returned YDB_ERR_NODEEND\n");
-			fflush(stdout);
-		} else
-		{
-			printf("ydb_subscript_previous_s() returned the wrong value: %d\n", status);
-			fflush(stdout);
-		}
-	}
-
-	printf("\n# Test that ydb_subscript_next_s() with nonexistent local variable with 0 subscripts returns an empty string\n"); fflush(stdout);
+	printf("\n# Test that ydb_subscript_next_s() with nonexistent local variable with 0 subscripts returns an empty string\n");
 	status = ydb_subscript_next_s(&basevar, 0, NULL, &ret_value);
-	if (YDB_OK != status)
-	{
-		ret_value.buf_addr[ret_value.len_used] = '\0';
-		if (ret_value.len_used == 0)
-		{
-			printf("ydb_subscript_next_s() returned an empty string\n");
-			fflush(stdout);
-		} else
-		{
-			printf("ydb_subscript_next_s() returned the wrong value: %s\n", ret_value.buf_addr);
-			fflush(stdout);
-		}
+	check_ret_value("ydb_subscript_next_s", status, &ret_value);
 
-		if (YDB_ERR_NODEEND == status)
-		{
-			printf("ydb_subscript_next_s() returned YDB_ERR_NODEEND\n");
-			fflush(stdout);
-		} else
-		{
-			printf("ydb_subscript_next_s() returned the wrong value: %d\n", status);
-			fflush(stdout);
-		}
-	}
-
-	printf("\n# Test that ydb_subscript_previous_s() with nonexistent local variable with 0 subscripts returns an empty string\n"); fflush(stdout);
+	printf("\n# Test that ydb_subscript_previous_s() with nonexistent local variable with 0 subscripts returns an empty string\n");
 	status = ydb_subscript_previous_s(&basevar, 0, NULL, &ret_value);
-	if (YDB_OK != status)
-	{
-		ret_value.buf_addr[ret_value.len_used] = '\0';
-		if (ret_value.len_used == 0)
-		{
-			printf("ydb_subscript_previous_s() returned an empty string\n");
-			fflush(stdout);
-		} else
-		{
-			printf("ydb_subscript_previous_s() returned the wrong value: %s\n", ret_value.buf_addr);
-			fflush(stdout);
-		}
-
-		if (YDB_ERR_NODEEND == status)
-		{
-			printf("ydb_subscript_previous_s() returned YDB_ERR_NODEEND\n");
-			fflush(stdout);
-		} else
-		{
-			printf("ydb_subscript_previous_s() returned the wrong value: %d\n", status);
-			fflush(stdout);
-		}
-	}
+	check_ret_value("ydb_subscript_previous_s", status, &ret_value);
 	return YDB_OK;
 }
