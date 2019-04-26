a7c86ef622bb4a864dd6c345b4b3e10f2eb4cc9f
Narayanan Iyer <nars@yottadb.com>
2019-04-26 09:29:16 -0400

    [DEBIAN10] Fix triggers/trigzsy test failure due to "date" utility output format change depending on C vs UTF8 locale
    See prior commit for details on the "date" issue.
    The triggers/trigzsy subtest had a trigger that did a ZSYSTEM "date" which ended up showing
    differently depending on whether the test ran in M vs UTF-8 mode. To avoid non-deterministic output
    the trigger has been changed to do "date" with a specific format that way the same output
    shows up in M or UTF-8 mode (i.e. C or UTF-8 locales).

--- triggers/inref/trigzsy.m
+++ triggers/inref/trigzsy.m
@@ -1,3 +1,16 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;								;
+; Copyright (c) 2019 YottaDB LLC and/or its subsidiaries.	;
+; All rights reserved.						;
+;								;
+;	This source code contains the intellectual property	;
+;	of its copyright holder(s), and is made available	;
+;	under a license.  If you do not know the terms of	;
+;	the license, please stop and do not read further.	;
+;								;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; This module is derived from FIS GT.M.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 	;Testing zsystem from inside triggers, technically this is a no no
 	;see the trigzsyxplode test for more information
 trigzsy
@@ -13,7 +26,7 @@ trigzsy
 basic	write "Test zsystem from inside a trigger",!
 	set x=$ztrigger("item","+^a -command=S -xecute=""w ?10,$r,$c(61),$ztva,! zsy $ztva """)
 	set x=$ztrigger("item","+^b -command=S -xecute=""w ?10,$r,$c(61),$ztva,! zsy $ztva """)
-	set (^a,^b)="date"
+	set (^a,^b)="date +%d-%b-%Y_%H:%M:%S"
 	quit
 
 nest
