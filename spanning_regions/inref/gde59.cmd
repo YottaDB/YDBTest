707582db8e8b2fd39d02308ce123ed4b56fd2c39
Narayanan Iyer <nars@yottadb.com>
2019-05-29 15:38:29 -0400

    [V63007] Fix spanning_regions/collationtests and spanning_regions/gde subtest failure due to GTM-9065
    Because GDE now treats canonic numeric string subscripts as numeric subscripts,
    there were a few places in collationtests.gde where a NAMRANGEOVERLAP error now
    occurs. For example, $ZCHAR(50), formerly a string "1" subscript, is now the
    numeric subscript 1. Which confuses GDE when used in a range specification (:).
    This is now fixed by using $ZCHAR(47) and $ZCHAR(46) (which do not translate to
    a numeric subscript) instead of $ZCHAR(50) and $ZCHAR(49) respectively.
    
    Similar fixes were needed in the spanning_regions/gde subtest.

--- spanning_regions/inref/gde59.cmd
+++ spanning_regions/inref/gde59.cmd
@@ -1,8 +1,22 @@
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!								!
+! Copyright (c) 2019 YottaDB LLC and/or its subsidiaries.	!
+! All rights reserved.						!
+!								!
+!	This source code contains the intellectual property	!
+!	of its copyright holder(s), and is made available	!
+!	under a license.  If you do not know the terms of	!
+!	the license, please stop and do not read further.	!
+!								!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+! This module is derived from FIS GT.M.
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
 ! Test case that Kishore came up with which exposed a bad assert
-add -name PRODAGE("10":"20") -region=DECADE1
-add -name PRODAGE("20":) -region=DECADE2
+add -name PRODAGE("a10":"a20") -region=DECADE1
+add -name PRODAGE("a20":) -region=DECADE2
 add -name PRODAGE(20:) -r=D2
-delete -name PRODAGE("10":"20")
+delete -name PRODAGE("a10":"a20")
 add -name PRODAGE(20:) -r=D2
 exit
 add -region D2 -dyn=D2 -stdnullcoll
--- spanning_regions/inref/gde63.cmd
+++ spanning_regions/inref/gde63.cmd
@@ -1,8 +1,22 @@
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!								!
+! Copyright (c) 2019 YottaDB LLC and/or its subsidiaries.	!
+! All rights reserved.						!
+!								!
+!	This source code contains the intellectual property	!
+!	of its copyright holder(s), and is made available	!
+!	under a license.  If you do not know the terms of	!
+!	the license, please stop and do not read further.	!
+!								!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+! This module is derived from FIS GT.M.
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
 ! Slight enhancement of gde62.cmd
-add -name PRODAGE("10":"20") -region=DECADE1
-add -name PRODAGE("30":) -region=DECADE3
+add -name PRODAGE("a10":"a20") -region=DECADE1
+add -name PRODAGE("a30":) -region=DECADE3
 add -name PRODAGE(20:) -r=D2
-delete -name PRODAGE("10":"20")
+delete -name PRODAGE("a10":"a20")
 add -name PRODAGE(20:) -r=D2
 exit
 add -region D2 -dyn=D2 -stdnullcoll
