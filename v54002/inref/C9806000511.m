849c917ebc40812bf6962b0fea6dd2ee1cf778d7
Narayanan Iyer <nars@yottadb.com>
2019-05-30 13:06:35 -0400

    [V63007] Fix v54002/C9806000511 subtest failure due to $ZCSTATUS value changing from 1 to 150372730 (ERR_ERRORSUMMARY) due to compilation errors (GTM-9047)

--- v54002/inref/C9806000511.m
+++ v54002/inref/C9806000511.m
@@ -3,6 +3,9 @@
 ; Copyright (c) 2010-2015 Fidelity National Information 	;
 ; Services, Inc. and/or its subsidiaries. All rights reserved.	;
 ;								;
+; Copyright (c) 2019 YottaDB LLC and/or its subsidiaries.	;
+; All rights reserved.						;
+;								;
 ;	This source code contains the intellectual property	;
 ;	of its copyright holder(s), and is made available	;
 ;	under a license.  If you do not know the terms of	;
@@ -39,7 +42,7 @@
   set c=0
   for i=1:1  use in read x use $p do
   . ; Exclude some ISVs from comparison, as their values will be different
-  . quit:x?1"$"1(1"STORAGE",1"Y",1"ZALLOCSTOR",1"ZPOSITION",1"ZUSEDSTOR",1"ZREALSTOR",.1"Z"1"HOROLOG",1"ZUT",1"ZKEY")1"=".E
+  . quit:x?1"$"1(1"STORAGE",1"Y",1"ZALLOCSTOR",1"ZCSTATUS",1"ZPOSITION",1"ZUSEDSTOR",1"ZREALSTOR",.1"Z"1"HOROLOG",1"ZUT",1"ZKEY")1"=".E
   . ; Compare ISV to previous value. If incorrec, issue an error
   . do:x'=isvlist("I",i)
   . . write "-------------------",!,x,!," vs.",!,isvlist("I",i),!,"-------------------",!
