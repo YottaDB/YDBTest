a4bc9fc71756f468f20ec675a0039d303cbc9194
Brad Westhafer <brad@yottadb.com>
2020-05-04 18:36:15 -0400

    [YDB#567] Address r130/ydb567 test failure due to timing on 1-CPU machines
    On single-CPU machines, the r130/ydb567 test can fail due to other processes running on the machine which can cause it to hang for longer than twice the sum of the hangtimes. This commit fixes this by disabling the timing part of the test on single CPU machines.

--- r130/inref/ydb567.m
+++ r130/inref/ydb567.m
@@ -10,24 +10,27 @@
 ;								;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 test    ;
-        job child
-        for i=1:1 quit:$zsigproc($zjob,"SIGUSR1")  hang 0.001
+	job child
+	for i=1:1 quit:$zsigproc($zjob,"SIGUSR1")  hang 0.001
 	do ^waitforproctodie($zjob,300)
-        quit
+	quit
 
 child   ;
-        set $zinterrupt="if $increment(intrptcnt)"
-	set starttime=$zut
-        for i=1:1:10000  set hangtime(i)=$select(i#2:0.0001,1:0.001) hang hangtime(i)
-	set endtime=$zut
-	set totalhangtime=0
-	for i=1:1:10000 set totalhangtime=totalhangtime+hangtime(i)
-	set totaltime=endtime-starttime
-	set totaltime=totaltime/1000000 ; convert from nanoseconds to seconds
-	set maxtime=totalhangtime*2
-	if totaltime>maxtime do
-		. write "Hang test failed. The test took ",totaltime
-		. write " seconds which was more than twice the total hangtime of ",totalhangtime,!
+	set singlecpu=$ztrnlnm("gtm_test_singlecpu")
+	set $zinterrupt="if $increment(intrptcnt)"
+	set:(0=singlecpu) starttime=$zut
+	for i=1:1:10000  set hangtime(i)=$select(i#2:0.0001,1:0.001) hang hangtime(i)
+	if 0=singlecpu do
+	.	set endtime=$zut
+	.	set totalhangtime=0
+	.	for i=1:1:10000 set totalhangtime=totalhangtime+hangtime(i)
+	.	set totaltime=endtime-starttime
+	.	set totaltime=totaltime/1000000 ; convert from nanoseconds to seconds
+	.	set maxtime=totalhangtime*2
+	.	if totaltime>maxtime do
+	.	.	write "Hang test failed. The test took ",totaltime
+	.	.	write " seconds which was more than twice the total hangtime of ",totalhangtime,!
+	.	else  write "Hang test finished successfully",!
 	else  write "Hang test finished successfully",!
-        quit
+	quit
 
