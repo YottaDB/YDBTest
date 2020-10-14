442ed2cb31284e076cb18928f6166e2a8ab5f7c4
Joshua Nelson <joshua@yottadb.com>
2020-10-14 15:25:34 -0400

    [YottaDB/DB/YDB!863] Update YDBTest for new version of `verify_commit.sh` in YDB
    - Add `needs_copyright.sh`
    - Change `pre-commit` to use `needs_copyright.sh`
    - Change `.gitlab-ci.yml` to use the new name `verify_commit.sh` instead of `verify_gpg_commit.sh`
    - Only run the `commit_verify` pipeline for MRs, not for individual
    commits
    
      If this is not done, the environment variables for `TARGET_BRANCH`
      will not be set and there will be nothing to compare the HEAD commit
      to: the script will not know how much of the history is pre-existing and
      how much was added for the current MR.
    
    Note that `reference_extensions` cannot be removed from `pre-commit`
    because it's also used to tell which files shouldn't have whitespace
    removed.
    
    This should not be merged until after YDB!863 lands, or it will not be
    able to find the new copyright script.

---
+++ com/needs_copyright.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+#################################################################
+#								#
+# Copyright (c) 2020 YottaDB LLC and/or its subsidiaries.	#
+# All rights reserved.						#
+#								#
+#	This source code contains the intellectual property	#
+#	of its copyright holder(s), and is made available	#
+#	under a license.  If you do not know the terms of	#
+#	the license, please stop and do not read further.	#
+#								#
+#################################################################
+
+# Determines whether a file should need a copyright by its name
+# Returns 0 if it needs a copyright and 1 otherwise.
+# Returns 2 if an error occurs.
+set -eu
+
+if ! [ $# = 1 ]; then
+	echo "usage: $0 <filename>"
+	exit 2
+fi
+
+file="$1"
+
+skipextensions="txt out dat key crt cfg inp"	# List of extensions that cannot have copyrights.
+if echo "$skipextensions" | grep -q -w "$(echo "$file" | awk -F . '{print $NF}')"; then
+	exit 1
+fi
+
+# Below is a list of specific files that do not have a copyright so ignore them
+skiplist="COPYING"
+for skipfile in $skiplist; do
+	if [ $file = $skipfile ]; then
+		exit 1
+	fi
+done
