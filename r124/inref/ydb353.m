f00b8d55d83a34d1f60a2d5fae20b149025966c5
Narayanan Iyer <nars@yottadb.com>
2021-04-05 18:22:34 -0400

    [YDB#711] Enhance r124/ydb353 subtest to test NOISOLATION and $INCREMENT inside TP; Misc enhancements
    * This enhanced test fails reliably with the following symptoms if run without the YDB#711 code fixes.
    
      ```diff
      --- ydb353/ydb353.diff ---
      23,27c23,27
      < ^x(1)=5000
      < ^x(2)=5000
      < ^x(3)=5000
      < ^x(4)=5000
      < ^x(5)=5000
      ---
      > ^x(1)=1
      > ^x(2)=1
      > ^x(3)=""
      > ^x(4)=1
      > ^x(5)=1
      31a32,41
      > r124_0/ydb353/child_ydb353.mjo1
      > TEST-E-FAIL : ^x(1) : Actual = 1 : Expected = 3
      > r124_0/ydb353/child_ydb353.mjo2
      > TEST-E-FAIL : ^x(2) : Actual = 1 : Expected = 3
      > r124_0/ydb353/child_ydb353.mjo3
      > TEST-E-FAIL : ^x(3) : Actual = 1 : Expected = 2
      > r124_0/ydb353/child_ydb353.mjo4
      > TEST-E-FAIL : ^x(4) : Actual = 1 : Expected = 5
      > r124_0/ydb353/child_ydb353.mjo5
      > TEST-E-FAIL : ^x(5) : Actual = 1 : Expected = 2
      ```
    
      And passes reliably with the YDB#711 code fixes. So is a good enough test of YDB#711.
    
    * This test is also enhanced to check that `^x` is always a number and never `""`. This is because we
      had some failures with the below signature before the YDB#711 code fixes.
    
      ```diff
      --- ydb353/ydb353.diff ---
      14c14
      < ##DIFFERENT EXPRESSION##^\^x=[0-9]*$## VS ##^x=""##
      ---
      > ^x=""
      ```
    
    * This test is also enhanced to check if a `$get(^x(jobindex))` returns an unexpected value as that
      is used to then determine the new value to set `^x(jobindex)` to. This helps identify a failure right
      when it happens as opposed to seeing it at a later stage (when a `$INCREMENT(^x(jobindex))` is later
      attempted and the return from the `$INCREMENT` is not expected due to `^x(jobindex)` being set to the
      wrong value in the first place. Note that this is a very rare failure symptom we are currently seeing
      (approximately 1 in 1000 failures) on `ARMV7L` boxes (will be investigated/fixed in a future commit
      in the YDB repo).

--- r124/inref/ydb353.m
+++ r124/inref/ydb353.m
@@ -1,6 +1,6 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;								;
-; Copyright (c) 2018 YottaDB LLC and/or its subsidiaries.	;
+; Copyright (c) 2018-2021 YottaDB LLC and/or its subsidiaries.	;
 ; All rights reserved.						;
 ;								;
 ;	This source code contains the intellectual property	;
@@ -24,12 +24,26 @@ ydb353	;
 	quit
 
 child	;
+	new i,istp,postincr,incr2args,actual,expect
 	if ^cmdline="NOISOLATION" view "NOISOLATION":"^x"
 	for i=1:1:^max do
-	. if i#2'=0 set ^x(jobindex)=$get(^x(jobindex))_"abcdefgh"
+	. if i#2'=0 do
+	. . set actual=$get(^x(jobindex)),expect=$select(i=1:"",1:(i-1)/2)
+	. . if (actual'=expect) write "TEST-E-FAIL : $get(^x(",jobindex,")) : Actual = ",$zwrite(actual)," : Expected = ",$zwrite(expect),! zshow "*" halt
+	. . set ^x(jobindex)=actual_"abcdefgh"
 	. else      do
-	. . if $incr(^x(jobindex))
+	. . ; Test that $INCREMENT works inside and outside of TP hence the $random call below
+	. . set istp=$random(2)
+	. . tstart:istp ():(serial:transaction="BATCH")
+	. . ; Test $INCREMENT with 1 argument and 2 arguments. Both produce same result hence the $random call below.
+	. . set incr2args=$random(2)
+	. . set postincr=$select(0=incr2args:$increment(^x(jobindex)),1:$increment(^x(jobindex),1))
+	. . tcommit:istp
+	. . if (postincr'=(i/2)) write "TEST-E-FAIL : $increment(^x(",jobindex,")) : Actual = ",$zwrite(postincr)," : Expected = ",i/2,! zshow "*" halt
 	. . tstart ():(serial:transaction="BATCH")	; use BATCH to avoid test slowdown due to jnl hardening for every TCOMMIT
-	. . if $incr(^x)
+	. . set postincr=$increment(^x)
 	. . tcommit
+	. . ; Validate that $INCREMENT(^x) is always a number even if ^x is concurrently updated by multiple processes
+	. . ; even though it has NOISOLATION turned ON (it was seen to be the empty string before the YDB#711 fixes).
+	. . if (postincr'=+postincr) write "TEST-E-FAIL : $increment(^x) : Actual = ",$zwrite(postincr)," : Expected = Integer",! zshow "*" halt
 	quit
