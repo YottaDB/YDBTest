de721645a405e3de9999a10ea84aad27ddce9b9a
Jon Badiali <jon@yottadb.com>
2021-11-23 13:12:17 -0500

    Corrected typo in outstream.m by changing label name from 'outsream' to 'outstream'
    Also, added copyright notice to outstream.m.

--- com/outstream.m
+++ com/outstream.m
@@ -1,4 +1,15 @@
-outsream()
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;								;
+; Copyright (c) 2021 YottaDB LLC and/or its subsidiaries.	;
+; All rights reserved.						;
+;								;
+;	This source code contains the intellectual property	;
+;	of its copyright holder(s), and is made available	;
+;	under a license.  If you do not know the terms of	;
+;	the license, please stop and do not read further.	;
+;								;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+outstream()
 	;
 	; Filter for output files (outstream.log and subtest.log) using GT.M.
 	;
