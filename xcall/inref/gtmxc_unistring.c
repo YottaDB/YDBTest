3a77bff1a04f6d1538e44d50ea9f55fc4ce0c639
Narayanan Iyer <nars@yottadb.com>
2021-12-03 15:33:17 -0500

    [YDB#734] Fix buffer overflow in xcall/basic subtest (in xcall/inref/gtmxc_unistring.c)
    * The `wp` variable was an array of wide characters that was allocated using `malloc` for a length
      equal to the string length of the input. This was later passed to the `mbstowcs()` function.
      But no space was allocated for the terminating null wide character. This caused a buffer overflow.
    
    * Now fixed by allocating 1 more wide character to hold the null terminator wide character.
    
    * Similar fixes were needed in multiple places for variables `w1` and `w2` too.

--- xcall/inref/gtmxc_unistring.c
+++ xcall/inref/gtmxc_unistring.c
@@ -1,3 +1,17 @@
+/****************************************************************
+ *								*
+ * Copyright (c) 2021 YottaDB LLC and/or its subsidiaries.	*
+ * All rights reserved.						*
+ *								*
+ *	This source code contains the intellectual property	*
+ *	of its copyright holder(s), and is made available	*
+ *	under a license.  If you do not know the terms of	*
+ *	the license, please stop and do not read further.	*
+ *								*
+ ****************************************************************/
+/* This module is derived from FIS GT.M.
+ ****************************************************************/
+
 #include "shrenv.h"
 #include "gtm_stdio.h"
 #include "gtm_stdlib.h"
@@ -5,7 +19,6 @@
 #include "gtmxc_types.h"
 #include <string.h>
 #include <locale.h>
-
 
 void
 xc_wcslen( int count,
@@ -18,7 +31,7 @@ xc_wcslen( int count,
 
 	setlocale(LC_ALL, "");
 	c = *in_data;
-	n = strlen(*in_data) * sizeof(wchar_t);
+	n = (strlen(*in_data) + 1) * sizeof(wchar_t);
 	wp = (wchar_t *)malloc(n);
 	len = mbstowcs(wp, *in_data, n);
 	len = wcslen(wp);
@@ -36,16 +49,15 @@ void xc_wcscat	(int count,
 	int n1,n2,len;
 	setlocale(LC_ALL, "");
 
-	n1 = strlen(*in_data1) * sizeof(wchar_t);
+	n1 = (strlen(*in_data1) + 1) * sizeof(wchar_t);
 	w1 = (wchar_t *)malloc(n1);
-	len = mbstowcs(w1,*in_data1,n1);	
-	n2 = strlen(*in_data2) * sizeof(wchar_t);
+	len = mbstowcs(w1,*in_data1,n1);
+	n2 = (strlen(*in_data2) + 1) * sizeof(wchar_t);
 	w2 = (wchar_t *)malloc(n2+n1);
-	len = mbstowcs(w2,*in_data2,n2);	
+	len = mbstowcs(w2,*in_data2,n2);
 	wcscat(w2,w1);
         (void)SPRINTF(*out_data,"%ls",w2);
 	fflush(stdout);
-	
 }
 
 void xc_wcscpy	(int count,
@@ -55,13 +67,13 @@ void xc_wcscpy	(int count,
 	wchar_t *w1,*w2;
 	int n,len;
 	setlocale(LC_ALL, "");
-	
-	n = strlen(*in_data) * sizeof(wchar_t);
+
+	n = (strlen(*in_data) + 1) * sizeof(wchar_t);
 	w1 = (wchar_t *)malloc(n);
 	w2 = (wchar_t *)malloc(n);
 	len = mbstowcs(w1,*in_data,n);
 
 	wcscpy(w2,w1);
-	(void)SPRINTF(*out_data,"%ls",w2);	
+	(void)SPRINTF(*out_data,"%ls",w2);
 	fflush(stdout);
 }
