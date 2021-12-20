YottaDB/YDBTest

commit 976bab31472a5fd9cc8b7e84e66768a11ef6213b

[YDB#734] Disable r130/ydb534 and expect/gtm4661 subtests if ASAN is enabled (hopefully temporarily)
* When YottaDB was built with ASAN and tests were run with the PRO builds, the 2 tests mentioned
  in the commit message title failed with weird errors and core files.

* The errors from the address sanitizer in the case of those failures were one of the following types.

  ```
  ==43174==ERROR: AddressSanitizer: stack-buffer-overflow on address 0x7f7ee39cc870 at pc 0x7f7ee44e5373 bp 0x7f7ee39cc260 sp 0x7f7ee39cc250
  ==28802==ERROR: AddressSanitizer: stack-use-after-scope on address 0x7fc5fb236880 at pc 0x7fc5fbf46327 bp 0x7fc5fb235550 sp 0x7fc5fb235540
  ==42544==ERROR: AddressSanitizer: SEGV on unknown address 0x7f97d85ed0e4 (pc 0x7f952d1718da bp 0x7f952db42630 sp 0x7f952db51b80 T0)
  ```

* One of the errors had a stack trace that seemed like a YottaDB code issue.

  ```
  =================================================================
  ==41843==ERROR: AddressSanitizer: stack-use-after-scope on address 0x7f06e0a9f880 at pc 0x7f06e17af327 bp 0x7f06e0a9e550 sp 0x7f06e0a9e540
  READ of size 4 at 0x7f06e0a9f880 thread T0
      #0 0x7f06e17af326 in util_format sr_unix/util_output.c:491
      #1 0x7f06e17b0b08 in util_out_print_vaparm sr_unix/util_output.c:800
      #2 0x7f06e167a83e in gtm_putmsg_list sr_unix/gtm_putmsg_list.c:122
      #3 0x7f06e1678d22 in gtm_putmsg_csa sr_unix/gtm_putmsg.c:71
      #4 0x7f06e1d96922 in jobexam_signal_handler sr_unix/jobexam_signal_handler.c
      #5 0x7f06e10bcd9f  (/usr/lib64/libc.so.6+0x3cd9f)
      #6 0x7f06e1b2b70e in setzdir sr_port/setzdir.c:32
      #7 0x7f06e1e40f5d in parse_file sr_unix/parse_file.c:121
      #8 0x7f06e1e40e04 in parse_file sr_unix/parse_file.c:109
      #9 0x7f06e1e1f37d in op_fnzparse sr_unix/op_fnzparse.c:134
      #10 0x7f06e19b1420 in jobexam_dump sr_port/jobexam_process.c:202
      #11 0x7f06e19b09a8 in jobexam_process sr_port/jobexam_process.c:146

r130/instream.csh
@@ -71,14 +71,7 @@ else
 endif
 
 # Use $subtest_exclude_list to remove subtests that are to be disabled on a particular host or OS
-setenv subtest_exclude_list	""
-if ("pro" == "$tst_image") then
-	setenv subtest_exclude_list "$subtest_exclude_list "
-else
-	# ydb534 subtest generates a SIGABRT signal which causes an assert in `generic_signal_handler.c` assert(SIGABRT != sig)`
-	# to fail hence this subtest needs to be skipped in dbg (i.e. run it only in pro).
-	setenv subtest_exclude_list "$subtest_exclude_list ydb534"
-endif
+setenv subtest_exclude_list	" "
 if ($gtm_platform_size != 64) then
 	## Disable ydb518 on non-64-bit machines
 	setenv subtest_exclude_list "$subtest_exclude_list ydb518"
@@ -94,6 +87,26 @@ if ("armv6l" == `uname -m`) then
 	# in the system and it starts using swap. But not sure what happens at that point, the system becomes
 	# unreachable even though gigabytes of swap space has been configured. So disable this on ARMV6L.
 	setenv subtest_exclude_list "$subtest_exclude_list ydb547"
+endif
+
+setenv subtest_exclude_list    ""
+
+if ("pro" == "$tst_image") then
+	source $gtm_tst/com/is_libyottadb_asan_enabled.csh
+	if ($gtm_test_libyottadb_asan_enabled) then
+		# We see cores when ASAN is used with tests that send signals (ydb534 sends SIGABRT etc.)
+		# This happens whether YottaDB is compiled with gcc or clang.
+		# And the stack traces are inside ASAN code where a SIG-11 occurs as well.
+		# There is an indication of stack smash error too in some cases.
+		# Not yet sure if it is an ASAN issue or a YottaDB issue inside the signal handler.
+		# Exclude this subtest until we can find time to investigate this further.
+		# The same test runs fine without ASAN and so is enabled in that case.
+		setenv subtest_exclude_list "$subtest_exclude_list ydb534"
+	endif
+else
+	# ydb534 subtest generates a SIGABRT signal which causes an assert in `generic_signal_handler.c` assert(SIGABRT != sig)`
+	# to fail hence this subtest needs to be skipped in dbg (i.e. run it only in pro).
+	setenv subtest_exclude_list "$subtest_exclude_list ydb534"
 endif
 
 # Submit the list of subtests

expect/instream.csh
@@ -4,7 +4,7 @@
 # Copyright (c) 2012-2016 Fidelity National Information		#
 # Services, Inc. and/or its subsidiaries. All rights reserved.	#
 #                                                               #
-# Copyright (c) 2017 YottaDB LLC and/or its subsidiaries.	#
+# Copyright (c) 2017-2021 YottaDB LLC and/or its subsidiaries.	#
 # All rights reserved.						#
 #								#
 #	This source code contains the intellectual property	#
@@ -57,9 +57,15 @@ else
 	setenv subtest_list "$subtest_list_common $subtest_list_non_replic"
 endif
 
-# thunder has syslog rate limiting issues. Until those are resolved, disable this test
-if ("thunder" == "$HOST:ar") then
-        setenv subtest_exclude_list "$subtest_exclude_list gtm4661"
+source $gtm_tst/com/is_libyottadb_asan_enabled.csh
+if ($gtm_test_libyottadb_asan_enabled) then
+	# We see cores when ASAN is used with tests that send signals (gtm4661 sends SIGTERM etc.)
+	# This happens whether YottaDB is compiled with gcc or clang.
+	# And the stack traces are inside ASAN code where a SIG-11 occurs as well.
+	# Not yet sure if it is an ASAN issue or a YottaDB issue inside the signal handler.
+	# Exclude this subtest until we can find time to investigate this further.
+	# The same test runs fine without ASAN and so is enabled in that case.
+	setenv subtest_exclude_list "$subtest_exclude_list gtm4661"
 endif
 
 # filter out white box tests that cannot run in pro

expect/u_inref/gtm4661.csh
@@ -15,15 +15,6 @@
 #################################################################
 # gtm4661a 	[bahirs] 	GT.M process does not stuck if terminated with SIGTERM signal.
 
-if ($?ASAN_OPTIONS) then
-	# ASAN_OPTIONS env var is defined. In that case, "abort_on_error=1" would have most likely been set
-	# by the test framework. For reasons not clear, having that flag in this test causes a SIG-11.
-	# We suspect it is an ASAN (address sanitizer) issue and so work around it by turning that off.
-	# Note that "abort_on_error=0" only turns off producing a core file in case an issue is detected
-	# by the sanitizer. It will still report it and cause a test failure.
-	setenv ASAN_OPTIONS "${ASAN_OPTIONS}:abort_on_error=0"
-endif
-
 $gtm_tst/com/dbcreate.csh mumps 1
 setenv TERM xterm
 # Process does not stuck up after receiving TERM signal while running in background.

commit f455e1d58be301795c03df9a6e5a2f970f2f2eff

[YDB#734] Fix r130/ydb607 subtest failure on AARCH64 due to a regression
* The `r130/ydb607` subtest failed on AARCH64 during in-house testing. That subtest was never
  supposed to run on that platform since they don't have prior GT.M versions.

* The reason it ran is a regression introduced in 976bab31472a5fd9cc8b7e84e66768a11ef6213b.
  As part of that commit, the env var `subtest_exclude_list` was set to the empty string in two
  places. The second setting is obviously incorrect because that would basically clear all the
  subtests which got added to this env var until then (`ydb607` was one such subtest).

* This is now fixed by removing the second set of the env var to the empty string.

r130/instream.csh
@@ -71,7 +71,8 @@ else
 endif
 
 # Use $subtest_exclude_list to remove subtests that are to be disabled on a particular host or OS
-setenv subtest_exclude_list	" "
+setenv subtest_exclude_list	""
+
 if ($gtm_platform_size != 64) then
 	## Disable ydb518 on non-64-bit machines
 	setenv subtest_exclude_list "$subtest_exclude_list ydb518"
@@ -88,8 +89,6 @@ if ("armv6l" == `uname -m`) then
 	# unreachable even though gigabytes of swap space has been configured. So disable this on ARMV6L.
 	setenv subtest_exclude_list "$subtest_exclude_list ydb547"
 endif
-
-setenv subtest_exclude_list    ""
 
 if ("pro" == "$tst_image") then
 	source $gtm_tst/com/is_libyottadb_asan_enabled.csh