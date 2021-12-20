YottaDB/YDBTest

commit 7a261dc55b4620f630200ef34e20b9939e327ec8

[YDB#734] Fix multi-host imptp test failures where local host YottaDB is not built with ASAN but remote host is
* In-house testing showed the following failure in a multi-host test that used imptp. On the remote side,
  imptp.csh would be invoked which tried to build a C version of the imptp application and failed
  during link time.

  ```diff
  --- switch_over/switch_over.diff ---
  70a71
  > TEST-E-MULTISITE replic action failed.Pls. check msr_execute_##FILTERED##NO.csh and all logs related to ##FILTERED## ##TIMESTAMP##
  96a98,102
  > remotehost:msreplic_E_1_4/switch_over/instance2/endtp.out
  > $ZSTATUS="150372994,wait+9^job,%YDB-E-GVUNDEF, Global variable undefined: ^%jobwait(0,""njobs"")"
  > $ZSTATUS="150372994,wait+9^job,%YDB-E-GVUNDEF, Global variable undefined: ^%jobwait(0,""njobs"")"
  > remotehost:msreplic_E_1_4/switch_over/instance2/imptp.out
  > LVNSET-E-LINKFAIL : Linking simpleapi_imptp failed. See simpleapi_imptp.map for details
  ```

  And the link map file had the following error.

  ```sh
  $ cat simpleapi_imptp.map
  .
  .
  /usr/bin/ld: libyottadb.so: undefined reference to `__asan_stack_free_7'
  .
  .
  ```

* This happens because `libyottadb.so` on the remote host was built with ASAN support but the local host
  was not built with ASAN support and therefore the remote host inherited the `gt_ld_options_common` env
  var from the local host which did not have the needed `-fsanitize=address` linker flags.

* This is fixed by moving the code in `com/gtmtest.csh` that set `gt_ld_options_common` (and a few other
  related env vars) to a new file `com/set_asan_other_env_vars.csh`.

  And this new file is now invoked from the following 2 files in the test framework which are usually
  invoked on the remote side by various tests.
  - com/imptp.csh
  - com/remote_getenv.csh

com/remote_getenv.csh
@@ -66,15 +66,12 @@ source $gtm_tst/com/set_specific.csh
 # this will take care of gtmroutines and locale set up defintions along with other regular setups
 source $gtm_tst/com/getenv.csh
 
-# Check if the YottaDB build has ASAN enabled on the remote side of a multi-host test.
-# If so, need to set ASAN_OPTIONS env var on the remote side to disable leak check (LSAN) as that can cause failures.
-# The below logic is similar to that in com/gtm_env_missing_csh (executed on the primary/local side for all tests).
+# Check if the YottaDB build has ASAN enabled on the remote side of a multi-host test. If so, need to set
+# ASAN_OPTIONS env var (among other things) on the remote side to disable leak check (LSAN) as that can cause failures.
 # Note: Do this BEFORE the "source $gtm_tst/com/set_gtm_machtype.csh" step as that invokes YottaDB and would otherwise
 # fail (due to memory leaks identified) in case ASAN is enabled in the current build.
 source $gtm_tst/com/is_libyottadb_asan_enabled.csh
-if ($gtm_test_libyottadb_asan_enabled) then
-	source $gtm_tst/com/set_asan_options_env_var.csh
-endif
+source $gtm_tst/com/set_asan_other_env_vars.csh	# sets a few other associated asan env vars
 
 # override machtype environment from primary
 source $gtm_tst/com/set_gtm_machtype.csh

com/gtmtest.csh
@@ -111,28 +111,9 @@ source $gtm_test_com_individual/defaults.csh ${TMP_FILE_PREFIX}_defaults_csh
 if ($status) exit $status
 
 # The above "source" invocation would have defined "gt_ld_options_common" and "gtm_tst" env vars.
-# Now add address sanitizer related flags to the former (if YottaDB build being tested was linked with it).
-# This way any "$gt_ld_options_common" usages in the tests automatically linked with libasan.
-# Not doing so would cause tests that use "gt_ld_options_common" to fail as follows.
-#	ASan runtime does not come first in initial library list; you should either link runtime to your application or manually preload it with LD_PRELOAD
+# The below "sourc" invocation would update "gt_ld_options_common" with "-fsanitize=address" if needed.
 source $gtm_tst/com/is_libyottadb_asan_enabled.csh
-if ($gtm_test_libyottadb_asan_enabled) then
-	setenv gt_ld_options_common "$gt_ld_options_common -fsanitize=address"
-	source $gtm_tst/com/set_asan_options_env_var.csh
-	# If ASAN checks are happening, it is more likely for it to detect memory issues if YottaDB uses the system malloc/free.
-	# Therefore, disable the YottaDB memory manager by setting gtmdbglvl to the below specific value. Do this randomly
-	# so we also test ASAN with the YottaDB memory manager, not just with the system memory manager. Note that this env
-	# var is modified in various tests to specific values (using "setenv gtmdbglvl" and "unsetenv gtmdbglvl"). Those places
-	# can be enhanced to set gtmdbglvl to this value. For example,
-	# - A "unsetenv gtmdbglvl" can be replaced by a "setenv gtmdbglvl 0x80000000"
-	# - A "setenv gtmdbglvl 0x80" can be replaced by a "setenv gtmdbglvl 0x80000080"
-	# The places that might benefit from this is suspected to be a very small fraction of the test system and hence it is
-	# not considered worth the effort right now. That is left as a future activity if/when it becomes useful enough.
-	set enable_system_malloc_free = `shuf -i 0-1 -n 1`
-	if ($enable_system_malloc_free) then
-		source $gtm_tst/com/set_gtmdbglvl_to_use_system_malloc_free.csh
-	endif
-endif
+source $gtm_tst/com/set_asan_other_env_vars.csh	# sets a few other associated asan env vars
 
 source $gtm_tst/com/set_ldlibpath.csh
 # Various checks and exits :

com/imptp.csh
@@ -85,6 +85,9 @@ if ($gtm_test_dbfill == "IMPTP" || $gtm_test_dbfill == "IMPZTP") then
 			# Wait for Rust stable to support ASAN before enabling it. Until then do not choose rust
 			# for imptp if YottaDB build has ASAN enabled.
 			source $gtm_tst/com/is_libyottadb_asan_enabled.csh
+			# Need to run this because it is possible for "imptp.csh" to be run on a remote host in
+			# a multi-host test run where the local host does not have asan enabled but the remote host has.
+			source $gtm_tst/com/set_asan_other_env_vars.csh	# sets a few other associated asan env vars
 			if ((true == $rust_supported) && ! $gtm_test_libyottadb_asan_enabled) then
 				set rand = 5
 			else if ($gtm_test_libyottadb_asan_enabled) then

com/set_asan_other_env_vars.csh
@@ -1,0 +1,47 @@
+#################################################################
+#								#
+# Copyright (c) 2021 YottaDB LLC and/or its subsidiaries.	#
+# All rights reserved.						#
+#								#
+#	This source code contains the intellectual property	#
+#	of its copyright holder(s), and is made available	#
+#	under a license.  If you do not know the terms of	#
+#	the license, please stop and do not read further.	#
+#								#
+#################################################################
+
+if ($gtm_test_libyottadb_asan_enabled) then
+	if (! $?gtm_test_asan_env_vars_set_done) then
+		setenv gtm_test_asan_env_vars_set_done 0
+	endif
+	if ($gtm_test_asan_env_vars_set_done) then
+		# Below env vars were already set by a previous invocation of this script in the same process.
+		# So skip setting it again. This is particularly useful with "gt_ld_options_common" as we do not
+		# want to add "-fsanitize=address" multiple times. Just exit.
+		exit
+	endif
+	if ($?gt_ld_options_common) then
+		# gt_ld_options_common env var is defined. Add -fsanitize=address to it.
+		# Add address sanitizer related flags to the former (if YottaDB build being tested was linked with it).
+		# This way any "$gt_ld_options_common" usages in the tests automatically linked with libasan.
+		# Not doing so would cause tests that use "gt_ld_options_common" to fail as follows.
+		#	ASan runtime does not come first in initial library list; you should either link runtime to your application or manually preload it with LD_PRELOAD
+		setenv gt_ld_options_common "$gt_ld_options_common -fsanitize=address"
+	endif
+	source $gtm_tst/com/set_asan_options_env_var.csh
+	# If ASAN checks are happening, it is more likely for it to detect memory issues if YottaDB uses the system malloc/free.
+	# Therefore, disable the YottaDB memory manager by setting gtmdbglvl to the below specific value. Do this randomly
+	# so we also test ASAN with the YottaDB memory manager, not just with the system memory manager. Note that this env
+	# var is modified in various tests to specific values (using "setenv gtmdbglvl" and "unsetenv gtmdbglvl"). Those places
+	# can be enhanced to set gtmdbglvl to this value. For example,
+	# - A "unsetenv gtmdbglvl" can be replaced by a "setenv gtmdbglvl 0x80000000"
+	# - A "setenv gtmdbglvl 0x80" can be replaced by a "setenv gtmdbglvl 0x80000080"
+	# The places that might benefit from this is suspected to be a very small fraction of the test system and hence it is
+	# not considered worth the effort right now. That is left as a future activity if/when it becomes useful enough.
+	set enable_system_malloc_free = `shuf -i 0-1 -n 1`
+	if ($enable_system_malloc_free) then
+		source $gtm_tst/com/set_gtmdbglvl_to_use_system_malloc_free.csh
+	endif
+	setenv gtm_test_asan_env_vars_set_done 1
+endif
+

commit b59d539627a881531667db45a746c0e12c49033f

[YDB#734] Fix multi-host test failures where local and remote host YottaDB have different ASAN build settings
* Multi-host tests where the remote side had a build of YottaDB with ASAN enabled failed with the
  following diff.

  ```diff
  > =================================================================
  > ==60463==ERROR: LeakSanitizer: detected memory leaks
  ```

* This is a regression introduced by 7a261dc55b4620f630200ef34e20b9939e327ec8. As part of that commit,
  `com/remote_getenv.csh` was reworked to no longer invoke `set_asan_options_env_var.csh` but instead
  invoke `set_asan_other_env_vars.csh`. But the latter had an optimization that caused it to return
  without doing anything if the env var `gtm_test_asan_env_vars_set_done` was set to 1. While this
  optimization was okay to do in a local environment, it was not appropriate in the remote side of
  a multi-host test where this env var got automatically inherited on to the remote side from the local
  side (all env vars of the form `gtm_test_*` get inherited). While `gtm_test_libyottadb_asan_enabled`
  env var got recomputed on the remote side, the dependent `gtm_test_asan_env_vars_set_done` got
  inherited from the local side which made the 2 variables out of sync and in turn caused the
  `set_asan_other_env_vars.csh` script to incorrectly return prematurely when invoked on the remote side.

* This meant that `ASAN_OPTIONS` env var did not get set appropriately which later caused the test
  failure due to extra lines of memory leak output from the `LeakSanitizer`.

* This is now fixed by not invoking `set_asan_other_env_vars.csh` as even if we disable the optimization,
  the script does more things than is needed in `com/remote_getenv.csh` (no need to set `gtmdbglvl` etc.).
  And instead we just set `ASAN_OPTIONS` env var using the `set_asan_options_env_var.csh` script.

* At least for now, the `set_asan_other_env_vars.csh` call in `com/remote_getenv.csh` is considered not

com/remote_getenv.csh
@@ -71,7 +71,16 @@ source $gtm_tst/com/getenv.csh
 # Note: Do this BEFORE the "source $gtm_tst/com/set_gtm_machtype.csh" step as that invokes YottaDB and would otherwise
 # fail (due to memory leaks identified) in case ASAN is enabled in the current build.
 source $gtm_tst/com/is_libyottadb_asan_enabled.csh
-source $gtm_tst/com/set_asan_other_env_vars.csh	# sets a few other associated asan env vars
+# Note: We would normally invoke the following more general script but it relies on an env var "gtm_test_asan_env_vars_set_done"
+# that would have been inherited on the remote side (this side) from the local side. Since YottaDB builds on the local and remote
+# side could have been independently built with or without ASAN, the env var on the local side does not make sense in the remote
+# side. Therefore we skip invoking this script.
+#	set source $gtm_tst/com/set_asan_other_env_vars.csh	# sets a few other associated asan env vars
+# And instead invoke exactly what we need (which is the "ASAN_OPTIONS" env var being set based on the newly computed value of
+# "$gtm_test_libyottadb_asan_enabled" env var) from inside that generic script. Those commands are pasted below.
+if ($gtm_test_libyottadb_asan_enabled) then
+	source $gtm_tst/com/set_asan_options_env_var.csh
+endif
 
 # override machtype environment from primary
 source $gtm_tst/com/set_gtm_machtype.csh