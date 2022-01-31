YottaDB/YDBTest

commit 963d37526b89112a9b4ef5088134b5f8b0a2e1ed

Exclude r130/ydb534 subtest on Ubuntu 21.10 due to what we suspect is a Ubuntu regression
In Ubuntu 21.10, a call to abort(), instead of just sending a signal-6 to the process also started
creating an unrequested core dump even though the signal was handled. This then created two core
files (the handling of the sig-6 intentionally created a core using sig-3) which the test did not
expect. This caused a test failure. The simplest solution is to bypass this subtest on Ubuntu 21.10
and hoping 22.04 holds a fix. This change is only looking explicitly for Ubuntu 21.10 so this
subtest will start running again when subsequent versions become available.

com/set_gtm_machtype.csh
@@ -4,7 +4,7 @@
 # Copyright (c) 2008-2016 Fidelity National Information		#
 # Services, Inc. and/or its subsidiaries. All rights reserved.	#
 #								#
-# Copyright (c) 2017-2021 YottaDB LLC and/or its subsidiaries.	#
+# Copyright (c) 2017-2022 YottaDB LLC and/or its subsidiaries.	#
 # All rights reserved.						#
 #								#
 #	This source code contains the intellectual property	#
@@ -111,10 +111,13 @@ setenv gtm_endian `echo -n A | od -h | awk '{if ($2 == "0041") {print "LITTLE_EN
 # on a linux server. It will be set to "" on other servers
 if (-X lsb_release) then
 	setenv gtm_test_linux_distrib `lsb_release -is | sed 's/ //g' | tr '[A-Z]' '[a-z]'`
+	setenv gtm_test_linux_version `lsb_release -sr`
 else if (-f /etc/os-release) then
 	setenv gtm_test_linux_distrib `grep -w ID /etc/os-release | cut -d= -f2 | cut -d'"' -f2`
+	setenv gtm_test_linux_version `grep -w VERSION_ID /etc/os-release | tr -d \" | cut -d= -f2`
 else
 	setenv gtm_test_linux_distrib ""
+	setenv gtm_test_linux_version ""
 endif
 
 # Determine whether this is a single-cpu system or not. This will be used later to disable certain heavyweight tests.

com/outref.awk
@@ -3,7 +3,7 @@
 # Copyright (c) 2002-2016 Fidelity National Information		#
 # Services, Inc. and/or its subsidiaries. All rights reserved.	#
 #								#
-# Copyright (c) 2017-2021 YottaDB LLC and/or its subsidiaries.	#
+# Copyright (c) 2017-2022 YottaDB LLC and/or its subsidiaries.	#
 # All rights reserved.						#
 #								#
 #	This source code contains the intellectual property	#
@@ -346,6 +346,12 @@ BEGIN {
 	option_names[++no_options] = "gtm_test_asan_compiler_clang11or12"
 	if ("1" == ENVIRON["gtm_test_asan_compiler_clang11or12"])
 		envir[no_options] = "ASAN_CLANG_11_OR_12"
+	#
+	if (("ubuntu" == ENVIRON["gtm_test_linux_distrib"]) && ("21.10" == ENVIRON["gtm_test_linux_version"]))
+	{
+		option_names[++no_options] = "ubuntu_21.10"
+		envir[no_options] = "UBUNTU_21.10"
+	}
 	#
 	split(tst_hostos_machtype_all, all_platforms, " ")
 	if ("AIX" == tst_osname)

r130/instream.csh
@@ -1,7 +1,7 @@
 #!/usr/local/bin/tcsh -f
 #################################################################
 #								#
-# Copyright (c) 2019-2021 YottaDB LLC and/or its subsidiaries.	#
+# Copyright (c) 2019-2022 YottaDB LLC and/or its subsidiaries.	#
 # All rights reserved.						#
 #								#
 #	This source code contains the intellectual property	#
@@ -100,6 +100,13 @@ if ("pro" == "$tst_image") then
 		# Not yet sure if it is an ASAN issue or a YottaDB issue inside the signal handler.
 		# Exclude this subtest until we can find time to investigate this further.
 		# The same test runs fine without ASAN and so is enabled in that case.
+		setenv subtest_exclude_list "$subtest_exclude_list ydb534"
+	endif
+	# We have noted that Ubuntu 21.10 has an odd issue where when the code explicitly codes an 'abort()' call,
+	# the system creates a core even if the exception is handled. This causes the ydb534 test to have more
+	# cores than it can deal with so the test fails. Bypass ydb534 on Ubutnu 21.10 and hope when 22.04 comes
+	# around, this issue is fixed.
+	if (("ubuntu" == $gtm_test_linux_distrib) && ("21.10" == $gtm_test_linux_version)) then
 		setenv subtest_exclude_list "$subtest_exclude_list ydb534"
 	endif
 else

commit a62be48544703b303bb91382cb4f39cc973e0f87

Fix com/set_gtm_machtype.csh regression in 963d3752 that caused <Unmatched '"'> error in tcsh

com/set_gtm_machtype.csh
@@ -114,7 +114,7 @@ if (-X lsb_release) then
 	setenv gtm_test_linux_version `lsb_release -sr`
 else if (-f /etc/os-release) then
 	setenv gtm_test_linux_distrib `grep -w ID /etc/os-release | cut -d= -f2 | cut -d'"' -f2`
-	setenv gtm_test_linux_version `grep -w VERSION_ID /etc/os-release | tr -d \" | cut -d= -f2`
+	setenv gtm_test_linux_version `grep -w VERSION_ID /etc/os-release | tr -d '"' | cut -d= -f2`
 else
 	setenv gtm_test_linux_distrib ""
 	setenv gtm_test_linux_version ""