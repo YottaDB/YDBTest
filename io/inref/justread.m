8ccce9335c187cd253e9f8bef7019e03e630af38
Jaahanavee Sikri <jaahanavee@yottadb.com>
2022-06-06 11:34:24 -0400

    [#436] Update ps command used in io/inref/justread.m to fix io/pipetest sub-test failure
    Issue:
    
    io/pipetest fails because ps truncates the username to just 7 characters whereas $user contains the full username. `ps -ef | grep $user` causes the failure and is present in io/inref/justread.m.
    
    Fix:
    
    Replace `ps -ef | grep $user` with `ps -fu $user` in io/inref/justread.m.

--- io/inref/justread.m
+++ io/inref/justread.m
@@ -1,6 +1,18 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;								;
+; Copyright (c) 2022 YottaDB LLC and/or its subsidiaries.	;
+; All rights reserved.						;
+;								;
+;	This source code contains the intellectual property	;
+;	of its copyright holder(s), and is made available	;
+;	under a license.  If you do not know the terms of	;
+;	the license, please stop and do not read further.	;
+;								;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 JUSTREAD
 	set a="test"
-	open a:(comm="ps -ef | grep $user | grep justread | grep -v grep":readonly)::"pipe"
+	open a:(comm="ps -fu $user | grep justread | grep -v grep":readonly)::"pipe"
 	use a
 	for  read x quit:$zeof  use $p write "line= ",x,! use a
 	close a
