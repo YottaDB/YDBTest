a82a3781b32a64a8b5f2fb7f2bf28ab7872de281
Brad Westhafer <brad@yottadb.com>
2022-06-13 20:48:28 -0400

    [YDB#604] [V63013] Fix unicode/functions and rundown_argless/readonly_dbflag failures
    After running the V6.3-013 code and test changes through weekend tests, there were
    several test failures. This commit fixes 2 failures that failed reliably across
    every machine:
    
    * The `unicode/functions` test failed because of `+` characters that were accidentally
      placed before the first line of the copyright comment in the `ztranslate.m` file.
      Once these `+` characters are removed, the test passes reliably.
    * The `rundown_argless/readonly_dbflag` failed because of the new `DBFILERDONLY` error
      which reliably shows up in 5 different `mupip.err` files. The fix for this is to
      update the reference file to expect this. After making this change, the test
      passes reliably.

--- unicode/inref/ztranslate.m
+++ unicode/inref/ztranslate.m
@@ -1,16 +1,16 @@
-+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-+;								;
-+; Copyright (c) 2022 YottaDB LLC and/or its subsidiaries.	;
-+; All rights reserved.						;
-+;								;
-+;	This source code contains the intellectual property	;
-+;	of its copyright holder(s), and is made available	;
-+;	under a license.  If you do not know the terms of	;
-+;	the license, please stop and do not read further.	;
-+;								;
-+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-+; This module is derived from FIS GT.M.
-+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;								;
+; Copyright (c) 2022 YottaDB LLC and/or its subsidiaries.	;
+; All rights reserved.						;
+;								;
+;	This source code contains the intellectual property	;
+;	of its copyright holder(s), and is made available	;
+;	under a license.  If you do not know the terms of	;
+;	the license, please stop and do not read further.	;
+;								;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; This module is derived from FIS GT.M.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ztranslate ;
 		; since we have byte by byte processing below we need to turn BADCHAR off
 		if $VIEW("BADCHAR") do
