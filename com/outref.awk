YottaDB/YDBTest

commit 918b864ee2c726a2ee5699c2aa435247a8e47343

Disable r132/ydb704 test on Arch Linux because of valgrind issues on arch
The commit d92aca31c44ea393a5cb9f1b58882add69572d1c made a change that temporarily fixed
valgrind issues on the in-house Arch Linux machine by setting `DEBUGINFOD_URLS`. This worked
for a few weeks but the valgrind test (`r132/ydb704` has since started failing again even with
the line setting `DEBUGINFOD_URLS`. This failure only happens on Arch and affects every build
on the box. The error looks like the below:

```
> setenv DEBUGINFOD_URLS "https://debuginfod.archlinux.org"
> valgrind -q $ydb_dist/yottadb -version

valgrind:  Fatal error at startup: a function redirection
valgrind:  which is mandatory for this platform-tool combination
valgrind:  cannot be set up.  Details of the redirection are:
valgrind:
valgrind:  A must-be-redirected function
valgrind:  whose name matches the pattern:      strlen
valgrind:  in an object with soname matching:   ld-linux-x86-64.so.2
valgrind:  was not found whilst processing
valgrind:  symbols from the object with soname: ld-linux-x86-64.so.2
valgrind:
valgrind:  Possible fixes: (1, short term): install glibc's debuginfo
valgrind:  package on this machine.  (2, longer term): ask the packagers
valgrind:  for your Linux distribution to please in future ship a non-
valgrind:  stripped ld.so (or whatever the dynamic linker .so is called)
valgrind:  that exports the above-named function using the standard
valgrind:  calling conventions for this platform.  The package you need
valgrind:  to install for fix (1) is called
valgrind:
valgrind:    On Debian, Ubuntu:                 libc6-dbg
valgrind:    On SuSE, openSuSE, Fedora, RHEL:   glibc-debuginfo
valgrind:
valgrind:  Note that if you are debugging a 32 bit process on a
valgrind:  64 bit system, you will need a corresponding 32 bit debuginfo
valgrind:  package (e.g. libc6-dbg:i386).
valgrind:
valgrind:  Cannot continue -- exiting now.  Sorry.
>
```

com/outref.awk
@@ -276,6 +276,16 @@ BEGIN {
 		else if ("ubuntu" == ENVIRON["gtm_test_linux_distrib"])
 			envir[no_options] = "UBUNTU_LINUX_AARCH64"
 	}
+	if ("arch" == ENVIRON["gtm_test_linux_distrib"])
+		envir[no_options] = "ARCH_LINUX"
+	else if ("debian" == ENVIRON["gtm_test_linux_distrib"])
+		envir[no_options] = "DEBIAN_LINUX"
+	else if ("ubuntu" == ENVIRON["gtm_test_linux_distrib"])
+		envir[no_options] = "UBUNTU_LINUX"
+	else if ("centos" == ENVIRON["gtm_test_linux_distrib"])
+		envir[no_options] = "CENTOS_LINUX"
+	else if ("rhel" == ENVIRON["gtm_test_linux_distrib"])
+		envir[no_options] = "RHEL_LINUX"
 	#
 	option_names[++no_options] = "gtm_test_singlecpu"
 	if (1 == ENVIRON["gtm_test_singlecpu"])

r132/instream.csh
@@ -96,6 +96,13 @@ if ("14" == "$clangmajorver") then
 	setenv subtest_exclude_list "$subtest_exclude_list ydb704"
 endif
 
+# Disable ydb704 subtest on Arch Linux because valgrind currently doesn't
+# work on the in-house Arch machine and setting DEBUGINFOD_URLS didn't
+# fix the failures long-term.
+if ("arch" == $gtm_test_linux_distrib) then
+	setenv subtest_exclude_list "$subtest_exclude_list ydb704"
+endif
+
 if ($gtm_test_asan_compiler_clang11or12) then
 	# libyottadb.so was built with ASAN and CLANG 11 or CLANG 12
 	# In this case, the ydb632 subtest fails with the following diff

r132/u_inref/ydb704.csh
@@ -18,12 +18,6 @@
 # file simple.
 source $gtm_tst/com/gtm_test_disable_autorelink.csh
 
-# Set DEBUGINFOD_URLS on Arch so that valgrind will work correctly
-# See https://bbs.archlinux.org/viewtopic.php?id=276422 for more information
-if ("arch" == $gtm_test_linux_distrib) then
-	setenv DEBUGINFOD_URLS "https://debuginfod.archlinux.org"
-endif
-
 echo "# Test invoking YottaDB via valgrind does not produce a %YDB-E-YDBDISTUNVERIF"
 
 $echoline

commit 71b8cdac75f80e541e3a7c98332b45dc1e5523d0

Fix r124 test failure due to regression in 918b864e
* As part of 918b864e a `option_names[++no_options]` line got missed out when defining
  the environment variable for the current OS. This meant it hijacked the preceding
  the previous section in `com/outref.awk` which defined env vars like `DEBIAN_LINUX_X86_64`.
  And this meant that reference files which relied on `DEBIAN_LINUX_X86_64` (like
  `r124/outref/outref.txt`) had issues where subtests which were inside a SUSPEND/ALLOW section
  using those hijacked env vars ended up failing due to the 918b864e change.

* This is now fixed by adding the missed out line which defines the OS env var as a new option
  separate from the previous section that defined a OS_ARCH env var.

com/outref.awk
@@ -257,7 +257,7 @@ BEGIN {
 	#
 	if ("x86_64" == ENVIRON["real_mach_type"])
 	{
-		option_names[++no_options] = "gtm_test_linux_distrib"
+		option_names[++no_options] = "OS_ARCH"
 		if ("arch" == ENVIRON["gtm_test_linux_distrib"])
 			envir[no_options] = "ARCH_LINUX_X86_64"
 		else if ("debian" == ENVIRON["gtm_test_linux_distrib"])
@@ -270,12 +270,13 @@ BEGIN {
 			envir[no_options] = "RHEL_LINUX_X86_64"
 	} else if ("aarch64" == ENVIRON["real_mach_type"])
 	{
-		option_names[++no_options] = "gtm_test_linux_distrib"
+		option_names[++no_options] = "OS_ARCH"
 		if ("debian" == ENVIRON["gtm_test_linux_distrib"])
 			envir[no_options] = "DEBIAN_LINUX_AARCH64"
 		else if ("ubuntu" == ENVIRON["gtm_test_linux_distrib"])
 			envir[no_options] = "UBUNTU_LINUX_AARCH64"
 	}
+	option_names[++no_options] = "OS"
 	if ("arch" == ENVIRON["gtm_test_linux_distrib"])
 		envir[no_options] = "ARCH_LINUX"
 	else if ("debian" == ENVIRON["gtm_test_linux_distrib"])