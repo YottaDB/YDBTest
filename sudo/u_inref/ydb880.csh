e0f129303f3060805dbc506b680faa6161efe647
Jaahanavee Sikri <jaahanavee@yottadb.com>
2022-08-09 17:46:35 -0400

    [#464][YDB#880][YDB#856] New sudo/ydb880 test for ydbinstall/ydbinstall.sh --{link,copy}{exec,env} command line options
    This commit adds a new test `sudo/ydb880` which tests the `--linkexec`, `--linkenv`, `--copyexec`, and `--copyenv` command line options of the `ydbinstall` / `ydbinstall.sh` script.
    
    The following changes have been tested:
    * Changes related to YDB#880
       * `--linkenv` and `--linkexec` options replace existing links; previously existing links were unchanged
       * `--linkenv [dirname]` is a default option with `dirname` defaulting to `/usr/local/etc`. The new `--nolinkenv` option provides the former default behavior.
       * `--linkexec [dirname]` is a default option, with `dirname` defaulting to `/usr/local/bin`. The new `--nolinkexec` option provides the former default behavior.
       * `--copyenv [dirname]` if specified without a directory defaults to `/usr/local/etc`.
       * `--copyexec [dirname]` if specified without a directory defaults to `/usr/local/bin`.
    * Changes related to YDB#856
       * Creation of
         * a symbolic link in `/usr/local/bin` to the `ydb` script in the installation directory;
         * a symbolic link from `/usr/local/bin/gtm` to `./ydb`;
         * symbolic links in `/usr/local/etc` to the `ydb_env_set` and `ydb_env_unset` files in the installation directory; and
         * a symbolic link from `/usr/local/etc/gtmprofile` to `./ydb_env_set`.

---
+++ sudo/u_inref/ydb880.csh
@@ -0,0 +1,55 @@
+#!/usr/local/bin/tcsh
+#################################################################
+#                                                               #
+# Copyright (c) 2022 YottaDB LLC and/or its subsidiaries.       #
+# All rights reserved.                                          #
+#                                                               #
+#       This source code contains the intellectual property     #
+#       of its copyright holder(s), and is made available       #
+#       under a license.  If you do not know the terms of       #
+#       the license, please stop and do not read further.       #
+#                                                               #
+#################################################################
+
+echo "# Tests the ydbinstall/ydbinstall.sh --linkexec, --linkenv, --copyexec,"
+echo "# and --copyenv command line options"
+
+# setup of the image environment
+set curr_dir=`pwd`
+mkdir install # the install destination
+cd install
+# we have to make this folder before hand otherwise the installer will throw out errors
+# this happens only when this script is invoked by the test system
+# it works fine without permission issues when run as root in an interactive terminal
+mkdir gtmsecshrdir
+chmod -R 755 .
+cp $gtm_tst/$tst/u_inref/ydb880.sh  .
+
+mkdir dir1
+mkdir dir2
+
+cd ../
+mkdir tmpbin
+mkdir tmpetc
+
+cd install
+# we pass these things as variables to ydb880.sh because it doesn't inherit the tcsh environment variables
+source $gtm_tst/$tst/u_inref/setinstalloptions.csh      # sets the variable "installoptions" (e.g. "--force-install" if needed)
+$sudostr sh ./ydb880.sh $gtm_verno $tst_image $curr_dir/install/dir1 $curr_dir/install/dir2 $curr_dir/tmpbin $curr_dir/tmpetc "$installoptions"
+
+# run a few yottadb commands to test the install works
+cat >> ../yottadbTest.txt << xx
+write \$zyrel
+write \$zreldate
+set a="Hello,"
+set a(1)="World!"
+zwrite a
+h
+xx
+$curr_dir/install/dir1/yottadb -direct < ../yottadbTest.txt
+
+# clean up the install and temporary directories since the files are owned by root
+cd ..
+sudo rm -rf install
+sudo rm -rf tmpbin
+sudo rm -rf tmpetc
---
+++ sudo/u_inref/ydb880.sh
@@ -0,0 +1,319 @@
+#!/bin/sh
+#################################################################
+# Copyright (c) 2022 YottaDB LLC and/or its subsidiaries.	#
+# All rights reserved.						#
+#								#
+#								#
+#	This source code contains the intellectual property	#
+#	of its copyright holder(s), and is made available	#
+#	under a license.  If you do not know the terms of	#
+#	the license, please stop and do not read further.	#
+#								#
+#################################################################
+
+echo "# Backing up ydb and gtm symlinks under /usr/local/bin (if any)"
+cd /usr/local/bin
+existing_ydb=`readlink ydb`
+existing_gtm=`readlink gtm`
+echo ""
+echo ""
+
+echo "# Backing up ydb_env_set, ydb_env_unset, and gtmprofile symlinks under /usr/local/etc (if any)"
+cd /usr/local/etc
+existing_ydb_env_set=`readlink ydb_env_set`
+existing_ydb_env_unset=`readlink ydb_env_unset`
+existing_gtmprofile=`readlink gtmprofile`
+echo ""
+echo ""
+
+echo "# Installing YottaDB in install/dir1 without --linkexec and --linkenv options"
+/Distrib/YottaDB/$1/$2/yottadb_r*/ydbinstall --installdir $3 --utf8 default --user $USER $7 --nopkg-config --overwrite-existing
+echo ""
+
+cd /usr/local/bin
+current_ydb=`readlink ydb`
+current_gtm=`readlink gtm`
+default_gtm=`readlink -f gtm`
+
+echo "# Current ydb and gtm symlinks under /usr/local/bin point to install/dir1"
+echo $current_ydb
+echo $current_gtm
+echo $default_gtm
+echo ""
+echo ""
+
+cd /usr/local/etc
+current_ydb_env_set=`readlink ydb_env_set`
+current_ydb_env_unset=`readlink ydb_env_unset`
+current_gtmprofile=`readlink gtmprofile`
+default_gtmprofile=`readlink -f gtmprofile`
+
+echo "# Current ydb_env_set, ydb_env_unset, and gtmprofile symlinks under /usr/local/etc point to install/dir1"
+echo $current_ydb_env_set
+echo $current_ydb_env_unset
+echo $current_gtmprofile
+echo $default_gtmprofile
+echo ""
+echo ""
+
+echo "# Installing YottaDB in install/dir2 without --linkexec and --linkenv options"
+/Distrib/YottaDB/$1/$2/yottadb_r*/ydbinstall --installdir $4 --utf8 default --user $USER $7 --nopkg-config --overwrite-existing
+echo ""
+
+cd /usr/local/bin
+current_ydb=`readlink ydb`
+current_gtm=`readlink gtm`
+
+echo "# Current ydb and gtm symlinks under /usr/local/bin now point to install/dir2"
+echo $current_ydb
+echo $current_gtm
+echo ""
+echo ""
+
+cd /usr/local/etc
+current_ydb_env_set=`readlink ydb_env_set`
+current_ydb_env_unset=`readlink ydb_env_unset`
+current_gtmprofile=`readlink gtmprofile`
+
+echo "# Current ydb_env_set, ydb_env_unset, and gtmprofile symlinks under /usr/local/etc now point to install/dir2"
+echo $current_ydb_env_set
+echo $current_ydb_env_unset
+echo $current_gtmprofile
+echo ""
+echo ""
+
+#############################################################
+
+echo "# Deleting current ydb and gtm symlinks to check that --linkexec creates symlinks if none exist"
+cd /usr/local/bin
+sudo rm gtm ydb
+echo ""
+echo ""
+
+echo "# Installing YottaDB in install/dir1 with --linkexec option"
+/Distrib/YottaDB/$1/$2/yottadb_r*/ydbinstall --installdir $3 --linkexec --utf8 default --user $USER $7 --nopkg-config --overwrite-existing
+echo ""
+
+cd /usr/local/bin
+current_ydb=`readlink ydb`
+current_gtm=`readlink gtm`
+
+echo "# ydb and gtm symlinks created under /usr/local/bin point to install/dir1"
+echo $current_ydb
+echo $current_gtm
+echo ""
+echo ""
+
+echo "# Installing YottaDB in install/dir2 with --linkexec option"
+/Distrib/YottaDB/$1/$2/yottadb_r*/ydbinstall --installdir $4 --linkexec --utf8 default --user $USER $7 --nopkg-config --overwrite-existing
+echo ""
+
+current_ydb=`readlink ydb`
+current_gtm=`readlink gtm`
+
+echo "# Current ydb and gtm symlinks under /usr/local/bin now point to install/dir2"
+echo $current_ydb
+echo $current_gtm
+echo ""
+echo ""
+
+echo "# Installing YottaDB in install/dir1 with --nolinkexec option"
+/Distrib/YottaDB/$1/$2/yottadb_r*/ydbinstall --installdir $3 --nolinkexec --utf8 default --user $USER $7 --nopkg-config --overwrite-existing
+echo ""
+
+current_ydb=`readlink ydb`
+current_gtm=`readlink gtm`
+
+echo "# Current ydb and gtm symlinks under /usr/local/bin still point to install/dir2"
+echo $current_ydb
+echo $current_gtm
+echo ""
+echo ""
+
+#############################################################################
+
+echo "# Deleting current ydb_env_set, ydb_env_unset, and gtmprofile symlinks to check that --linkenv creates symlinks if none exist"
+cd /usr/local/etc
+sudo rm ydb_env_set ydb_env_unset gtmprofile
+echo ""
+echo ""
+
+echo "# Installing YottaDB in install/dir2 with --linkenv option"
+/Distrib/YottaDB/$1/$2/yottadb_r*/ydbinstall --installdir $4 --linkenv --utf8 default --user $USER $7 --nopkg-config --overwrite-existing
+echo ""
+
+current_ydb_env_set=`readlink ydb_env_set`
+current_ydb_env_unset=`readlink ydb_env_unset`
+current_gtmprofile=`readlink gtmprofile`
+
+echo "# ydb_env_set, ydb_env_unset, and gtmprofile symlinks created under /usr/local/etc point to install/dir2"
+echo $current_ydb_env_set
+echo $current_ydb_env_unset
+echo $current_gtmprofile
+echo ""
+echo ""
+
+echo "# Installing YottaDB in install/dir1 with --linkenv option"
+/Distrib/YottaDB/$1/$2/yottadb_r*/ydbinstall --installdir $3 --linkenv --utf8 default --user $USER $7 --nopkg-config --overwrite-existing
+echo ""
+
+current_ydb_env_set=`readlink ydb_env_set`
+current_ydb_env_unset=`readlink ydb_env_unset`
+current_gtmprofile=`readlink gtmprofile`
+
+echo "# Current ydb_env_set, ydb_env_unset, and gtmprofile symlinks under /usr/local/etc now point to install/dir1"
+echo $current_ydb_env_set
+echo $current_ydb_env_unset
+echo $current_gtmprofile
+echo ""
+echo ""
+
+echo "# Installing YottaDB in install/dir2 with --nolinkenv option"
+/Distrib/YottaDB/$1/$2/yottadb_r*/ydbinstall --installdir $4 --nolinkenv --utf8 default --user $USER $7 --nopkg-config --overwrite-existing
+echo ""
+
+current_ydb_env_set=`readlink ydb_env_set`
+current_ydb_env_unset=`readlink ydb_env_unset`
+current_gtmprofile=`readlink gtmprofile`
+
+echo "# Current ydb_env_set, ydb_env_unset, and gtmprofile symlinks under /usr/local/etc still point to install/dir1"
+echo $current_ydb_env_set
+echo $current_ydb_env_unset
+echo $current_gtmprofile
+echo ""
+echo ""
+
+#############################################################################
+
+echo "# Installing YottaDB in install/dir1 with --linkexec tmpbin/"
+/Distrib/YottaDB/$1/$2/yottadb_r*/ydbinstall --installdir $3 --linkexec $5 --utf8 default --user $USER $7 --nopkg-config --overwrite-existing
+echo ""
+
+cd $5
+current_ydb=`readlink ydb`
+current_gtm=`readlink gtm`
+
+echo "# Current ydb and gtm symlinks under tmpbin/ point to install/dir1"
+echo $current_ydb
+echo $current_gtm
+echo ""
+echo ""
+
+echo "# Installing YottaDB in install/dir2 with --linkexec tmpbin/"
+/Distrib/YottaDB/$1/$2/yottadb_r*/ydbinstall --installdir $4 --linkexec $5 --utf8 default --user $USER $7 --nopkg-config --overwrite-existing
+echo ""
+
+cd $5
+current_ydb=`readlink ydb`
+current_gtm=`readlink gtm`
+
+echo "# Current ydb and gtm symlinks under tmpbin/ now point to install/dir2"
+echo $current_ydb
+echo $current_gtm
+echo ""
+echo ""
+
+echo "# Installing YottaDB in install/dir1 with --linkenv tmpetc/"
+/Distrib/YottaDB/$1/$2/yottadb_r*/ydbinstall --installdir $3 --linkenv $6 --utf8 default --user $USER $7 --nopkg-config --overwrite-existing
+echo ""
+
+cd $6
+current_ydb_env_set=`readlink ydb_env_set`
+current_ydb_env_unset=`readlink ydb_env_unset`
+current_gtmprofile=`readlink gtmprofile`
+
+echo "# Current ydb_env_set, ydb_env_unset, and gtmprofile symlinks under tmpetc/ point to install/dir1"
+echo $current_ydb_env_set
+echo $current_ydb_env_unset
+echo $current_gtmprofile
+echo ""
+echo ""
+
+echo "# Installing YottaDB in install/dir2 with --linkenv tmpetc/"
+/Distrib/YottaDB/$1/$2/yottadb_r*/ydbinstall --installdir $4 --linkenv $6 --utf8 default --user $USER $7 --nopkg-config --overwrite-existing
+echo ""
+
+cd $6
+current_ydb_env_set=`readlink ydb_env_set`
+current_ydb_env_unset=`readlink ydb_env_unset`
+current_gtmprofile=`readlink gtmprofile`
+
+echo "# Current ydb_env_set, ydb_env_unset, and gtmprofile symlinks under tmpetc/ now point to install/dir2"
+echo $current_ydb_env_set
+echo $current_ydb_env_unset
+echo $current_gtmprofile
+echo ""
+echo ""
+
+############################################################
+
+echo "# Installing YottaDB in install/dir1 with --copyexec option"
+/Distrib/YottaDB/$1/$2/yottadb_r*/ydbinstall --installdir $3 --copyexec --utf8 default --user $USER $7 --nopkg-config --overwrite-existing
+echo ""
+
+cd /usr/local/bin
+ls ydb
+readlink gtm
+echo ""
+echo ""
+
+echo "# Installing YottaDB in install/dir2 with --copyenv option"
+/Distrib/YottaDB/$1/$2/yottadb_r*/ydbinstall --installdir $4 --copyenv --utf8 default --user $USER $7 --nopkg-config --overwrite-existing
+echo ""
+
+cd /usr/local/etc
+ls ydb_env_set
+ls ydb_env_unset
+readlink gtmprofile
+echo ""
+echo ""
+
+echo "# Installing YottaDB in install/dir1 with --copyexec tmpbin/"
+/Distrib/YottaDB/$1/$2/yottadb_r*/ydbinstall --installdir $3 --copyexec $5 --utf8 default --user $USER $7 --nopkg-config --overwrite-existing
+echo ""
+
+cd $5
+ls ydb
+readlink gtm
+echo ""
+echo ""
+
+echo "# Installing YottaDB in install/dir2 with --copyenv tmpetc/"
+/Distrib/YottaDB/$1/$2/yottadb_r*/ydbinstall --installdir $4 --copyenv $6 --utf8 default --user $USER $7 --nopkg-config --overwrite-existing
+echo ""
+
+cd $6
+ls ydb_env_set
+ls ydb_env_unset
+readlink gtmprofile
+echo ""
+echo ""
+
+########################################################
+
+cd /usr/local/bin
+echo "# Restoring ydb and gtm symlinks under /usr/local/bin to their original state"
+sudo rm gtm ydb
+if [ "$existing_ydb" != "" ]; then
+    sudo ln -s $existing_ydb ydb
+fi
+if [ "$existing_gtm" != "" ]; then
+    sudo ln -s $existing_gtm gtm
+fi
+echo ""
+echo ""
+
+cd /usr/local/etc
+echo "# Restoring ydb_env_set, ydb_env_unset, and gtmprofile symlinks under /usr/local/etc to their original state"
+sudo rm ydb_env_set ydb_env_unset gtmprofile
+if [ "$existing_ydb_env_set" != "" ]; then
+    sudo ln -s $existing_ydb_env_set ydb_env_set
+fi
+if [ "$existing_ydb_env_unset" != "" ]; then
+    sudo ln -s $existing_ydb_env_unset ydb_env_unset
+fi
+if [ "$existing_gtmprofile" != "" ]; then
+    sudo ln -s $existing_gtmprofile gtmprofile
+fi
+echo ""
+echo ""
