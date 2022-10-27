5372d9e5568ee3bd506bb96d7abf59bc2eebdf72
Narayanan Iyer <nars@yottadb.com>
2022-10-27 14:05:32 -0400

    [YDB#564] Rework r126/ydb446 subtest now that return value is truncated (no INVSTRLEN error)
    * The `r126/ydb446` subtest was originally written to test YDB#446 which issued a `INVSTRLEN` error
      if a `ydb_string_t *` output parameter return value length exceeded the original length before
      the call-in (`ydb_ci()` etc. call).
    
    * But with YDB#564, the `INVSTRLEN` error is no longer issued. Instead, we copy a truncated return
      value (i.e. we copy only as much as we can safely do without overflowing the buffer).
    
    * This commit reworks the `r126/ydb446` subtest to test the new behavior.

--- r126/inref/ydb446.c
+++ r126/inref/ydb446.c
@@ -1,6 +1,6 @@
 /****************************************************************
  *								*
- * Copyright (c) 2019-2020 YottaDB LLC and/or its subsidiaries.	*
+ * Copyright (c) 2019-2022 YottaDB LLC and/or its subsidiaries.	*
  * All rights reserved.						*
  *								*
  *	This source code contains the intellectual property	*
@@ -15,41 +15,40 @@
 
 int sapi(){
 	int status;
-	char goodBuf[21], badBuf[15], errBuf[1024];
+	char goodBuf[21], badBuf[16];
 	ydb_string_t goodStr, badStr;
 	goodStr.address = goodBuf;
-	goodStr.length = 21;
 	badStr.address = badBuf;
-	badStr.length = 15;
 
 	printf("\nydb_ci() tests\n");
 	printf("ydb_ci() on a properly allocated buffer should return no error\n");
+	goodStr.length = sizeof(goodBuf) - 1;	/* leave space for null terminator byte */
 	status = ydb_ci("retStr", &goodStr);
 	if(YDB_OK != status){
 		printf("FAIL: ydb_ci() did not return the correct status. Got: %d; Expected: %d\n", status, YDB_OK);
 	} else {
 		printf("PASS: ydb_ci() returned YDB_OK\n");
 	}
-	goodStr.address[20] = '\0'; // null terminate for strcmp()
+	goodStr.address[goodStr.length] = '\0'; // null terminate for strcmp()
 	if (0 != strcmp(goodStr.address, "a0a1a2a3a4a5a6a7a8a9")){
 		printf("FAIL: ydb_ci() did not return the correct string. Got: %s; Expected: a0a1a2a3a4a5a6a7a8a9\n", goodStr.address);
 	} else {
 		printf("PASS: ydb_ci() returned the correct string\n");
 	}
 
-	printf("\nydb_ci() on a improperly (too small) buffer; should return INVSTRLEN\n");
+	printf("\nydb_ci() on a improperly (too small) buffer; should return TRUNCATED buffer\n");
+	badStr.length = sizeof(badBuf) - 1;	/* leave space for null terminator byte */
 	status = ydb_ci("retStr", &badStr);
-	ydb_zstatus(errBuf, 1024);
-	if (YDB_ERR_INVSTRLEN != status){
-		printf("FAIL: ydb_ci() did not return the correct status. Got: %d; Expected: %d\n", status, YDB_ERR_INVSTRLEN);
+	if(YDB_OK != status){
+		printf("FAIL: ydb_ci() did not return the correct status. Got: %d; Expected: %d\n", status, YDB_OK);
 	} else {
-		printf("PASS: ydb_ci() returned INVSTRLEN\n");
+		printf("PASS: ydb_ci() returned YDB_OK\n");
 	}
-	if(0 != strcmp(errBuf, "150375522,(Call-In),%YDB-E-INVSTRLEN, Invalid string length 20: max 15")){
-		printf("FAIL: ydb_ci() did not return the correct error message. Got: \"%s\"; Expected: "
-				"\"150375522,(Call-In),%%YDB-E-INVSTRLEN, Invalid string length 20: max 15\"\n", errBuf);
+	badStr.address[badStr.length] = '\0'; // null terminate for strcmp()
+	if (0 != strcmp(badStr.address, "a0a1a2a3a4a5a6a")){
+		printf("FAIL: ydb_ci() did not return the correct string. Got: %s; Expected: a0a1a2a3a4a5a6a\n", badStr.address);
 	} else {
-		printf("PASS: ydb_ci() returned the correct error message\n");
+		printf("PASS: ydb_ci() returned the correct string\n");
 	}
 
 	printf("\n\nydb_cip() tests\n");
@@ -58,78 +57,73 @@ int sapi(){
 	callin.rtn_name.length = 6;
 	callin.handle = NULL;
 	printf("ydb_cip() on a properly allocated buffer should return no error\n");
+	goodStr.length = sizeof(goodBuf) - 1;	/* leave space for null terminator byte */
 	status = ydb_cip(&callin, &goodStr);
 	if(YDB_OK != status){
 		printf("FAIL: ydb_cip() did not return the correct status. Got: %d; Expected: %d\n", status, YDB_OK);
 	} else {
 		printf("PASS: ydb_cip() returned YDB_OK\n");
 	}
-	goodStr.address[20] = '\0'; // null terminate for strcmp()
+	goodStr.address[goodStr.length] = '\0'; // null terminate for strcmp()
 	if (0 != strcmp(goodStr.address, "a0a1a2a3a4a5a6a7a8a9")){
 		printf("FAIL: ydb_cip() did not return the correct string. Got: %s; Expected: a0a1a2a3a4a5a6a7a8a9\n", goodStr.address);
 	} else {
 		printf("PASS: ydb_cip() returned the correct string\n");
 	}
 
-	printf("\nydb_cip() on a improperly (too small) buffer; should return INVSTRLEN\n");
+	printf("\nydb_cip() on a improperly (too small) buffer; should return TRUNCATED buffer\n");
+	badStr.length = sizeof(badBuf) - 1;	/* leave space for null terminator byte */
 	status = ydb_cip(&callin, &badStr);
-	ydb_zstatus(errBuf, 1024);
-	if (YDB_ERR_INVSTRLEN != status){
-		printf("FAIL: ydb_cip() did not return the correct status. Got: %d; Expected: %d\n", status, YDB_ERR_INVSTRLEN);
+	if(YDB_OK != status){
+		printf("FAIL: ydb_cip() did not return the correct status. Got: %d; Expected: %d\n", status, YDB_OK);
 	} else {
-		printf("PASS: ydb_cip() returned INVSTRLEN\n");
+		printf("PASS: ydb_cip() returned YDB_OK\n");
 	}
-	if(0 != strcmp(errBuf, "150375522,(Call-In),%YDB-E-INVSTRLEN, Invalid string length 20: max 15")){
-		printf("FAIL: ydb_ci() did not return the correct error message. Got: \"%s\"; Expected: "
-				"\"150375522,(Call-In),i%%YDB-E-INVSTRLEN, Invalid string length 20: max 15\"\n", errBuf);
+	badStr.address[badStr.length] = '\0'; // null terminate for strcmp()
+	if (0 != strcmp(badStr.address, "a0a1a2a3a4a5a6a")){
+		printf("FAIL: ydb_cip() did not return the correct string. Got: %s; Expected: a0a1a2a3a4a5a6a\n", badStr.address);
 	} else {
-		printf("PASS: ydb_ci() returned the correct error message\n");
+		printf("PASS: ydb_cip() returned the correct string\n");
 	}
 }
 
 int stapi(){
 	int status;
-	char goodBuf[21], badBuf[15], errBuf[1024];
+	char goodBuf[21], badBuf[16];
 	ydb_string_t goodStr, badStr;
-	ydb_buffer_t errstr;
 	goodStr.address = goodBuf;
-	goodStr.length = 21;
 	badStr.address = badBuf;
-	badStr.length = 15;
-	errstr.buf_addr = errBuf;
-	errstr.len_alloc = sizeof(errBuf);
-	errstr.len_used = 0;
 
 	printf("\n\nydb_ci_t() tests\n");
 	printf("ydb_ci_t() on a properly allocated buffer should return no error\n");
-	status = ydb_ci_t(YDB_NOTTP, &errstr, "retStr", &goodStr);
+	goodStr.length = sizeof(goodBuf) - 1;	/* leave space for null terminator byte */
+	status = ydb_ci_t(YDB_NOTTP, NULL, "retStr", &goodStr);
 	if(YDB_OK != status){
 		printf("FAIL: ydb_ci_t() did not return the correct status. Got: %d; Expected: %d\n", status, YDB_OK);
 	} else {
 		printf("PASS: ydb_ci_t() returned YDB_OK\n");
 	}
-	goodStr.address[20] = '\0'; // null terminate for strcmp()
+	goodStr.address[goodStr.length] = '\0'; // null terminate for strcmp()
 	if (0 != strcmp(goodStr.address, "a0a1a2a3a4a5a6a7a8a9")){
 		printf("FAIL: ydb_ci_t() did not return the correct string. Got: %s; Expected: a0a1a2a3a4a5a6a7a8a9\n", goodStr.address);
 	} else {
 		printf("PASS: ydb_ci_t() returned the correct string\n");
 	}
 
-	printf("\nydb_ci_t() on a improperly (too small) buffer; should return INVSTRLEN\n");
-	status = ydb_ci_t(YDB_NOTTP, &errstr, "retStr", &badStr);
-	errstr.buf_addr[errstr.len_used] = '\0';
-	if (YDB_ERR_INVSTRLEN != status){
-		printf("FAIL: ydb_ci_t() did not return the correct status. Got: %d; Expected: %d\n", status, YDB_ERR_INVSTRLEN);
+	printf("\nydb_ci_t() on a improperly (too small) buffer; should return TRUNCATED buffer\n");
+	badStr.length = sizeof(badBuf) - 1;	/* leave space for null terminator byte */
+	status = ydb_ci_t(YDB_NOTTP, NULL, "retStr", &badStr);
+	if(YDB_OK != status){
+		printf("FAIL: ydb_ci_t() did not return the correct status. Got: %d; Expected: %d\n", status, YDB_OK);
 	} else {
-		printf("PASS: ydb_ci_t() returned INVSTRLEN\n");
+		printf("PASS: ydb_ci_t() returned YDB_OK\n");
 	}
-	if(0 != strcmp(errBuf, "150375522,(SimpleThreadAPI),%YDB-E-INVSTRLEN, Invalid string length 20: max 15")){
-		printf("FAIL: ydb_ci_t() did not return the correct error message. Got: \"%s\"; Expected: "
-				"\"150375522,(SimpleThreadAPI),%%YDB-E-INVSTRLEN, Invalid string length 20: max 15\"\n", errBuf);
+	badStr.address[badStr.length] = '\0'; // null terminate for strcmp()
+	if (0 != strcmp(badStr.address, "a0a1a2a3a4a5a6a")){
+		printf("FAIL: ydb_ci_t() did not return the correct string. Got: %s; Expected: a0a1a2a3a4a5a6a\n", badStr.address);
 	} else {
-		printf("PASS: ydb_ci_t() returned the correct error message\n");
+		printf("PASS: ydb_ci_t() returned the correct string\n");
 	}
-
 
 	printf("\n\nydb_cip_t() tests\n");
 	ci_name_descriptor callin;
@@ -137,32 +131,33 @@ int stapi(){
 	callin.rtn_name.length = 6;
 	callin.handle = NULL;
 	printf("ydb_cip_t() on a properly allocated buffer should return no error\n");
-	status = ydb_cip_t(YDB_NOTTP, &errstr, &callin, &goodStr);
+	goodStr.length = sizeof(goodBuf) - 1;	/* leave space for null terminator byte */
+	status = ydb_cip_t(YDB_NOTTP, NULL, &callin, &goodStr);
 	if(YDB_OK != status){
 		printf("FAIL: ydb_cip_t() did not return the correct status. Got: %d; Expected: %d\n", status, YDB_OK);
 	} else {
 		printf("PASS: ydb_cip_t() returned YDB_OK\n");
 	}
-	goodStr.address[20] = '\0'; // null terminate for strcmp()
+	goodStr.address[goodStr.length] = '\0'; // null terminate for strcmp()
 	if (0 != strcmp(goodStr.address, "a0a1a2a3a4a5a6a7a8a9")){
 		printf("FAIL: ydb_cip_t() did not return the correct string. Got: %s; Expected: a0a1a2a3a4a5a6a7a8a9\n", goodStr.address);
 	} else {
 		printf("PASS: ydb_cip_t() returned the correct string\n");
 	}
 
-	printf("\nydb_cip_t() on a improperly (too small) buffer; should return INVSTRLEN\n");
-	status = ydb_cip_t(YDB_NOTTP, &errstr, &callin, &badStr);
-	errstr.buf_addr[errstr.len_used] = '\0';
-	if (YDB_ERR_INVSTRLEN != status){
-		printf("FAIL: ydb_cip_t() did not return the correct status. Got: %d; Expected: %d\n", status, YDB_ERR_INVSTRLEN);
+	printf("\nydb_cip_t() on a improperly (too small) buffer; should return TRUNCATED buffer\n");
+	badStr.length = sizeof(badBuf) - 1;	/* leave space for null terminator byte */
+	status = ydb_cip_t(YDB_NOTTP, NULL, &callin, &badStr);
+	if(YDB_OK != status){
+		printf("FAIL: ydb_cip_t() did not return the correct status. Got: %d; Expected: %d\n", status, YDB_OK);
 	} else {
-		printf("PASS: ydb_cip_t() returned INVSTRLEN\n");
+		printf("PASS: ydb_cip_t() returned YDB_OK\n");
 	}
-	if(0 != strcmp(errBuf, "150375522,(SimpleThreadAPI),%YDB-E-INVSTRLEN, Invalid string length 20: max 15")){
-		printf("FAIL: ydb_cip_t() did not return the correct error message. Got: \"%s\"; Expected: "
-				"\"150375522,(SimpleThreadAPI),%%YDB-E-INVSTRLEN, Invalid string length 20: max 15\"\n", errBuf);
+	badStr.address[badStr.length] = '\0'; // null terminate for strcmp()
+	if (0 != strcmp(badStr.address, "a0a1a2a3a4a5a6a")){
+		printf("FAIL: ydb_cip_t() did not return the correct string. Got: %s; Expected: a0a1a2a3a4a5a6a\n", badStr.address);
 	} else {
-		printf("PASS: ydb_cip_t() returned the correct error message\n");
+		printf("PASS: ydb_cip_t() returned the correct string\n");
 	}
 	return 0;
 }
