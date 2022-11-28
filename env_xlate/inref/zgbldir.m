b6bf0e803ce203a2c1bdf297c7af3f7fc47a40bf
Narayanan Iyer <nars@yottadb.com>
2022-11-28 15:30:52 -0500

    [YDB#488] Enhance env_xlate/ydb488 subtest to test $VIEW("GBLDIRXLATE")
    Background
    ----------
    * https://gitlab.com/YottaDB/DB/YDB/-/merge_requests/699#note_1181967887 has more details. But basically,
      no automated test was added for `$VIEW("GBLDIRXLATE")`.
    
    Fix
    ---
    * The existing `env_xlate/ydb488` subtest has been enhanced to test this functionality.
    
    * We do a `$VIEW("GBLDIRXLATE","T")` in the test. The `"T"` is a special case in the `ydb_gbldir_xlate()`
      function in `env_xlate/inref/ydb_gbldir_xlate.c` where it internally treats this as the global
      directory in the current directory named `mumps` (i.e. `mumps.gld`).
    
    * We also verify that `$ZGBLDIR` still reflects the untranslated name whereas the current/internal/active
      global directory reflects the translated one.

--- env_xlate/inref/zgbldir.m
+++ env_xlate/inref/zgbldir.m
@@ -1,6 +1,6 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;								;
-; Copyright (c) 2019 YottaDB LLC and/or its subsidiaries.	;
+; Copyright (c) 2019-2022 YottaDB LLC and/or its subsidiaries.	;
 ; All rights reserved.						;
 ;								;
 ;	This source code contains the intellectual property	;
@@ -14,4 +14,12 @@
 	write "a.gld     : ",^GBL,!
 	set $ZGBLDIR="b"
 	write "b.gld     : ",^GBL,!
+	write "; Test SET $ZGBLDIR=""T"" switches to mumps.gld but $ZGBLDIR still shows up as ""T""",!
+	write "; We verify mumps.gld is current gld by checking value of ^GBL which should say THIS IS MUMPS in that case",!
+	set $ZGBLDIR="T"
+	write "  " zwrite ^GBL
+	write "  " zwrite $ZGBLDIR
+	write "; Test that $VIEW(GBLDIRXLATE) works.",!
+	write "; Run [$VIEW(""GBLDIRXLATE"",""T"")] and verify it returns the expected [mumps].",!
+	write "  ",$view("GBLDIRXLATE","T"),!
 	quit
