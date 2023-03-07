a39616cce451d9483819ce5d2c81ee6a2cb07b41
Steven Estes <steven.estes@yottadb.com>
2023-03-07 17:28:48 -0500

    [#477] Fix v63006/gtm7952 test for ARM platforms.
    The following issues were addressed:
    1. The strace output differed between X86_64 and ARM systems so wildcarded a bit more of the output
       than was previously done.
    2. The TIMERHANDLER message that is emitted during the test has the handler address in it which is
       8 bytes on 64 bit platforms and 4 bytes on 32 bit platforms. Added the alternate 32 bit value into
       the reference file.
    3. The system was emitting messages about how long the timer ran for (set for 5 seconds) but the actual
       wait varied from 5 to 31 seconds was the longest I saw. These wide ranging times are due to the ARM
       servers busy running tests at this time. At a suggestion from @nars1, changed the output so we only
       put the sleep time out in x86_64 systems and avoid that message in the reference files for ARM as
       their performance is highly variable since they are generally small.
    4. For especially loaded systems, the intermediate job that was doing the hang could errantly  be
       determined to be hung if the wait (supposed to be 5 seconds) exceeded 30 seconds. To avoid this on
       loaded PI systems, @nars1 has requested a 300 second timeout which will affect runs on GT.M (minimum
       test time on GT.M is 5 mins as opposed to a small handfull of seconds on YottaDB) but that shouldn't
       be a problem for the few times that will happen versus the many times/week this test is run on
       YottaDB.

--- v63006/inref/gtm7952.m
+++ v63006/inref/gtm7952.m
@@ -25,9 +25,9 @@ gtm7952
 	set ^waittime=5		; Length of our HANG
 	for test=1:1:2 do	; Run 2 test cycles
 	. write:test=1 !,"# First test using external call sigdisable2() which does NOT have SIGSAFE defined on the external call "
-	. write:test=1 "(expect success)",!
+	. write:test=1 "(expect no errors)",!
 	. write:test=2 !,"# Second test using external call sigdisable1() which has SIGSAFE defined on the external call "
-	. write:test=2 "(expect HANG command to hang until killed on GT.M, and to pass on YottaDB)",!
+	. write:test=2 "(expect HANG command to hang until killed on GT.M, and no errors on YottaDB)",!
 	. ;
 	. set ^test=test
 	. zsystem "strace -o gtm7952_strace_"_test_".txt $gtm_dist/mumps -run hangtest^gtm7952"
@@ -36,7 +36,7 @@ gtm7952
 ;
 ; Routine called to do actual hang test and trace it via strace. These traces will later be scrutinized for indications
 ; of whether signal processing was avoided on the run that drove the SIGSAFE signed version of the sigdisable() routine.
-; Note: expects ^i to be set to an incremented value.
+; Note: expects ^test to be set to an incremented value.
 ;
 hangtest
 	;
@@ -55,14 +55,16 @@ hangtest
 	. close pidfile
 	job @("hangkill^gtm7952("_$job_",^waittime):(output=""gtm7952-"_^test_".mjo"":error=""gtm7952-"_^test_".mje"")")
 	set x=$select(^test=1:$&sigdisable1(),1:$&sigdisable2())
-	set killerpid=$zjob	; Record the process we job'd off and wait for it to die if the hang succeeded
-	set hangstart=$zut
-	hang ^waittime		; See if this hang wakes up
-	set hangstop=$zut
+	set killerpid=$zjob			; Record the process we job'd off and wait for it to die if the hang succeeded
+	set isx8664=$zyrelease["x86_64"		; Only put out hang time for x86_64
+	set:isx8664 hangstart=$zut
+	hang ^waittime				; See if this hang wakes up
+	set:isx8664 hangstop=$zut
 	set ^done=1
-	set hangtime=hangstop-hangstart
-	set hangsec=hangtime\(1000**2)
-	write "Hang time was ",hangtime,"us (",hangsec," seconds)",!
+	do:isx8664
+	. set hangtime=hangstop-hangstart
+	. set hangsec=hangtime\(1000**2)
+	. write "Hang time was ",hangtime,"us (",hangsec," seconds)",!
 	;
 	; Now make sure the hangkill job we started up has shutdown and wait till it does.
 	;
@@ -78,19 +80,20 @@ hangkill(hangpid,waitsec)
 	write "hangkill: Active and sleeping for ",waitsec," seconds",!
 	hang waitsec		    	; Wait for how long the hang process should hang
 	;
-	; Wait a few more seconds for the hang process to wakeup and shutdown
+	; Wait a few more seconds (max 300) for the hang process to wakeup and shutdown
 	;
-	for i=1:1:5 quit:1=^done  hang 1
-	write "hangkill: Sleeping complete - checking hang process now",!
+	write "hangkill: Sleep up to 300 seconds for the process to indicate it is done",!
+	for i=1:1:300 quit:1=^done  hang 1
+	write "hangkill: Sleeping complete (in ",i," seconds)- checking hang process now",!
 	;
 	; Now see if the hang process has completed its HANGs or not
 	;
-	if ^done write "hangkill: Found hang in complete status so exiting",!
+	if ^done write "hangkill: Found target process in complete status so exiting",!
 	else  do
-	. write "hangkill: Found hang (pid ",hangpid,") in a NOT complete status so sending it a SIGTERM interrupt",!
-	. set x=$zsigproc(hangpid,15) 	; Send SIGTERM
-	. write:0'=x "hangkill: $ZSIGPROC() to kill hang pid failed with rc ",x,!
+	. write "hangkill: Found target process (pid ",hangpid,") in a NOT complete status (hang) so sending it a SIGTERM interrupt",!
+	. set x=$zsigproc(hangpid,15) 	; Send SIGTERM - use signal number (15) instead of name so can run on GT.M
 	. write:0=x "hangkill: SIGTERM sent to hang process",!
+	. ; If the $zsigproc() failed, it is only because the process is gone so we're done too
 	;
 	; Either hang is already done or we've told it to quit and expect it to so now we need to quit as well.
 	;
