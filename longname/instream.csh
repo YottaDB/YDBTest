YottaDB/YDBTest

commit c7d31ddc2d611b5b274e38ad993401c2e7bda1fb

Disable longname/objtest subtest on SUSE until r1.38 is released
Background
----------
* The `longname/objtest` subtest failed on a SUSE system with the following diff.

  ```diff
  1c1,3
  < PASS
  ---
  > maximum: Undefined variable.
  > maximum: Undefined variable.
  > No prior versions available: RANDOMVER-E-CANNOTRUN : Could not determine previous version matching the given criteria. Exiting..
  > RANDOMVER-E-CANNOTRUN : Could not determine previous version matching the given criteria - -type obj_mismatch. Exiting...
  ```

Issue
-----
* At this point, YottaDB r1.36 is the current production release.

* And the only prior version build that is available on a SUSE system in-house is r1.36.

* The master branch of YottaDB has the same object format (`OBJ_UNIX_LABEL` in `sr_port/objlabel.h`)
  as r1.36.

* Therefore, the `objtest` subtest cannot run as it requires an older version with a different object
  format.

Fix
---
* This subtest is disabled until YottaDB r1.38 is released (at which point we are guaranteed to have changed
  the object file format at least once due to merging GT.M V7.0-000 and later).

* The changes are done in such a way that we will be automatically alerted when r1.38 is released (`gtm_curpro`
  env var will be changed) and the test will start failing then. At that point, the `objtest` subtest
  can be enabled even on SUSE.

longname/instream.csh
@@ -4,6 +4,9 @@
 # Copyright (c) 2004, 2015 Fidelity National Information	#
 # Services, Inc. and/or its subsidiaries. All rights reserved.	#
 #								#
+# Copyright (c) 2023 YottaDB LLC and/or its subsidiaries.	#
+# All rights reserved.						#
+#								#
 #	This source code contains the intellectual property	#
 #	of its copyright holder(s), and is made available	#
 #	under a license.  If you do not know the terms of	#
@@ -55,6 +58,16 @@ setenv subtest_exclude_list ""
 # If the platform/host does not have prior GT.M versions, disable tests that require them
 if ($?gtm_test_nopriorgtmver) then
 	setenv subtest_exclude_list "$subtest_exclude_list objtest mupip_load_V4"
+else if ("suse" == $gtm_test_linux_distrib) then
+	# Disable "objtest" subtest on SUSE Linux until r1.38 is released as the only prior release at this
+	# point on the SUSE systems is r1.36 which has the same object format as the current master branch of YDB.
+	# Once r1.38 is released and "gtm_curpro" is changed, the "longname" test will fail on SUSE boxes and
+	# will require the below "if" block to be removed.
+	if ($?gtm_curpro) then
+		if ($gtm_curpro == "V63014_R136") then
+			setenv subtest_exclude_list "$subtest_exclude_list objtest"
+		endif
+	endif
 endif
 #On platforms that don't have a V4 version, disable the subtests that rely on V4 versions
 if ($?gtm_platform_no_V4) then

commit 2d17cbb915cdd74f3a274775e74cd3e32d1a4f08

Disable longname/objtest subtest on SUSE until r1.40 is released
* The `longname/objtest` subtest had been disabled on SUSE in c7d31ddc. Now that r1.38 is released,
  this subtest started failing as expected by c7d31ddc.

* But r1.38 has the same object format as r1.36 and so this subtest needs to continue to be disabled
  on SUSE boxes.

* This commit takes care of that.

Notes
-----
* This subtest is disabled until YottaDB r1.40 is released (at which point we are guaranteed to have changed
  the object file format at least once due to merging GT.M V7.0-000 and later).

* The changes are done in such a way that we will be automatically alerted when r1.38 is released (`gtm_curpro`
  env var will be changed) and the test will start failing then. At that point, the `objtest` subtest
  can be enabled even on SUSE.

longname/instream.csh
@@ -59,12 +59,12 @@ setenv subtest_exclude_list ""
 if ($?gtm_test_nopriorgtmver) then
 	setenv subtest_exclude_list "$subtest_exclude_list objtest mupip_load_V4"
 else if ("suse" == $gtm_test_linux_distrib) then
-	# Disable "objtest" subtest on SUSE Linux until r1.38 is released as the only prior release at this
+	# Disable "objtest" subtest on SUSE Linux until r1.40 is released as the only prior release at this
 	# point on the SUSE systems is r1.36 which has the same object format as the current master branch of YDB.
-	# Once r1.38 is released and "gtm_curpro" is changed, the "longname" test will fail on SUSE boxes and
+	# Once r1.40 is released and "gtm_curpro" is changed, the "longname" test will fail on SUSE boxes and
 	# will require the below "if" block to be removed.
 	if ($?gtm_curpro) then
-		if ($gtm_curpro == "V63014_R136") then
+		if ($gtm_curpro == "V63014_R138") then
 			setenv subtest_exclude_list "$subtest_exclude_list objtest"
 		endif
 	endif