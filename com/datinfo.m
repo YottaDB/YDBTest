YottaDB/YDBTest

commit 7a92b24274fd260f77842e7082a0363a7ff08cb9

[YDB#780] V7.0-000 [#510] [GTM-9357] Changes to test system caused by removal of ^%DSEWRAP from the test system in favor of ^%PEEKBYNAME()
With this GT.M issue, %DSEWRAP.m is removed from the system so tests that use it must now use ^%PEEKBYNAME(). The test bits changed are:

- com/datinfo.m - Removed ^%DSEWRAP method of obtaining DSE information - simplified routine (tested with refresh_sec/use_backup_and_recover).
- mupip/inref/musetchk.m - Significant changes to use $$^%PEEKBYNAME() instead of ^%DSEWRAP of several fields (see below).
- v60000/inref/searchdump.m - Removed as is no longer used.
- v60000/u_inref/C9B12001861.csh - Changed invocation of searchdump.m to print one field with ^%DSEWRAP to printing one field with ^%PEEKBYNAME().
- v60001/inref/gtm7160.m - Removed as it is no longer used (old DSEWRAP test).
- v60001/outref/gtm7160.txt - removed as it is no longer used (old DSEWRAP test).
- v60001/u_inref/gtm7160.csh - Test removed as it was the old test for DSEWRAP.m which is removed from the test system in favor of PEEKBYNAME.m.
- v60003/gtm7777.m - Changed DSEWRAP usage to fetching fields with ^%PEEKBYNAME().

The changes for mupip/inref/musetchk.m were significant. I tried to make the smallest set of
changes possible by creating the same output that was previous created by the extract from the
output of %DSEWRAP so the extracted before and after files looked the same.

Previously, %DSEWRAP was used to create an array of the possible DSE fields that were displayed.
There was a variable (keep) that listed the field names by which the values generated by %DSEWRAP
were indexed. I kept this array to serve as field titles used later. I added a 'where' array indexed
the same as 'keep' (numeric increasing index) to say what the field name actually was in the
sgmnt_data structure (all fields were from this struct - if any had been from other structs,
the structure name would be part of the fieldname in this array). Since these are the raw values

com/datinfo.m
@@ -3,6 +3,9 @@
 ; Copyright (c) 2012-2015 Fidelity National Information 	;
 ; Services, Inc. and/or its subsidiaries. All rights reserved.	;
 ;								;
+; Copyright (c) 2023 YottaDB LLC and/or its subsidiaries.	;
+; All rights reserved.						;
+;								;
 ;	This source code contains the intellectual property	;
 ;	of its copyright holder(s), and is made available	;
 ;	under a license.  If you do not know the terms of	;
@@ -65,33 +68,27 @@ dseget(datinfo)
 	; In case of an error, use the by file mechanism
 	set $ETRAP="use $p zshow ""*"" set $ecode="""" zgoto "_$zlevel_":byfile^datinfo"
 	set myetrap=$ETRAP
-	set %DSEWRAP("debug")=1
-	if version>62002 do  quit
-byzpeek	.	set reg=$view("GVNEXT","")  ; Use information from the first available region
+	do:(version>62002)  quit	; Use ^%PEEKBYNAME() if it is available (V63000 and later)
+	.	set reg=$view("GVNEXT","")  ; Use information from the first available region
 	.	set datinfo("block_size")=$$^%PEEKBYNAME("sgmnt_data.blk_size",reg)
 	.	set datinfo("max_record_size")=$$^%PEEKBYNAME("sgmnt_data.max_rec_size",reg)
 	.	set datinfo("max_key_size")=$$^%PEEKBYNAME("sgmnt_data.max_key_size",reg)
-	if version>60000 do
-byrtn	.	do dump^%DSEWRAP("*",.dseoutput)
-	.	if myetrap'=$ETRAP write "TEST-W-WARN : ETRAP changed to",$ETRAP,! set $ETRAP=myetrap
-	.	set reg=$order(dseoutput(""))		; some tests change collation
-	.	set datinfo("block_size")=dseoutput(reg,"Block size (in bytes)")
-	.	set datinfo("max_record_size")=dseoutput(reg,"Maximum record size")
-	.	set datinfo("max_key_size")=dseoutput(reg,"Maximum key size")
-	if version'>60000 do
-byfile	.	set file="dsedump.txt"
-	.	set dsecmd="$DSE dump -fileheader >&! "_file
-	.	zsystem dsecmd
-	.	open file:readonly
-	.	use file
-	.	for line=1:1  quit:$zeof  read line(line) do
-	.	. if $length($text(^%MPIECE))>0 do  ; We don't have MPIECE implementation in the older versions so use flexpiece instead
-	.	.	. if line(line)?.e1" "1"Block size (in bytes)".e set datinfo("block_size")=$piece($$^%MPIECE(line(line),"  ","|"),"|",4)
-	.	. 	. if line(line)?." "1"Maximum record size".e set datinfo("max_record_size")=$piece($$^%MPIECE(line(line),"  ","|"),"|",2)
-	.	. 	. if line(line)?." "1"Maximum key size".e set datinfo("max_key_size")=$piece($$^%MPIECE(line(line),"  ","|"),"|",2)
-	.	. else  do
-	.	.	. if line(line)?.e1" "1"Block size (in bytes)".e set datinfo("block_size")=$piece($$^v53003flexpiece(line(line),"  ","|"),"|",4)
-	.	. 	. if line(line)?." "1"Maximum record size".e set datinfo("max_record_size")=$piece($$^v53003flexpiece(line(line),"  ","|"),"|",2)
-	.	. 	. if line(line)?." "1"Maximum key size".e set datinfo("max_key_size")=$piece($$^v53003flexpiece(line(line),"  ","|"),"|",2)
-	.	close file
+	;
+	; Else, we are stuck pulling it out of DSE itself
+	;
+	set file="dsedump.txt"
+	set dsecmd="$DSE dump -fileheader >&! "_file
+	zsystem dsecmd
+	open file:readonly
+	use file
+	for line=1:1  quit:$zeof  read line(line) do
+	. if $length($text(^%MPIECE))>0 do  ; We don't have MPIECE implementation in the older versions so use flexpiece instead
+	.	. if line(line)?.e1" "1"Block size (in bytes)".e set datinfo("block_size")=$piece($$^%MPIECE(line(line),"  ","|"),"|",4)
+	. 	. if line(line)?." "1"Maximum record size".e set datinfo("max_record_size")=$piece($$^%MPIECE(line(line),"  ","|"),"|",2)
+	. 	. if line(line)?." "1"Maximum key size".e set datinfo("max_key_size")=$piece($$^%MPIECE(line(line),"  ","|"),"|",2)
+	. else  do
+	.	. if line(line)?.e1" "1"Block size (in bytes)".e set datinfo("block_size")=$piece($$^v53003flexpiece(line(line),"  ","|"),"|",4)
+	. 	. if line(line)?." "1"Maximum record size".e set datinfo("max_record_size")=$piece($$^v53003flexpiece(line(line),"  ","|"),"|",2)
+	. 	. if line(line)?." "1"Maximum key size".e set datinfo("max_key_size")=$piece($$^v53003flexpiece(line(line),"  ","|"),"|",2)
+	close file
 	quit

mupip/inref/musetchk.m
@@ -3,7 +3,7 @@
 ; Copyright (c) 2015-2016 Fidelity National Information		;
 ; Services, Inc. and/or its subsidiaries. All rights reserved.	;
 ;								;
-; Copyright (c) 2018-2021 YottaDB LLC and/or its subsidiaries.	;
+; Copyright (c) 2018-2023 YottaDB LLC and/or its subsidiaries.	;
 ; All rights reserved.						;
 ;								;
 ;	This source code contains the intellectual property	;
@@ -19,15 +19,71 @@ musetchk(file)
 	set $ecode="",$etrap="zgoto "_$zlevel_":error^"_$text(+0)
 	open file:(newversion:exception="goto badfile")
 	use file:rewind
-	new d,label,reg
-	do dump^%DSEWRAP("*",.d,"fileheader","all")
-	set keep=$piece($text(keep),";",2,99)_$piece($text(keep+1),";",2,99),(label,reg)=""
-	for  set reg=$order(d(reg)) quit:""=reg  do
-	. for  set label=$order(d(reg,label)) quit:""=label  zwrite:keep[label d(reg,label)
+	new fldval,label,reg
+	set keep=$zpiece($text(keep),";",2,99)_$zpiece($text(keep+1),";",2,99),(label,reg)=""
+	set where=$zpiece($text(where),";",2,99)_$zpiece($text(where+1),";",2,99),(label,reg)=""
+	set fldfmt=$zpiece($text(fldfmt),";",2,99)_$zpiece($text(fldfmt+1),";",2,99),(label,reg)=""
+	set reg=""
+	for  set reg=$view("GVNEXT",reg) quit:""=reg  do
+	. for pieceidx=1:1  set label=$zpiece(where,";",pieceidx) quit:label=""  do
+	. . set fldval=$$^%PEEKBYNAME("sgmnt_data."_label,reg)
+	. . set fldfmter=$zpiece(fldfmt,";",pieceidx)
+	. . set:fldfmter'="0" @("fldfmted=$$"_fldfmter_"(fldval)")
+	. . set:fldfmter=0 fldfmted=fldval
+	. . set flddesc=$zpiece(keep,";",pieceidx)
+	. . set d(reg,flddesc)=fldfmted
+	. . zwrite d(reg,flddesc)
 	close file
 	quit
+
+	;
+	; Following are the field descriptions. The old $$^%DSEWRAP() script used to use these names as the indexes so in order
+	; to not have to redesign the whole test, we still use these names as indexes when we create the array. Note the index
+	; is the same amongst the keep/where/fldfmt arrays.
+	;
 keep	;Access method;Defer allocation;Extension Count;Global Buffers;Lock space;Maximum key size;Maximum record size;
 	;Mutex Queue Slots;Mutex Sleep Spin Count;Quick database rundown is active;Reserved Bytes;Spin sleep time mask
+
+	;
+	; Following are the field names in sgmnt_data we need to fetch for each region - indexed same as keep().
+	;
+where	;acc_meth;defer_allocate;extension_size;n_bts;lock_space_size;max_key_size;max_rec_size;
+	;mutex_spin_parms.mutex_que_entry_space_size;mutex_spin_parms.mutex_sleep_spin_count;mumps_can_bypass;reserved_bytes;mutex_spin_parms.mutex_spin_sleep_mask
+
+	;
+	; Following is a list of routines to call to format the values fetched by PEEKBYNAME or 0 if value is formatted
+	; correctly already.
+	;
+fldfmt	;fmtaccmeth;fmtboolean;0;0;fmtlockspace;0;0;
+	;0;0;fmtboolean;0;fmthex
+
+;
+; Routine fmtaccmeth to turn raw fetched value into BG or MM
+;
+fmtaccmeth(fmt)
+	quit $select("1"=fmt:"BG","2"=fmt:"MM")		; Anything unexpected causes a select error
+
+;
+; Routine fmtboolean to turn raw boolean value into "TRUE" or "FALSE"
+;
+fmtboolean(bool)
+	quit $select("0"=bool:"FALSE",1:"TRUE")
+
+;
+; Routine fmtlockspace to turn lock space bytes into 512 blocks but as a hex value
+;
+fmtlockspace(bytes)
+	quit $$fmthex(bytes/512)
+
+;
+; Routine fmthex to turn fetched decimal values into hex values
+;
+fmthex(dec)
+	quit "0x"_$$FUNC^%DH(dec,8)
+
+;
+; Error routines
+;
 badfile	close file
 	write !,"Error with ",file,!,$zstatus
 	quit
@@ -39,6 +95,10 @@ error	set fname=$io
 	for zl=$stack(-1):1:0 for info="PLACE","MCODE","ECODE" write !,$stack(zl,info)
 	set $ecode=""
 	quit
+
+;
+; Check values saved before and after test
+;
 dochk(before,after)
 	new goodcnt,badcnt,file,i,j,label,reg,x
 	new etrap

v60000/inref/searchdump.m
@@ -1,12 +1,0 @@
-;search for a partucular line in dse dump
-searchdump(particular)
-	do dump^%DSEWRAP("DEFAULT",.s,"","all")
-	set sfile="savedsewrap.out"
-	open sfile:append
-	use sfile
-	zwrite s
-	close sfile
-	set y="s"
-	for  set y=$query(@y) quit:(y="")!($order(@y)=particular)
-	write @($query(@y))
-	quit

v60000/u_inref/C9B12001861.csh
@@ -4,6 +4,9 @@
 # Copyright (c) 2013-2016 Fidelity National Information		#
 # Services, Inc. and/or its subsidiaries. All rights reserved.	#
 #								#
+# Copyright (c) 2023 YottaDB LLC and/or its subsidiaries.	#
+# All rights reserved.						#
+#								#
 #	This source code contains the intellectual property	#
 #	of its copyright holder(s), and is made available	#
 #	under a license.  If you do not know the terms of	#
@@ -64,7 +67,7 @@ EOF
 	$MUPIP rundown -reg "*"
 	@ i += 1
 	#Let's check if DSE changes are applied and LKE sees the same changes
-	$gtm_exe/mumps -run %XCMD 'do ^searchdump("Maximum record size")'
+	$gtm_exe/mumps -run %XCMD 'write $$^%PEEKBYNAME("sgmnt_data.max_rec_size","DEFAULT"),!'
 end
 
 $gtm_tst/com/dbcheck.csh

v60001/inref/gtm7160.m
@@ -1,82 +1,0 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;								;
-; Copyright (c) 2012-2016 Fidelity National Information		;
-; Services, Inc. and/or its subsidiaries. All rights reserved.	;
-;								;
-;	This source code contains the intellectual property	;
-;	of its copyright holder(s), and is made available	;
-;	under a license.  If you do not know the terms of	;
-;	the license, please stop and do not read further.	;
-;								;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-gtm7160
-	set $ETRAP="do ERROR^gtm7160"
-	new reg,dseout,which
-	; %DSEWRAP debugging
-	set %DSEWRAP("debug")=1
-	;dump the piped commands to a file;zbreak dump+30^%DSEWRAP:"write ""open -file=dse.out"",!"
-	; UTF-8 mode on Solaris and HPUX have a bug, use file mode
-	if ($zchset="UTF-8")&($select($zversion["Solaris":1,$zversion["HP-UX":1,1:0)) set %DSEWRAP("forcenopipe")=1
-	; Choose mode of operation. With a random $piece between 1 and 6 (see the end of the file)
-	; DEFAULT on VMS is "$DEFAULT"
-	if $zversion["VMS" set randreg=$text(vmsregs^gtm7160)
-	else  set randreg=$text(unixregs^gtm7160)
-	; Choose which reg to use
-	set which=+$zcmdline
-	if which=0 set which=1+$random($length(randreg,";")-1)
-	; drive dump^%DSEWRAP varying the input parameters, add 1 to avoid the mumps label
-	do dump^%DSEWRAP($piece(randreg,";",which+1),.dseout,,"all")
-	if '$data(dseout) do ERROR^gtm7160
-	for  set reg=$order(dseout($get(reg))) quit:reg=""  do sanitize(.dseout,reg)
-	zwrite dseout
-	set dbg="gtm7160.dbg" open dbg:newversion use dbg zwrite  close dbg
-	quit
-
-sanitize(out,reg)
-	set $ETRAP="do ERROR^gtm7160",MASKED="MASKED"
-	; santize "Date/Time"
-	if 10000>(+$piece(out(reg,"Date/Time"),",",2)) set $piece(out(reg,"Date/Time"),",",2)=".....]"
-	set out(reg,"Date/Time")=$$dtmask(out(reg,"Date/Time"))
-
-	; mask all TRUE/FALSE values
-	set field=""
-	for  set field=$order(out(reg,field)) quit:""=field  do:out(reg,field)?1(1"TRUE",1"FALSE")
-	.	set out(reg,field)=MASKED
-
-	; santize "Access method" randomization
-	set field="Access method"
-	set cmpval=$ztrnlnm("acc_meth") if $length(cmpval)=0 set cmpval="BG"
-	set out(reg,field)=(out(reg,field)=cmpval)
-
-	; detect endian "gtm_endian"
-	set field="Endian Format"
-	set cmpval=$ztrnlnm("gtm_endian"),cmpval=$select(cmpval="LITTLE_ENDIAN":"LITTLE",1:"BIG")
-	set out(reg,field)=(out(reg,field)=cmpval)
-
-	; just mask these values
-	set fields=$text(mask)
-	for i=2:1:$length(fields,";") do
-	. set out(reg,$piece(fields,";",i))=MASKED
-
-	; kill fields that don't always show up in the output
-	set fields=$text(remove)
-	for i=2:1:$length(fields,";") do
-	. kill out(reg,$piece(fields,";",i))
-	. kill out(reg,$piece(fields,";",i)_" TN") ; some of these have "buddy" fields
-
-	quit
-
-dtmask(str)
-	 set str=$translate(str,"ABCDEFGHIJKLMNOPQRSTUVWXYZ","..........................")
-	 quit $translate(str,"1234567890","..........")
-
-ERROR
-	write $zstatus,!
-	zshow "*"
-	halt
-
-	; DEFAULT on VMS is "$DEFAULT"
-unixregs;*;DEFAULT,AREG,BREG,DEFAULT;DEFAULT,AREG,BREG;DEFAULT,AREG;
-vmsregs;*;$DEFAULT,AREG,BREG,$DEFAULT;$DEFAULT,AREG,BREG;$DEFAULT,AREG;
-mask;Journal Alignsize;Last GT.M Minor Version;DB Current Minor Version;DB Creation Minor Version;DB Creation Version;Next Epoch_Time;Journal checksum seed;Free Global Buffers;MM defer_time;Journal Before imaging;Journal Epoch Interval;Writer fnd no writes;Blks Last Comprehensive Backup;CAT : # of crit acquired total successes;Database file encryption hash
-remove;Total buffer flushes;Writer fnd no writes;DFL : # of Database FLushes;DFS : # of Database FSyncs;wcs_wtstart intent cnt;wcs_wtstart pid count;Calls to wcs_wtstart

v60001/u_inref/gtm7160.csh
@@ -1,18 +1,0 @@
-#! /usr/local/bin/tcsh -f
-#################################################################
-#								#
-# Copyright (c) 2012-2016 Fidelity National Information		#
-# Services, Inc. and/or its subsidiaries. All rights reserved.	#
-#								#
-#	This source code contains the intellectual property	#
-#	of its copyright holder(s), and is made available	#
-#	under a license.  If you do not know the terms of	#
-#	the license, please stop and do not read further.	#
-#								#
-#################################################################
-setenv gtm_test_disable_randomdbtn 1
-setenv gtm_test_mupip_set_version "DISABLE"
-setenv gtm_test_freeze_on_error 0
-$gtm_tst/com/dbcreate.csh mumps 2 255 512 1024 2048 1024
-$gtm_exe/mumps -run gtm7160
-$gtm_tst/com/dbcheck.csh

v60003/inref/gtm7777.m
@@ -3,6 +3,9 @@
 ; Copyright (c) 2014-2015 Fidelity National Information 	;
 ; Services, Inc. and/or its subsidiaries. All rights reserved.	;
 ;								;
+; Copyright (c) 2023 YottaDB LLC and/or its subsidiaries.	;
+; All rights reserved.						;
+;								;
 ;	This source code contains the intellectual property	;
 ;	of its copyright holder(s), and is made available	;
 ;	under a license.  If you do not know the terms of	;
@@ -52,11 +55,24 @@ expout2	; *yes*, intervening update between DBFLUSH and EPOCH
 ; Print expected output, based on journal state
 ;
 expout(intervene)
-	new s,accmeth,jnlstate
-	do dump^%DSEWRAP("DEFAULT",.s,"","all")
-	set accmeth=s("DEFAULT","Access method")
-	if "DISABLED"=s("DEFAULT","Journal State") set jnlstate="nojnl"
-	else  if "TRUE"=s("DEFAULT","Journal Before imaging") set jnlstate="jnlbefore"
+	new accmeth,jnlstatus,jnlstate
+	;
+	; Set possible journal states
+	;
+	set jnlstatus("notallowed")=0
+	set jnlstatus("close")=1
+	set jnlstatus("open")=2
+	;
+	; Set possible access methods
+	;
+	set accmeth(1)="BG"
+	set accmeth(2)="MM"
+	;
+	set accmeth=$$^%PEEKBYNAME("sgmnt_data.acc_meth","DEFAULT")
+	set accmeth=accmeth(accmeth)
+	set jnlstatus=$$^%PEEKBYNAME("sgmnt_data.jnl_state","DEFAULT")
+	if jnlstatus("notallowed")=jnlstatus set jnlstate="nojnl"
+	else  if $$^%PEEKBYNAME("sgmnt_data.jnl_before_image","DEFAULT") set jnlstate="jnlbefore"
 	else  set jnlstate="jnlnobefore"
 	do print(accmeth_jnlstate_intervene)
 	quit

commit 6d2297b3d83127ca0a65f8dc40118302a7e8b918

Fix dseget(datinfo) to work with V62000 (another regression in 7a92b242)
Background
----------
* The `64bittn_1/rolling_upgrade` subtest failed with the following diff.

  ```diff
  --- rolling_upgrade/rolling_upgrade.diff ---
  > IMPTP-E-FAIL : imptp.csh invocation had errors in file [imptp.out]. Error output follows.
  > $ZSTATUS="150373210,get+15^datinfo,%GTM-E-DIVZERO, Attempt to divide by zero"
  ```

Issue
-----
* This test runs a rolling upgrade from an older version to the current version.
  In this test run, the older version used was V62000.

  ```sh
  $ cat priorver.txt
  V62000
  ```

* And the `imptp.csh` error above happened when using `V62000`.

* The `DIVZERO` error happened at line 37 below.

  **com/datinfo.m**
  ```m
     33         do dseget(.datinfo)
     34         ; Large record sizes have a tendency to fill disks. don't use them unless the test is spanning nodes
     35         if (blockspan=0)&(datinfo("max_record_size")>1000) set datinfo("max_record_size")=1000
     36         ; Don't let the max_key_size exceed more than 50% of the block size in general
     37         set:(.5<(datinfo("max_key_size")/datinfo("block_size"))) datinfo("max_key_size")=(datinfo("block_size")*0.5)\1
  ```

* And that is because `datinfo("block_size")` evaluated to 0.

* Below is the actual value of that variable from the test output.

  ```
  $ grep datinfo imptp.out | grep block_size
  imptp.out:datinfo("block_size")=" 1024"
  ```

* Notice the extra space at the start of ` 1024`. That is the reason why it evaluates to 0.

* The reason for this is line 86 below.

  **com/datinfo.m**
  ```m
     84         for line=1:1  quit:$zeof  read line(line) do
     85         . if $length($text(^%MPIECE))>0 do  ; We don't have MPIECE implementation in the older versions so use flexpiece instead
     86         .       . if line(line)?.e1" "1"Block size (in bytes)".e set datinfo("block_size")=$piece($$^%MPIECE(line(line),"  ","|"),"|",4)
  ```

com/datinfo.m
@@ -83,9 +83,9 @@ dseget(datinfo)
 	use file
 	for line=1:1  quit:$zeof  read line(line) do
 	. if $length($text(^%MPIECE))>0 do  ; We don't have MPIECE implementation in the older versions so use flexpiece instead
-	.	. if line(line)?.e1" "1"Block size (in bytes)".e set datinfo("block_size")=$piece($$^%MPIECE(line(line),"  ","|"),"|",4)
-	. 	. if line(line)?." "1"Maximum record size".e set datinfo("max_record_size")=$piece($$^%MPIECE(line(line),"  ","|"),"|",2)
-	. 	. if line(line)?." "1"Maximum key size".e set datinfo("max_key_size")=$piece($$^%MPIECE(line(line),"  ","|"),"|",2)
+	.	. if line(line)?.e1" "1"Block size (in bytes)".e set datinfo("block_size")=$$^%MPIECE($piece($$^%MPIECE(line(line),"  ","|"),"|",4))
+	. 	. if line(line)?." "1"Maximum record size".e set datinfo("max_record_size")=$$^%MPIECE($piece($$^%MPIECE(line(line),"  ","|"),"|",2))
+	. 	. if line(line)?." "1"Maximum key size".e set datinfo("max_key_size")=$$^%MPIECE($piece($$^%MPIECE(line(line),"  ","|"),"|",2))
 	. else  do
 	.	. if line(line)?.e1" "1"Block size (in bytes)".e set datinfo("block_size")=$piece($$^v53003flexpiece(line(line),"  ","|"),"|",4)
 	. 	. if line(line)?." "1"Maximum record size".e set datinfo("max_record_size")=$piece($$^v53003flexpiece(line(line),"  ","|"),"|",2)