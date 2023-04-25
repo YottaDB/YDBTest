YottaDB/YDBTest

commit 52d0fa2ccd02016aee9c19586b1a3e4116c7f62f

Fix v62002/gtm5894 subtest failure on ZFS filesystem (due to asynchronous pending changes)
Background
----------
* The `v62002/gtm5894` subtest failed on a ZFS filesystem with the following diff.

  ```diff
  --- gtm5894/gtm5894.diff ---
  7,39c7
  > TEST-E-FAIL database was not fully allocated. Check the size of the mumps.dat file.
  ```

* effb72db had recently fixed a similar failure when the ZFS filesystem used compression.

* But in the above failure case, the ZFS filesystem had compression turned OFF.

Issue
-----
* Some investigation revealed the following.

* Immediately after a `mupip create`, a `du -k` of the resulting `mumps.dat` file showed only
  1 block allocated. But if one waited a few seconds, the same `du -k` command showed a different
  value of 273 (which is what the test expects to see).

* Turns out ZFS does things in the background (for performance reasons, I guess).

  Below is pasted from https://docs.oracle.com/cd/E36784_01/html/E36835/gbchp.html

  ZFS is a transactional file system. Most file system modifications are bundled into transaction groups and
  committed to disk asynchronously. Until these modifications are committed to disk, they are called pending
  changes. The amount of disk space used, available, and referenced by a file or file system does not consider
  pending changes. Pending changes are generally accounted for within a few seconds. Even committing a change
  to disk by using fsync(3c) or O_SYNC does not necessarily guarantee that the disk space usage information
  is updated immediately.

* So what this means is that after a `mupip create`, the test should cannot immediately move on to the
  next stage which does a `du` of the resulting .dat file and compares it to the `ls -l` output of the
  file (i.e. allocated size vs actual size).

Fix
---
* One fix would be for the test to wait a few seconds until the `du` output stabilizes. But it is not clear

v62002/instream.csh
@@ -67,6 +67,11 @@ setenv subtest_exclude_list	""
 # gtm5894 relies on allocated disk space (using "du") which will not be accurate on compressed file systems. So skip it there.
 if (1 == $is_tst_dir_cmp_fs) then
 	setenv subtest_exclude_list "$subtest_exclude_list gtm5894"
+else if ("zfs" == $tst_dir_fstype) then
+	# On ZFS, "du" on a newly created file does not return the allocated size immediately after a "mupip create". It could
+	# take a few seconds before it returns the fully allocated size (these are called "pending changes" in ZFS). That can
+	# cause the "gtm5894" subtest to fail as it does "du" on the file a lot of times. Therefore disable this subtest if ZFS.
+	setenv subtest_exclude_list "$subtest_exclude_list gtm5894"
 endif
 # Remove rtnxfertst, gtm8290 and gtm8240 subtests from Linux-32bit and HPUX-IA64 as they involve auto-relink or recursive-relink
 if ((HOST_LINUX_IX86 == "$gtm_test_os_machtype") || (HOST_HP-UX_IA64 == "$gtm_test_os_machtype")) then

com/get_filesystem_type.csh
@@ -1,0 +1,33 @@
+#!/usr/local/bin/tcsh -f
+#################################################################
+#								#
+# Copyright (c) 2023 YottaDB LLC and/or its subsidiaries.	#
+# All rights reserved.						#
+#								#
+#	This source code contains the intellectual property	#
+#	of its copyright holder(s), and is made available	#
+#	under a license.  If you do not know the terms of	#
+#	the license, please stop and do not read further.	#
+#								#
+#################################################################
+
+# $1 - directory path; if no argument is passed, current directory (pwd) is assumed.
+#
+# Prints the filesystem type holding the directory passed in $1.
+# Prints nothing and returns $status of -1 in case of error.
+#
+
+set dir = "$1"
+if ("$dir" == "") then
+	set dir = "."
+endif
+
+set mountpoint = `df $dir | tail -1 | $tst_awk '{print $NF}'`
+if ($status) then
+	echo "TEST-E-ISCURDIRCMPFS : Error while determining filesystem for directory $dir"
+	exit -1
+endif
+
+set filesystype = `grep $mountpoint /etc/mtab | sed 's/rw.*//g' | $tst_awk '{print $NF}'`
+echo $filesystype
+

com/gtmtest.csh
@@ -163,6 +163,9 @@ endif
 # Set env var is_tst_dir_ssd to 1 if disk containing $tst_dir is a SSD. And to 0 otherwise.
 setenv is_tst_dir_ssd `$gtm_test_com_individual/is_curdir_ssd.csh $tst_dir`
 
+# Set env var tst_dir_fstype based on the filesystem of $tst_dir.
+setenv tst_dir_fstype `$gtm_test_com_individual/get_filesystem_type.csh $tst_dir`
+
 # Set env var to is_tst_dir_cmp_fs to 1 if disk containing $tst_dir is a compressed filesystem. And to 0 otherwise.
 setenv is_tst_dir_cmp_fs `$gtm_test_com_individual/is_curdir_compressed_fs.csh $tst_dir`
 

com/is_curdir_compressed_fs.csh
@@ -27,20 +27,17 @@ if ("$dir" == "") then
 	set dir = "."
 endif
 
-set mountpoint = `df $dir | tail -1 | $tst_awk '{print $NF}'`
+set filesystype = `source $gtm_tst/com/get_filesystem_type.csh $dir`
 if ($status) then
-	echo "TEST-E-ISCURDIRCMPFS : Error while determining filesystem for directory $dir"
 	exit -1
 endif
-
-set filesystype = `grep $mountpoint /etc/mtab | sed 's/rw.*//g' | $tst_awk '{print $NF}'`
 
 # For ext4 and xfs, compression does not seem to be supported at this point.
 # For f2fs, compression is supported but filesystem space is consumed at the same rate as if compression was disabled.
 # Therefore, the compression check is currently done only for ZFS filesystems.
 set is_compressed = 0
 if ("$filesystype" == "zfs") then
-	set compressed = `zfs get compression $mountpoint | tail -1 | $tst_awk '{print $3}'`
+	set compressed = `zfs get compression $dir | tail -1 | $tst_awk '{print $3}'`
 	if ("$compressed" != "off") then
 		set is_compressed = 1
 	endif

com/outref.awk
@@ -393,6 +393,12 @@ BEGIN {
 	else
 		envir[no_options] = "TST_DIR_NOT_COMPRESSED"
 	#
+	option_names[++no_options] = "tst_dir_fstype"
+	if ("zfs" == ENVIRON["tst_dir_fstype"])
+		envir[no_options] = "TST_DIR_ZFS"
+	else
+		envir[no_options] = "TST_DIR_NOT_ZFS"
+	#
 	split(tst_hostos_machtype_all, all_platforms, " ")
 	if ("AIX" == tst_osname)
 	{

commit b554b9de21c30038f23dc3b963d645d868b679e3

Fix v62002 test failure on f2fs filesystem (regression in 52d0fa2c)
* After 52d0fa2c, running the `v62002` test on a system which had the test output directory using
  a `f2fs` filesystem resulted in the following failure.

  ```diff
  $ cat diff.log
  2,26c2
  < PASS from gtm8214
  ---
  > if: Expression Syntax.
  ```

* This turned out to be because the file system type could be a multi-word string. In the
  failing system, it turned out to be `f2fs overlay`. And so testing it in csh scripts should
  always surround it with double-quotes to avoid syntax errors like the above.

* The above syntax error happened in `v62002/instream.csh` in an `if` check. And that is fixed
  in the current commit.

v62002/instream.csh
@@ -67,7 +67,7 @@ setenv subtest_exclude_list	""
 # gtm5894 relies on allocated disk space (using "du") which will not be accurate on compressed file systems. So skip it there.
 if (1 == $is_tst_dir_cmp_fs) then
 	setenv subtest_exclude_list "$subtest_exclude_list gtm5894"
-else if ("zfs" == $tst_dir_fstype) then
+else if ("zfs" == "$tst_dir_fstype") then
 	# On ZFS, "du" on a newly created file does not return the allocated size immediately after a "mupip create". It could
 	# take a few seconds before it returns the fully allocated size (these are called "pending changes" in ZFS). That can
 	# cause the "gtm5894" subtest to fail as it does "du" on the file a lot of times. Therefore disable this subtest if ZFS.