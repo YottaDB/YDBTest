1b9c2389705b9f20bd784296a570c37aa3a33874
Narayanan Iyer <nars@yottadb.com>
2023-07-18 11:48:23 -0400

    Fix rare gtcm_gnp/locks_two_clients subtest failure (when $H switches to next day during timed lock)
    Background
    ----------
    * We had a rare failure of this subtest during in-house testing with the following diff.
    
      ```diff
      35c35
      < ##DIFFERENT EXPRESSION##^The time it took the lock command to finish:([5-9]|1[0-9])$## VS ##The time it took the lock command to finish:-86394##
      ---
      > The time it took the lock command to finish:-86394
      ```
    
    Issue
    -----
    * The reference file allows for a lock command time to finish anywhere from 5 seconds to 19 seconds.
      But the test actually took `-86394` seconds to finish as per the above output.
    
    * This is clearly incorrect. Looking at `gtcm_gnp/inref/lockcl.m`, I see we get `$H` before and after
      the lock command and subtrace the seconds part of the two in coming up with the elapsed time. This
      is clearly incorrect in case the day part of $H changes during the LOCK command.
    
    Fix
    ---
    * The fix is simple and is to use `$$^difftime` to find the elapsed time in seconds between the two
      $H values. This takes care of changes in the seconds or the day part of $H.

--- gtcm_gnp/inref/lockcl.m
+++ gtcm_gnp/inref/lockcl.m
@@ -1,3 +1,18 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;								;
+; Copyright (c) 2023 YottaDB LLC and/or its subsidiaries.	;
+; All rights reserved.						;
+;								;
+; Portions Copyright (c) Fidelity National			;
+; Information Services, Inc. and/or its subsidiaries.		;
+;								;
+;	This source code contains the intellectual property	;
+;	of its copyright holder(s), and is made available	;
+;	under a license.  If you do not know the terms of	;
+;	the license, please stop and do not read further.	;
+;								;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 lockcl	;multiple clients, one holding the lock other one is waiting on
 	; waiting for lockbcl to lock ^b
 	s maxtime=60,found=0,timeout=0
@@ -15,13 +30,12 @@ lockcl	;multiple clients, one holding the lock other one is waiting on
 	w "it should not have gotten the lock",!,"(at ",$H,")",!,!
 	W "now tell it to let go",!,"(at ",$H,")",!
 	s ^bexitnow=1
-	s h1=$E($H,7,12)
+	s start=$h
 	l ^b:40
 	w $T,!
-	s h2=$E($H,7,12)
+	s end=$h,elapsed=$$^difftime(end,start)
 	w "now it should have gotten the lock",!,"(at ",$H,")",!
-	w "The time it took the lock command to finish:",h2-h1,!
+	w "The time it took the lock command to finish:",elapsed,!
 	w "it should be as long as the lockbcl sleeps after releasing the lock (which is 5 seconds now).",!
-	s dif=h2-h1
-	i dif>39 w "FAILED. Waited too long.",dif,!
+	i elapsed>39 w "FAILED. Waited too long.",elapsed,!
 	q
