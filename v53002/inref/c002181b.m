5bec96cefcc98e93c2677c0b3d491a407f9d6ea1
Narayanan Iyer <nars@yottadb.com>
2023-10-24 10:54:31 -0400

    [YDB#1037] Fix v53002/C9C11002181 subtest hang
    * The M code in `v53002/inref/c002181b.m` used to terminate with a `%YDB-E-STACKCRIT` error previously.
      But due to the code changes in YDB@ec48a8a2, it now went into an infinite loop (because the M stack
      space was not being used up in each iteration like it used to be). This caused the test to hang.
    
    * This is now fixed by limiting the number of iterations in the test to an arbitrary number of 10,000
      and stopping the test at that point (just like a similar test `r140/inref/ydb1037.m` does).

--- v53002/inref/c002181b.m
+++ v53002/inref/c002181b.m
@@ -1,6 +1,21 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;								;
+; Copyright (c) 2023 YottaDB LLC and/or its subsidiaries.	;
+; All rights reserved.						;
+;								;
+; Portions Copyright (c) Fidelity National			;
+; Information Services, Inc. and/or its subsidiaries.		;
+;								;
+;	This source code contains the intellectual property	;
+;	of its copyright holder(s), and is made available	;
+;	under a license.  If you do not know the terms of	;
+;	the license, please stop and do not read further.	;
+;								;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 x       ;
         set $ztrap="do ztr"
         set x=1/0
         quit
 ztr     ;
+	if $increment(cnt)>10000 write "Stopping test at 10,000 iterations to avoid infinite loop",! halt
         quit
