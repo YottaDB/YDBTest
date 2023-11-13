YottaDB/YDBTest

commit 00d6f2e4dc0b4cbcf6f043eafb12251d0efa9a32

Enhance test framework to issue error if subtest that requires -replic is submitted without -replic
Background
----------
* See https://gitlab.com/YottaDB/DB/YDBTest/-/merge_requests/1747#note_1643507860 and surrounding
  discussion for more details.

* Basically, if a new subtest is written to require `-replic` and one tries to run that subtest but
  forgets to provide the `-replic` flag, the subtest would run and produce weird output that is
  hard to understand.

* One way to fix this is to have a check of `$test_replic` inside the subtest script and issue an
  error.

* But this would mean duplicating this logic in a lot of the subtests that have this issue.

* Instead it would be helpful if the test framework did not run the subtest at all and instead printed
  a message indicating the lack of the `-replic` flag as the cause of the skip.

Primary Fix
-----------
* `com/submit_subtest.csh` has been enhanced to do this check. It checks if the following 3 env vars
  are defined (they are defined by 80% or so of the entire set of tests and definitely 100% of the
  time in new tests and so that is a good enough check for all tests that we will likely update in
  the near future).

* This script determines from these env vars if a subtest requires `-replic` and whether `-replic`
  was specified. If not, it issues an error.

* Likewise it issues an error if a subtest cannot run with `-replic` and a `-replic` was issued at
  test startup.

Secondary changes
-----------------
* I noticed that a few tests used env vars `subtest_list_replic_FE` and `subtest_list_non_replic_FE`
  to denote the list of subtests that are run with and without `-replic` respectively when `L_ALL`
  is not specified (i.e. `E_ALL` or `F_ALL` is specified). This meant adding this env var also to
  the `com/submit_subtest.csh` logic. But the number of such tests were very few and we never run
  `L_ALL` tests (i.e. light tests only) so removed this logic in all those few files and moved the

v44004/instream.csh
@@ -1,7 +1,7 @@
 #!/usr/local/bin/tcsh -f
 #################################################################
 #								#
-# Copyright (c) 2022 YottaDB LLC and/or its subsidiaries.	#
+# Copyright (c) 2022-2023 YottaDB LLC and/or its subsidiaries.	#
 # All rights reserved.						#
 #								#
 #	This source code contains the intellectual property	#
@@ -35,18 +35,14 @@
 #	b) a change to outref.txt to handle replication as well as non-replication test runs using ##TEST_AWK macros
 #
 echo "V44004 test starts..."
-setenv subtest_list_common ""
-setenv subtest_list_replic "D9D10002386 C9D12002472_1"
-setenv subtest_list_non_replic "D9D11002390 D9D06002344 D9D12002401 D9D08002352 D9E03002436 C9D01002206"
-setenv subtest_list_non_replic_FE "C9D12002472_1 C9D12002472_2"
+setenv subtest_list_common "C9D12002472_1"
+setenv subtest_list_replic "D9D10002386"
+setenv subtest_list_non_replic "D9D11002390 D9D06002344 D9D12002401 D9D08002352 D9E03002436 C9D01002206 C9D12002472_2"
 #
 if ($?test_replic == 1) then
 	setenv subtest_list "$subtest_list_common $subtest_list_replic"
 else
 	setenv subtest_list "$subtest_list_common $subtest_list_non_replic"
-	if ("L" != $LFE) then
-		setenv subtest_list "$subtest_list $subtest_list_non_replic_FE"
-	endif
 endif
 setenv subtest_exclude_list ""
 # filter out subtests that cannot pass with MM

triggers/instream.csh
@@ -177,9 +177,7 @@ setenv subtest_list_non_replic "$subtest_list_non_replic maxparse_default maxpar
 setenv subtest_list_non_replic "$subtest_list_non_replic ztrigio chainVnest trigmprof nodztrigintrig trigzsyxplode"
 setenv subtest_list_non_replic "$subtest_list_non_replic zbspfortrig trigzsource trigxcalls trigreadonly nullsub"
 setenv subtest_list_non_replic "$subtest_list_non_replic gtm6901 gtm6901a gtm7678 gtm7877 gtm7877a gtm7083 zprint"
-setenv subtest_list_non_replic "$subtest_list_non_replic gtm8273 gtm8389 gtm8399 gtm8342 poundt ydb500 ydb596"
-
-setenv subtest_list_non_replic_FE "trigthrash"
+setenv subtest_list_non_replic "$subtest_list_non_replic gtm8273 gtm8389 gtm8399 gtm8342 poundt ydb500 ydb596 trigthrash"
 
 # Replic tests
 setenv subtest_list_replic     "replictrigger ztworm_restart_rlbk ztworepl trigreplstate trigdefnosync"
@@ -195,9 +193,6 @@ if ($?test_replic == 1) then
 	setenv subtest_list "$subtest_list_common $subtest_list_replic"
 else
 	setenv subtest_list "$subtest_list_common $subtest_list_non_replic"
-	if ("L" != $LFE) then
-		setenv subtest_list "$subtest_list $subtest_list_non_replic_FE"
-	endif
 endif
 
 # Exclude tests that we know will fail due to randomization changes

v50000/instream.csh
@@ -1,4 +1,18 @@
 #!/usr/local/bin/tcsh -f
+#################################################################
+#								#
+# Copyright (c) 2023 YottaDB LLC and/or its subsidiaries.	#
+# All rights reserved.						#
+#								#
+# Portions Copyright (c) Fidelity National			#
+# Information Services, Inc. and/or its subsidiaries.		#
+#								#
+#	This source code contains the intellectual property	#
+#	of its copyright holder(s), and is made available	#
+#	under a license.  If you do not know the terms of	#
+#	the license, please stop and do not read further.	#
+#								#
+#################################################################
 #
 # ------------------------------------------------------------------------------
 # for bugs fixed in V50000
@@ -9,27 +23,23 @@
 # D9E12002512 [Mohammad] DSE ADD -STAR gets SIGADRALN error
 # D9E04002439 [Vinaya]    C-stack leaks if a local variable is passed by reference through indirection
 # D9D12002412 [Layek] $t process is killed by system signal 11
-# C9E11002670 [Layek] zlink with longer than valid M name prints junk 
+# C9E11002670 [Layek] zlink with longer than valid M name prints junk
 # D9E05002460 [Vinaya] Journal file leak in source server on a crash restart
 # ------------------------------------------------------------------------------
 #
-# The first subtest to be included in subtest_list_replic below will necessitate 
-#	a) a change to suite.txt to enable the test to be run with replication 
+# The first subtest to be included in subtest_list_replic below will necessitate
+#	a) a change to suite.txt to enable the test to be run with replication
 #	b) a change to outref.txt to handle replication as well as non-replication test runs using ##TEST_AWK macros
 #
 echo "V50000 test starts..."
 setenv subtest_list_common " "
 setenv subtest_list_replic "D9E05002460 "
 setenv subtest_list_non_replic "C9E11002655 C9E04002574 D9E10002504 D9E12002512 D9E04002439 D9D12002412 C9E11002670 "
-setenv subtest_list_non_replic_FE " "
 #
 if ($?test_replic == 1) then
 	setenv subtest_list "$subtest_list_common $subtest_list_replic"
 else
 	setenv subtest_list "$subtest_list_common $subtest_list_non_replic"
-	if ("L" != $LFE) then
-		setenv subtest_list "$subtest_list $subtest_list_non_replic_FE"
-	endif
 endif
 $gtm_tst/com/submit_subtest.csh
 echo "V50000 test DONE."

v51000/instream.csh
@@ -1,8 +1,11 @@
 #!/usr/local/bin/tcsh -f
 #################################################################
 #								#
-# Copyright (c) 2017 YottaDB LLC and/or its subsidiaries.	#
+# Copyright (c) 2017-2023 YottaDB LLC and/or its subsidiaries.	#
 # All rights reserved.						#
+#								#
+# Portions Copyright (c) Fidelity National			#
+# Information Services, Inc. and/or its subsidiaries.		#
 #								#
 #	This source code contains the intellectual property	#
 #	of its copyright holder(s), and is made available	#
@@ -34,8 +37,8 @@
 #
 # ------------------------------------------------------------------------------
 #
-# The first subtest to be included in subtest_list_replic below will necessitate 
-#	a) a change to suite.txt to enable the test to be run with replication 
+# The first subtest to be included in subtest_list_replic below will necessitate
+#	a) a change to suite.txt to enable the test to be run with replication
 #	b) a change to outref.txt to handle replication as well as non-replication test runs using ##TEST_AWK macros
 #
 echo "V51000 test starts..."
@@ -43,15 +46,11 @@ setenv subtest_list_common " "
 setenv subtest_list_replic " "
 setenv subtest_list_non_replic "C9E12002698 D9F11002577 D9G01002587 D9F11002578 D9G01002589 D9G01002590 D9G03002599 D9F07002561 "
 setenv subtest_list_non_replic "$subtest_list_non_replic D9G01002592 mu_bkup_change_permission mu_bkup_stop D9E08002477"
-setenv subtest_list_non_replic_FE " "
 #
 if ($?test_replic == 1) then
 	setenv subtest_list "$subtest_list_common $subtest_list_replic"
 else
 	setenv subtest_list "$subtest_list_common $subtest_list_non_replic"
-	if ("L" != $LFE) then
-		setenv subtest_list "$subtest_list $subtest_list_non_replic_FE"
-	endif
 endif
 if ($?gtm_test_temporary_disable) then
 	setenv subtest_exclude_list "mu_bkup_stop"

v53000/instream.csh
@@ -1,4 +1,18 @@
 #!/usr/local/bin/tcsh -f
+#################################################################
+#								#
+# Copyright (c) 2023 YottaDB LLC and/or its subsidiaries.	#
+# All rights reserved.						#
+#								#
+# Portions Copyright (c) Fidelity National			#
+# Information Services, Inc. and/or its subsidiaries.		#
+#								#
+#	This source code contains the intellectual property	#
+#	of its copyright holder(s), and is made available	#
+#	under a license.  If you do not know the terms of	#
+#	the license, please stop and do not read further.	#
+#								#
+#################################################################
 #
 # ------------------------------------------------------------------------------
 # for bugs fixed in V53000
@@ -10,23 +24,19 @@
 # C9H09002900 [MikeC] View command should show a changed global directory without having to access a variable
 # ------------------------------------------------------------------------------
 #
-# The first subtest to be included in subtest_list_replic below will necessitate 
-#	a) a change to suite.txt to enable the test to be run with replication 
+# The first subtest to be included in subtest_list_replic below will necessitate
+#	a) a change to suite.txt to enable the test to be run with replication
 #	b) a change to outref.txt to handle replication as well as non-replication test runs using ##TEST_AWK macros
 #
 echo "V53000 test starts..."
 setenv subtest_list_common ""
 setenv subtest_list_replic "C9H09002905 "
 setenv subtest_list_non_replic "C9F06002736 D9H08002668 C9C11002165 C9H05002863 C9H09002900"
-setenv subtest_list_non_replic_FE " "
 #
 if ($?test_replic == 1) then
 	setenv subtest_list "$subtest_list_common $subtest_list_replic"
 else
 	setenv subtest_list "$subtest_list_common $subtest_list_non_replic"
-	if ("L" != $LFE) then
-		setenv subtest_list "$subtest_list $subtest_list_non_replic_FE"
-	endif
 endif
 $gtm_tst/com/submit_subtest.csh
 echo "V53000 test DONE."

v53001/instream.csh
@@ -1,4 +1,18 @@
 #!/usr/local/bin/tcsh -f
+#################################################################
+#								#
+# Copyright (c) 2023 YottaDB LLC and/or its subsidiaries.	#
+# All rights reserved.						#
+#								#
+# Portions Copyright (c) Fidelity National			#
+# Information Services, Inc. and/or its subsidiaries.		#
+#								#
+#	This source code contains the intellectual property	#
+#	of its copyright holder(s), and is made available	#
+#	under a license.  If you do not know the terms of	#
+#	the license, please stop and do not read further.	#
+#								#
+#################################################################
 #
 # ------------------------------------------------------------------------------
 # for bugs fixed in V53001
@@ -14,15 +28,11 @@ echo "V53001 test starts..."
 setenv subtest_list_common ""
 setenv subtest_list_replic ""
 setenv subtest_list_non_replic "C9H11002926 C9B03001664"
-setenv subtest_list_non_replic_FE " "
 #
 if ($?test_replic == 1) then
         setenv subtest_list "$subtest_list_common $subtest_list_replic"
 else
         setenv subtest_list "$subtest_list_common $subtest_list_non_replic"
-        if ("L" != $LFE) then
-                setenv subtest_list "$subtest_list $subtest_list_non_replic_FE"
-        endif
 endif
 $gtm_tst/com/submit_subtest.csh
 echo "V53001 test DONE."

v53002/instream.csh
@@ -1,7 +1,7 @@
 #!/usr/local/bin/tcsh -f
 #################################################################
 #								#
-# Copyright (c) 2018-2021 YottaDB LLC and/or its subsidiaries.	#
+# Copyright (c) 2018-2023 YottaDB LLC and/or its subsidiaries.	#
 # All rights reserved.						#
 #								#
 #	This source code contains the intellectual property	#
@@ -41,15 +41,11 @@ setenv subtest_list_replic "D9I05002682 C9I06003000"
 setenv subtest_list_non_replic "C9I02002956 C9D12002471 C9I02002963 D9I03002674 C9E08002617 D9I03002676 C9C11002181"
 setenv subtest_list_non_replic "$subtest_list_non_replic C9F06002736 C9I07003006 D9I07002688 D9I07002689 D9I07002692 D9I07002690"
 setenv subtest_list_non_replic "$subtest_list_non_replic C9I07003009"
-setenv subtest_list_non_replic_FE ""
 #
 if ($?test_replic == 1) then
 	setenv subtest_list "$subtest_list_common $subtest_list_replic"
 else
 	setenv subtest_list "$subtest_list_common $subtest_list_non_replic"
-	if ("L" != $LFE) then
-		setenv subtest_list "$subtest_list $subtest_list_non_replic_FE"
-	endif
 endif
 setenv subtest_exclude_list ""
 # filter out a specific subtest for some servers:

v54003/instream.csh
@@ -87,10 +87,8 @@ setenv subtest_list_non_replic "$subtest_list_non_replic dbidmismatch1 shmsemrem
 setenv subtest_list_non_replic "$subtest_list_non_replic C9K12003344 C9H04002840 etrapinfinalretry D9L06002816 transbig1 transbig2"
 setenv subtest_list_non_replic "$subtest_list_non_replic incrollback dsejnlrectype dbinit_honor_userwait C9I12003062 D9I08002697"
 setenv subtest_list_non_replic "$subtest_list_non_replic deferred_mupip_stop C9I05002991 C9K11003340 gtm6957 indrindrdo gtm6994 C9L06003421"
-setenv subtest_list_non_replic "$subtest_list_non_replic gtm6811"
-setenv subtest_list_replic     "D9L04002809 C9J03003101 maxregseqno resync"
-setenv subtest_list_non_replic_FE "C9L05003412 db1tb"
-setenv subtest_list_replic_FE  "D9I12002716"
+setenv subtest_list_non_replic "$subtest_list_non_replic gtm6811 C9L05003412 db1tb"
+setenv subtest_list_replic     "D9L04002809 C9J03003101 maxregseqno resync D9I12002716"
 
 # only HP-UX_IA64 and OSF1 uses caller_id stub so run this test only for these platform
 if (("HOST_HP-UX_IA64" == "$gtm_test_os_machtype") || ("HOST_HP-UX_PA_RISC" == "$gtm_test_os_machtype")) then
@@ -99,14 +97,8 @@ endif
 
 if ($?test_replic == 1) then
 	setenv subtest_list "$subtest_list_common $subtest_list_replic"
-	if ("L" != $LFE) then
-		setenv subtest_list "$subtest_list $subtest_list_replic_FE"
-	endif
 else
 	setenv subtest_list "$subtest_list_common $subtest_list_non_replic"
-	if ("L" != $LFE) then
-		setenv subtest_list "$subtest_list $subtest_list_non_replic_FE"
-	endif
 endif
 
 # Use $subtest_exclude_list to remove subtests that are to be disabled on a particular host or OS

commit e0247ebd4127692489c2a74f0322594a6d4aeec7

Fix incorrect line ordering in v44004/outref/outref.txt (regression in 00d6f2e4)
* Commit 00d6f2e4 moved the `C9D12002472_1` subtest from the non-replic and replic subtest list
  into the common list in `v44004/instream.csh`.

* But it forgot to correspondingly update `v44004/outref/outref.txt`. That is taken care of in
  this commit.

v44004/instream.csh
@@ -36,8 +36,8 @@
 #
 echo "V44004 test starts..."
 setenv subtest_list_common "C9D12002472_1"
-setenv subtest_list_replic "D9D10002386"
 setenv subtest_list_non_replic "D9D11002390 D9D06002344 D9D12002401 D9D08002352 D9E03002436 C9D01002206 C9D12002472_2"
+setenv subtest_list_replic "D9D10002386"
 #
 if ($?test_replic == 1) then
 	setenv subtest_list "$subtest_list_common $subtest_list_replic"