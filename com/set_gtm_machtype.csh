YottaDB/YDBTest

commit 81a7f6d84730a7468c95128ef46ac693664de857

[YDB#835] [#533] [V7.0-001] [GTM-4272] Add v70001/gtm4272 test to verify MUPIP messages use standard YottaDB message format
MUPIP BACKUP and RESTORE messages now follow standard YottaDB message format. Most of the messages affected
(listed in the error section of the release notes - some added, some changed) are already tested in
various tests in the test system. There were 4 messages that were not tested:

   1. BKUPFILEPERM - Backup file permissions on existing file do not permit replacement
   2. BKUPPROGRESS - Progress messages printed during backup - note debug-only
   3. BKUPRETRY - Run a broken backup that attempts to retry after an error
   4. CMDERR - Attempt to backup repl instance file to nonexistant file/directory

This test causes these 4 messages to be generated to verify #533. Note that there are conditions
where some of the subtests can be bypassed. Those conditions are:

   1. If $ydb_test_4g_db_blks is non-zero, we bypass Test #2 because this envvar confuses the
      mechanism that determines when the BKUPPROGRESS messages come out such that they never
      come out and make the test fail. So in that environment, this subtest is bypassed.
   2. If we are running a PRO build, Test #3 is bypassed because it is a white-box test that
      requires a debug build to work correctly.
   3. We bypass Test #4 on RHEL 7 systems as the error message has quotes around a bad file
      name we passed to it but those quotes are unicode chars on RHEL 7 (fancy open and close
      quote) that don't appear to be easily matchable with our reference files.

com/set_gtm_machtype.csh
@@ -158,6 +158,21 @@ endif
 # We cannot use mach_type since that has already been modified to say ARMVXL for ARMV7L or ARMV6L
 setenv real_mach_type `uname -m`
 
+# Determine if kernel and glibc versions support the use of copy_file_range() library routine. We are looking
+# for a kernel version >= 5.3 and a glibc version > 2.27.
+set kversion = `uname -r | cut -d "." -s -f "1,2"`	# Looks like '6.2.0-36-generic'
+echo "5.3\n${kversion}" | sort -CV			# Verify kernel version sorts at or after 5.3 (return=0 when sorted)
+if (0 == $status) then					# If release values already sorted, returns 0, then fetch glibc version
+    set gversion = `ldd --version | head -n1 | cut -d ')' -f2 | cut -d ' ' -f2`	# 1st line looks like 'ldd (Ubuntu GLIBC 2.35-0ubuntu3.4) 2.35'
+    if (1 == `echo "(2.27 < $gversion)" | bc`) then
+      	setenv ydb_test_copy_file_range_avail 1
+    else
+	setenv ydb_test_copy_file_range_avail 0
+    endif
+else
+    setenv ydb_test_copy_file_range_avail 0
+endif
+
 # Determine if big_files directory is available. Used later to decide whether a few subtests can be enabled or not.
 if (-e $gtm_test/big_files) then
 	setenv big_files_present 1

com/outref.awk
@@ -299,6 +299,11 @@ BEGIN {
 		else if ("ubuntu" == ENVIRON["gtm_test_linux_distrib"])
 			envir[no_options] = "UBUNTU_LINUX_AARCH64"
 	}
+	option_names[++no_options] = "SUSE_SLED"
+	if ("sled" == ENVIRON["gtm_test_linux_suse_distro"])
+		envir[no_options] = "SUSE_SLED"
+	else
+		envir[no_options] = "NON_SUSE_SLED"
 	option_names[++no_options] = "OS"
 	if ("arch" == ENVIRON["gtm_test_linux_distrib"])
 		envir[no_options] = "ARCH_LINUX"
@@ -312,6 +317,11 @@ BEGIN {
 		envir[no_options] = "RHEL_LINUX"
 	else if ("suse" == ENVIRON["gtm_test_linux_distrib"])
 		envir[no_options] = "SUSE_LINUX"
+	#
+	if ("suse" != ENVIRON["gtm_test_linux_distrib"]) {
+		options_names[++no_options] = "NOT-SUSE"
+		envir[no_options] = "NON_SUSE_LINUX"	# Used to help identify non-SUSE-SLED type systems
+	}
 	#
 	option_names[++no_options] = "gtm_test_singlecpu"
 	if (1 == ENVIRON["gtm_test_singlecpu"])
@@ -437,6 +447,12 @@ BEGIN {
 			envir[no_options] = "YDB_READLINE8_OR_MORE"
 	} else
 		envir[no_options] = "NON_YDB_READLINE"
+	#
+	option_names[++no_options] = "ydb_test_copy_file_range_avail"
+	if (1 == ENVIRON["ydb_test_copy_file_range_avail"])
+		envir[no_options] = "COPYFILERANGE_AVAIL"
+	else
+		envir[no_options] = "NO_COPYFILERANGE_AVAIL"
 	#
 	# For now set the [UPGRADE_DOWNGRADE_UNSUPPORTED] tag all the time as MUPIP UPGRADE/MUPIP DOWNGRADE functionality
 	# is not supported in V7 format database and likely will not be supported (by GT.M) in the future either.

v70001/instream.csh
@@ -19,6 +19,7 @@
 # gtm9452	[sam,hoyt]	CLOSE deviceparameter REPLACE overwrites an existing file, which RENAME does not
 # gtm8681	[estess]	Verify MUPIP BACKUP -RECORD stores the time of its start when it completes successfully
 # gtm4814	[estess]	Verify M-profiling (VIEW "TRACE") restored after ZSTEP
+# gtm4272       [estess]	Verify that MUPIP BACKUP displays information in standard GT.M messages format
 #----------------------------------------------------------------------------------------------------------------------------------
 
 echo "v70001 test starts..."
@@ -26,7 +27,7 @@ echo "v70001 test starts..."
 # List the subtests seperated by spaces under the appropriate environment variable name
 setenv subtest_list_common	""
 setenv subtest_list_non_replic	"gtm9213 gtm8010 gtm9452 gtm8681 gtm4814"
-setenv subtest_list_replic	""
+setenv subtest_list_replic	"gtm4272"
 
 if ($?test_replic == 1) then
 	setenv subtest_list "$subtest_list_common $subtest_list_replic"

v70001/u_inref/gtm4272.csh
@@ -1,0 +1,108 @@
+#################################################################
+#								#
+# Copyright (c) 2023 YottaDB LLC and/or its subsidiaries.	#
+# All rights reserved.						#
+#								#
+#	This source code contains the intellectual property	#
+#	of its copyright holder(s), and is made available	#
+#	under a license.  If you do not know the terms of	#
+#	the license, please stop and do not read further.	#
+#								#
+#################################################################
+#
+echo '# GTM-4272 - Verify MUPIP BACKUP displays information in standard GT.M messages format'
+echo '#'
+echo '# Release note:'
+echo '#'
+echo '# MUPIP BACKUP and RESTORE display information in standard GT.M messages format. The messages'
+echo '# display the full path when they include a file context. Please refer to the Error and Other'
+echo '# Messages section of these release notes for details on the standard messages. Previously, certain'
+echo '# MUPIP BACKUP and RESTORE messages did not follow the standard GT.M message format and/or did'
+echo '# not display the full path when there was a file context. (GTM-4272)'
+echo '#'
+echo '# Note that the existing tests in the test system tested several of errors mentioned in the errors'
+echo '# section of the release notes. Those are not tested here. The remaining errors that are tested'
+echo '# here are the following:'
+echo '#'
+echo '#   1. BKUPFILEPERM - Backup file permissions on existing file do not permit replacement'
+echo '#   2. BKUPPROGRESS - Progress messages printed during backup - note debug-only'
+echo '#   3. BKUPRETRY - Run a broken backup that attempts to retry after an error'
+echo '#   4. CMDERR - Attempt to backup repl instance file to nonexistent file/directory'
+echo '#'
+echo
+echo '# Create database'
+$gtm_tst/com/dbcreate.csh mumps
+echo
+echo '# Put a little something in the database so we have something to backup'
+$gtm_dist/mumps -dir <<EOF
+for i=1:1:100000 set ^a(\$justify(i,50))=\$justify(i,75)
+EOF
+#
+# Test #1
+#
+echo
+$echoline
+echo
+echo '# Test #1 - BKUPFILEPERM - backup file exists and permissions do not allow existing file replacement'
+echo '#   Command used: $MUPIP backup -replace DEFAULT mumps_backup.dat'
+echo
+echo '#   Create the backup directory and change its permissions'
+touch mumps_backup.dat
+chmod -w mumps_backup.dat
+echo
+echo '#   Attempt to backup DB into created file using command above - expect BKUPFILEPERM error'
+$MUPIP backup -replace DEFAULT mumps_backup.dat
+# Remove empty file
+chmod +w mumps_backup.dat
+rm mumps_backup.dat
+#
+# Test # 2
+#
+if (1 == $ydb_test_copy_file_range_avail) then
+    echo
+    $echoline
+    echo
+    echo '# Test # 2 - BKUPPROGRESS - run a backup that prints progress messages (grep from output file)'
+    echo '#   Command used: $MUPIP backup -showprogress DEFAULT mumps_backup.dat'
+    # These progress messages aren't very interesting due to the smallness of the backup but it shows it is functional. Note since
+    # the BKUPPROGRESS message is not deterministic (in terms of how many we get), we write the output to the file and select the
+    # first message that occurs. We only need to see one to prove these messages exist.
+    $MUPIP backup -showprogress DEFAULT mumps_backup.dat >& mupip_backup_showprogress.log
+    $grep BKUPPROGRESS mupip_backup_showprogress.log | head -n1
+    rm mumps_backup.dat
+endif
+#
+# Test # 3 uses white box testing so only run this sub-test if this is a debug build
+#
+if ("dbg" == "$tst_image") then
+    echo
+    $echoline
+    echo
+    echo '# Test #3 - BKUPRETRY - Attempt backup with whitebox test WBTEST_BACKUP_FORCE_MV_RV enabled to cause an error and generate a retry'
+    echo '#   Command used: $MUPIP backup -retry=2 DEFAULT mumps_backup.dat'
+    setenv gtm_white_box_test_case_enable 1
+    setenv gtm_white_box_test_case_number 175	# WBTEST_BACKUP_FORCE_MV_RV to generate an error
+    $MUPIP backup -retry=2 DEFAULT mumps_backup.dat
+    unsetenv gtm_white_box_test_case_enable
+    unsetenv gtm_white_box_test_case_number
+endif
+#
+# Test #4
+#
+# We restrict this test from running on RHEL 7.9 as the error message it prints about the bad file is surrounded by unicode single
+# quotes in that version which we don't consider worth matching given this is old RHEL 7 in a reference file due to the quote's
+# multi-byte representation so this subtest is bypassed under RHEL 7.9.
+if (("rhel" != $gtm_test_linux_distrib) || ("7.9" != $gtm_test_linux_version)) then
+    echo
+    $echoline
+    echo
+    echo '# Test #4 - CMDERR - Backup the replinstance file to a file under a non-existent directory to generate CMDERR'
+    echo '#   Command used: $MUPIP backup -replinstance=/tmp/nonexist42/efgh.repl'
+    $MUPIP backup -replinstance=/tmp/nonexist42/efgh.repl
+endif
+#
+echo
+$echoline
+echo
+echo '# Validate DB'
+$gtm_tst/com/dbcheck.csh

commit 4461e2f9a74edaf462bb96a5b1dbed3460b9f778

[YDB#835] [#533] [V7.0-001] [GTM-4272] Fix r124 test failure on RHEL systems due to com/set_gtm_machtype.csh regression in 81a7f6d8
Our Redhat 7 and 8 systems both failed in r124/instream.csh where it called com/set_gtm_machtype.csh.
The problem was in determining whether the copy_file_range() entry point was available or not
(kernel version >= 5.3 and glibc version > 2.27). When the version being considered is less-than 5.3,
the sort command used returned a non-zero return code and on these RedHat systems, that would cause
the script to abort. The script was changed so we no longer generate any non-zero return codes when
making this determination. My thanks to @nars1 for suggesting the fix.

This issue is discussed with more context here: https://gitlab.com/YottaDB/DB/YDBTest/-/merge_requests/1747#note_1668728283

com/set_gtm_machtype.csh
@@ -161,15 +161,20 @@ setenv real_mach_type `uname -m`
 # Determine if kernel and glibc versions support the use of copy_file_range() library routine. We are looking
 # for a kernel version >= 5.3 and a glibc version > 2.27.
 set kversion = `uname -r | cut -d "." -s -f "1,2"`	# Looks like '6.2.0-36-generic'
-echo "5.3\n${kversion}" | sort -CV			# Verify kernel version sorts at or after 5.3 (return=0 when sorted)
-if (0 == $status) then					# If release values already sorted, returns 0, then fetch glibc version
+set base="5.3"
+set min = `echo "$base\n${kversion}" | sort -V | head -1`
+if ("$min" == "$base") then
+    # Kernel version is >= 5.3
     set gversion = `ldd --version | head -n1 | cut -d ')' -f2 | cut -d ' ' -f2`	# 1st line looks like 'ldd (Ubuntu GLIBC 2.35-0ubuntu3.4) 2.35'
     if (1 == `echo "(2.27 < $gversion)" | bc`) then
+        # glibc version is > 2.27
       	setenv ydb_test_copy_file_range_avail 1
     else
+	# glibc version is <=  2.27
 	setenv ydb_test_copy_file_range_avail 0
     endif
 else
+    # Kernel version is < 5.3
     setenv ydb_test_copy_file_range_avail 0
 endif
 