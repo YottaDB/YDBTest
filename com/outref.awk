YottaDB/YDBTest

commit d1f968e652768c4233f57eac08146c60b642a8d4

[YDB#936] [V70004] Fix tls_1/errors subtest failure due to GTM-F158404 changes
Background
----------
* The `tls_1/errors` subtest failed with the following diff after GT.M V7.0-004 was merged.

  ```diff
  122,129c122,136
  < ----------
  < Error W-TLSCONVSOCK seen in ##FILTERED##RCVR_##TIMESTAMP##.log as expected:
  < %YDB-W-TLSCONVSOCK, Failed to convert Unix TCP/IP socket to TLS/SSL aware socket
  < ----------
  < ----------
  < Error YDB-I-TEXT seen in ##FILTERED##RCVR_##TIMESTAMP##.log as expected:
  < %YDB-I-TEXT, Configuration parameter `tls.dh512' not specified.
  < ----------
  ---
  > TEST-E-NOTFOUND message TLSCONVSOCK not found in ##FILTERED##RCVR_##TIMESTAMP##.log after 120 secs
  ```

Issue
-----
* It is the following stage of the test that failed.

  ```
  TEST CASE 6: Test that specifying only one of dh* parameters issues an appropriate error.
  ```

* The test expects the receiver server log to have a `TLSCONVSOCK` error. But it did not.

* And this is because of the below GT.M V7.0-004 change.

  http://tinco.pair.com/bhaskar/gtm/doc/articles/GTM_V7.0-004_Release_Notes.html#GTM-F158404

  When using OpenSSL 3.0+, the GT.M TLS example plugin uses automatic Diffie-Hellman parameters. This
  action is the recommended behavior from the OpenSSL project. Previously, the plugin allowed
  Diffie-Hellman parameter configuration files which required the user understand how those parameter
  configuration options affected the Diffie-Hellman key exchange.

* The encryption plugin no longer requires Diffie-Hellman parameters in the configuration files.

Fix
---
* The test has been reworked to expect no `TLSCONVSOCK` error. It now removes `dh512` or `dh1024`
  or both from the configuration file (does this randomly) and expects no errors in all cases.

* When this test is run with a YottaDB build before GT.M V7.0-004 was merged, one can see the
  following failure diff which confirms the test does its job of testing the Diffie-Hellman

tls/u_inref/errors.csh
@@ -4,7 +4,7 @@
 # Copyright (c) 2013-2016 Fidelity National Information		#
 # Services, Inc. and/or its subsidiaries. All rights reserved.	#
 #                                                               #
-# Copyright (c) 2017-2019 YottaDB LLC and/or its subsidiaries.	#
+# Copyright (c) 2017-2024 YottaDB LLC and/or its subsidiaries.	#
 # All rights reserved.						#
 #								#
 #	This source code contains the intellectual property	#
@@ -284,25 +284,29 @@ source $gtm_tst/$tst/u_inref/filter_TLSHANDSHAKE.csh $time_src $time_rcvr "ALLOW
 $MSR STOP INST1 INST2
 
 echo
-echo "TEST CASE 6: Test that specifying only one of dh* parameters issues an appropriate error."
+echo "TEST CASE 6: Test that not specifying both dh512 and dh1024 parameters does NOT issue any error."
+echo "This used to issue a TLSCONVSOCK error but that stopped after GTM-F158404 in GT.M V7.0-004"
 echo
 # First fix the config file containing the expired certificate.
 cp $gtmcrypt_config $gtmcrypt_config.bad3
 cp $gtmcrypt_config.good $gtmcrypt_config
 
-# If we wipe of dh512 from the config file, that would cause both the replication servers to error out with TLSCONVSOCK. But,
-# sometimes, if the timing is right, only one of them would fail and the other would just experience connection reset. To test
-# this deterministically, let's trigger the error only on INSTANCE1 by having a different config file for INSTANCE1.
-sed 's/dh512.*//' $gtmcrypt_config.good >&! $gtmcrypt_config
-$MSR STARTSRC INST1 INST2 RP
-$MSR STARTRCV INST1 INST2
-get_msrtime
-set rcv_logfile = RCVR_${time_msr}.log
-
-$MSR RUN INST2 "set msr_dont_trace; $gtm_tst/com/wait_for_log.csh -log $rcv_logfile -message TLSCONVSOCK"
-$MSR RUN INST2 "set msr_dont_trace; $msr_err_chk $rcv_logfile 'W-TLSCONVSOCK' 'YDB-I-TEXT'"
-$gtm_tst/com/knownerror.csh $msr_execute_last_out "YDB-W-TLSCONVSOCK"
-
+echo "# Randomly remove dh512 OR dh1024 OR both from the config file."
+echo "# We expect the source and receiver server to still connect fine."
+set rand = `$gtm_tst/com/genrandnumbers.csh 1 0 2`      # choose 1 random number between 0 and 2 (both included)
+if (0 == $rand) then
+	# Remove just dh512
+	sed 's/dh512.*//;' $gtmcrypt_config.good >&! $gtmcrypt_config
+else if (1 == $rand) then
+	# Remove just dh1024
+	sed 's/dh1024.*//;' $gtmcrypt_config.good >&! $gtmcrypt_config
+else
+	# Remove BOTH dh512 and dh1024
+	sed 's/dh512.*//;s/dh1024.*//;' $gtmcrypt_config.good >&! $gtmcrypt_config
+endif
+$MSR START INST1 INST2 RP
+$MSR RUN INST1 "$gtm_tst/com/simpleinstanceupdate.csh 10"
+$MSR SYNC INST1 INST2	# to ensure replication works fine
 $MSR STOP INST1 INST2
 
 echo

commit 78732a3bf1456b4a67305a99eaea59d0c9afb74d

[YDB#936] [V70004] Fix tls_1/errors subtest failure due to GTM-F158404 changes if OpenSSL version < 3.0
Background
----------
* The `tls_1/errors` subtest failed with the following diff on some systems.

  ```diff
  177a178,179
  > ##HOST##:##TEST_PATH##/tls_1/errors/instance2/RCVR_08_58_54_39.log
  > %YDB-W-TLSCONVSOCK, Failed to convert Unix TCP/IP socket to TLS/SSL aware socket
  ```

Issue
-----
* One thing that was noticed was that only systems that had OpenSSL 3.0 or higher passed
  the test. All systems that had OpenSSL 1.1 (i.e. less than OpenSSL 3.0) failed the test.

* The cause turns out to be a regression in commit d1f968e6. It had reworked the test to
  take GTM-F158404 changes into account.

* But it did not take note of the release note where `When using OpenSSL 3.0+` is also mentioned.

* That is, the new behavior (no TLSCONVSOCK error) is expected only when using OpenSSL 3.0
  or higher.

Fix
---
* The `TEST CASE 6` stage of this subtest (which is what failed above) is now enabled only
  on OpenSSL 3.0+ systems. And disabled otherwise.

com/outref.awk
@@ -375,6 +375,12 @@ BEGIN {
 	else
 		envir[no_options] = "TLS1.3MINUS"
 	#
+	option_names[++no_options] = "ydb_test_openssl3_plus"
+	if (1 == ENVIRON["ydb_test_openssl3_plus"])
+		envir[no_options] = "OPENSSL3PLUS"
+	else
+		envir[no_options] = "OPENSSL3MINUS"
+	#
 	option_names[++no_options] = "gtm_test_dynamic_literals"
 	if ("DYNAMIC_LITERALS" == ENVIRON["gtm_test_dynamic_literals"])
 		envir[no_options] = "DYNAMIC_LITERALS"

com/set_gtm_machtype.csh
@@ -196,6 +196,8 @@ endif
 # If $ydb_dist/plugin/libgtmtls.so is not present, then assume openssl version is < 1.1.1 (i.e. not tls 1.3).
 # Note use '|&' coming out of ldd because on Alpine, ldd generates extra warning messages about gtm_malloc/free not
 # being satisfied.
+setenv ydb_test_openssl3_plus 0
+setenv ydb_test_tls13_plus 0
 if (-e $ydb_dist/plugin/libgtmtls.so) then
 	# Previously we used to do "strings libssl.so" and search for OpenSSL version number.
 	# But that did not work on Ubuntu 21.04 where OpenSSL version was 1.1.1j.
@@ -207,11 +209,11 @@ if (-e $ydb_dist/plugin/libgtmtls.so) then
 	set opensslver = `strings $libcryptopath | grep -w '^OpenSSL [0-9]' | awk '{print $2}' | sort -r | head -1`
 	if ( `expr "$opensslver" \>= "1.1.1"` ) then
 		setenv ydb_test_tls13_plus 1
-	else
-		setenv ydb_test_tls13_plus 0
 	endif
-else
-	setenv ydb_test_tls13_plus 0
+	set opensslmajorver = `echo $opensslver | sed 's/\..*//;'`
+	if ( `expr "$opensslmajorver" \>= "3"` ) then
+		setenv ydb_test_openssl3_plus 1
+	endif
 endif
 set anyerror
 ##### HOST SPECIFIC FUNNIES ####

tls/u_inref/errors.csh
@@ -283,31 +283,35 @@ source $gtm_tst/$tst/u_inref/filter_TLSHANDSHAKE.csh $time_src $time_rcvr "ALLOW
 
 $MSR STOP INST1 INST2
 
-echo
-echo "TEST CASE 6: Test that not specifying both dh512 and dh1024 parameters does NOT issue any error."
-echo "This used to issue a TLSCONVSOCK error but that stopped after GTM-F158404 in GT.M V7.0-004"
-echo
-# First fix the config file containing the expired certificate.
-cp $gtmcrypt_config $gtmcrypt_config.bad3
-cp $gtmcrypt_config.good $gtmcrypt_config
+# TEST CASE 6 requires an OpenSSL 3.0+ system (TLSCONVSOCK error is issued otherwise)
+# So run this stage of the test only if YottaDB TLS plugin was built with OpenSSL 3.0 or higher.
+if ($ydb_test_openssl3_plus) then
+	echo
+	echo "TEST CASE 6: Test that not specifying both dh512 and dh1024 parameters does NOT issue any error."
+	echo "This used to issue a TLSCONVSOCK error but that stopped after GTM-F158404 in GT.M V7.0-004"
+	echo
+	# First fix the config file containing the expired certificate.
+	cp $gtmcrypt_config $gtmcrypt_config.bad3
+	cp $gtmcrypt_config.good $gtmcrypt_config
 
-echo "# Randomly remove dh512 OR dh1024 OR both from the config file."
-echo "# We expect the source and receiver server to still connect fine."
-set rand = `$gtm_tst/com/genrandnumbers.csh 1 0 2`      # choose 1 random number between 0 and 2 (both included)
-if (0 == $rand) then
-	# Remove just dh512
-	sed 's/dh512.*//;' $gtmcrypt_config.good >&! $gtmcrypt_config
-else if (1 == $rand) then
-	# Remove just dh1024
-	sed 's/dh1024.*//;' $gtmcrypt_config.good >&! $gtmcrypt_config
-else
-	# Remove BOTH dh512 and dh1024
-	sed 's/dh512.*//;s/dh1024.*//;' $gtmcrypt_config.good >&! $gtmcrypt_config
+	echo "# Randomly remove dh512 OR dh1024 OR both from the config file."
+	echo "# We expect the source and receiver server to still connect fine."
+	set rand = `$gtm_tst/com/genrandnumbers.csh 1 0 2`      # choose 1 random number between 0 and 2 (both included)
+	if (0 == $rand) then
+		# Remove just dh512
+		sed 's/dh512.*//;' $gtmcrypt_config.good >&! $gtmcrypt_config
+	else if (1 == $rand) then
+		# Remove just dh1024
+		sed 's/dh1024.*//;' $gtmcrypt_config.good >&! $gtmcrypt_config
+	else
+		# Remove BOTH dh512 and dh1024
+		sed 's/dh512.*//;s/dh1024.*//;' $gtmcrypt_config.good >&! $gtmcrypt_config
+	endif
+	$MSR START INST1 INST2 RP
+	$MSR RUN INST1 "$gtm_tst/com/simpleinstanceupdate.csh 10"
+	$MSR SYNC INST1 INST2	# to ensure replication works fine
+	$MSR STOP INST1 INST2
 endif
-$MSR START INST1 INST2 RP
-$MSR RUN INST1 "$gtm_tst/com/simpleinstanceupdate.csh 10"
-$MSR SYNC INST1 INST2	# to ensure replication works fine
-$MSR STOP INST1 INST2
 
 echo
 echo "TEST CASE 7: Test that revoked certificates issues appropriate errors."