YottaDB/YDBTest

commit 2cb49ad0423d4d4d2d00ca9ce34cd4d0bc7c8b81

[#731] Ensure test output directory name is unique when multiple gtmtest invocations occur in the same second.
Implement this using mktemp to append a 3-character random code onto the end of the directory name.

com/gtmtest.csh
@@ -671,9 +671,9 @@ if ("ATL" != "$gtm_server_location") then
 	setenv gtm_test_port_range `expr $gtm_test_server_serial_no - 40`
 endif
 if (0 > $gtm_test_port_range) setenv gtm_test_port_range 00
-setenv gtm_tst_out tst_${tst_ver}_${gtm_exe:t}_${gtm_test_server_serial_no}_${test_dirn}
+setenv gtm_tst_out `mktemp -d $tst_dir/tst_${tst_ver}_${gtm_exe:t}_${gtm_test_server_serial_no}_${test_dirn}_XXX`  # unique
+setenv gtm_tst_out `echo $gtm_tst_out | sed -e "s|$tst_dir/||"`   # gtm_tst_out shouldn't have $tst_dir/ on the front
 
-mkdir -p $tst_dir/$gtm_tst_out
 if ($status == 0) then
 	echo "Created ${tst_dir}/$gtm_tst_out"
 	chmod g+w $tst_dir/$gtm_tst_out

com/settest.csh
@@ -78,7 +78,8 @@ setenv gtm_test_autorelink_support 1
 setenv gtmtest_noxendian 1
 set gtm_test_server_serial_no = 00
 set test_dirn = `date +%y%m%d_%H%M%S`
-setenv gtm_tst_out tst_${tst_ver}_${gtm_exe:t}_${gtm_test_server_serial_no}_${test_dirn}
+setenv gtm_tst_out `mktemp -d $tst_dir/tst_${tst_ver}_${gtm_exe:t}_${gtm_test_server_serial_no}_${test_dirn}_XXX`  # unique
+setenv gtm_tst_out `echo $gtm_tst_out | sed -e "s|$tst_dir/||"`   # gtm_tst_out shouldn't have $tst_dir/ on the front
 
 # Settings from submit.csh
 limit coredumpsize unlimited

commit 2417d61befb1e3e81be76ca9c0b084ab10f84076

Fix v54002/C9B11001805 test failure due to regression in 2cb49ad0 (mkdir vs mktemp group permissions)
Error:
* @nars1 While running a D_ALL, I noticed the `v54002/C9B11001805` subtest fail reliably and tracked it down to a regression in !2025.
Line 676 of gtmtest.csh previously used to do a `mkdir -p` whereas line 674 now does a `mktemp -d`. The resulting directory previously
had `rx` permissions for group whereas it now does not. And this causes a `cd` to the directory when done as userid `gtmtest1`
(which various tests do for multi-user testing) to result in a `Permission denied` error.
[Related thread here](https://gitlab.com/YottaDB/DB/YDBTest/-/merge_requests/2025#note_1989403221).

Fixed by explicitly setting directory permissions to 775, just like `mkdir -p` used to do.

com/gtmtest.csh
@@ -672,6 +672,7 @@ if ("ATL" != "$gtm_server_location") then
 endif
 if (0 > $gtm_test_port_range) setenv gtm_test_port_range 00
 setenv gtm_tst_out `mktemp -d $tst_dir/tst_${tst_ver}_${gtm_exe:t}_${gtm_test_server_serial_no}_${test_dirn}_XXX`  # unique
+chmod 755 $gtm_tst_out   # ensures directory is accessible by other userids in the same group (e.g. gtmtest1)
 setenv gtm_tst_out `echo $gtm_tst_out | sed -e "s|$tst_dir/||"`   # gtm_tst_out shouldn't have $tst_dir/ on the front
 
 if ($status == 0) then

com/settest.csh
@@ -79,6 +79,7 @@ setenv gtmtest_noxendian 1
 set gtm_test_server_serial_no = 00
 set test_dirn = `date +%y%m%d_%H%M%S`
 setenv gtm_tst_out `mktemp -d $tst_dir/tst_${tst_ver}_${gtm_exe:t}_${gtm_test_server_serial_no}_${test_dirn}_XXX`  # unique
+chmod 755 $gtm_tst_out   # ensures directory is accessible by other userids in the same group (e.g. gtmtest1)
 setenv gtm_tst_out `echo $gtm_tst_out | sed -e "s|$tst_dir/||"`   # gtm_tst_out shouldn't have $tst_dir/ on the front
 
 # Settings from submit.csh