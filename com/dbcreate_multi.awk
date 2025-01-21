YottaDB/YDBTest

commit 927380bca1e6e59dc926f4b343735fe3253b5aae

Fix rare v63000/gtm8423 subtest failure (%GDE-E-KEYWRDBAD, STATS is not a valid qualifier)
* The above subtest failed with the following diff in one rare test run.

  ```diff
  1a2,5
  > ---------------------------------------------------------------
  > TEST-E-DBCREATE, errors seen in the log file dbcreate.out:
  > %GDE-E-KEYWRDBAD, STATS is not a valid qualifier in this context
  ```

* This subtest randomly chose the following prior version to do the dbcreate.

  ```sh
  $ cat priorver.txt
  V63000A
  ```

* Commit f8c8d64c had made a change to add a `change -region -nostats` in the list of GDE commands
  in `com/dbcreate_multi.awk`.

* But `-stats` became a supported qualifier in the `change -region` command only from V63001.
  Therefore the above error is expected given V63000A is prior to V63001.

* The fix is to check if the minor ver is greater than or equal to `63001` and only then
  add the `change -region -nostats` command to the list of GDE commands in `com/dbcreate_multi.awk`.

com/dbcreate_multi.awk
@@ -3,7 +3,7 @@
 # Copyright (c) 2002-2016 Fidelity National Information		#
 # Services, Inc. and/or its subsidiaries. All rights reserved.	#
 #								#
-# Copyright (c) 2017-2024 YottaDB LLC and/or its subsidiaries.	#
+# Copyright (c) 2017-2025 YottaDB LLC and/or its subsidiaries.	#
 # All rights reserved.						#
 #								#
 #	This source code contains the intellectual property	#
@@ -302,6 +302,7 @@ function print_block(segname,regname){
       if (value["test_stats"])
 		print "change -region " regname " " value["test_stats"]
       major_ver = gensub(/^.*V([0-9])[0-9]+.*/,"\\1","g",ENVIRON["gtm_dist"])
+      minor_ver = gensub(/^.*V[0-9]([0-9]+).*/,"\\1","g",ENVIRON["gtm_dist"])
       # QDBRUNDOWN and INST_FREEZE_ON_ERROR options are not available for versions older than V60000
       if (6 <= major_ver)
       {
@@ -310,7 +311,7 @@ function print_block(segname,regname){
 	  if (value["qdbrundown"])
 		print "change -region " regname " " value["qdbrundown"]
 	  # DEFER_ALLOCATE option is available from V62002
-	  if ((6 < major_ver) || ((6 == major_ver) && (2002 <= gensub(/^.*V[0-9]([0-9]+).*/,"\\1","g",ENVIRON["gtm_dist"]))))
+	  if ((6 < major_ver) || ((6 == major_ver) && (2002 <= full_ver)))
 	  {
 	      if (value["defer_allocate"])
 		  print "change -segment " segname " " value["defer_allocate"]
@@ -319,7 +320,7 @@ function print_block(segname,regname){
 	  }
       }
       # ASYNCIO option is only available from V63001 onwards
-      if ((6 < major_ver) || ((6 == major_ver) && (3001 <= gensub(/^.*V[0-9]([0-9]+).*/,"\\1","g",ENVIRON["gtm_dist"]))))
+      if ((6 < major_ver) || ((6 == major_ver) && (3001 <= minor_ver)))
       {
           # Even though -asyncio was inherited from the gtm_test_asyncio env var after checking the access method env var is BG
           # at the time of do_random_settings.csh, it is possible a test (e.g. mm/basic subtest) overrides it by setting the
@@ -336,7 +337,11 @@ function print_block(segname,regname){
 	      # issues when trying to mmap() this huge .gst file (e.g. "%SYSTEM-E-ENO12, Cannot allocate memory" error).
 	      # Therefore, disable statistics sharing for this database by setting "-nostats" for all regions in case
 	      # this is a replication test (identified by the "test_replic" env var being 1 or MULTISITE).
-              print "change -region " regname " -nostats"
+
+	      # Note also that NOSTATS started being supported only from V63001 and so disable adding this if
+	      # the current version is less than V63001.
+		if ((6 < major_ver) || ((6 == major_ver) && (3001 <= minor_ver)))
+			print "change -region " regname " -nostats"
       }
       if  ("NULL" == value["null_subscripts"]) # this NULL has nothing to do with "NULL"_subscripts, it is dbcreate_multi.awk's "NULL"
 		value["null_subscripts"]="ALWAYS"

commit 44f1d2cf17a19715215d028a7abb19ae89fadd69

Fix v62002/gtm5894 subtest failure when a V6 version is used in dbcreate.csh
* The above subtest started failing with the following diff in some cases.

  ```diff
  7,39c7
  .
  .
  ---
  > TEST-E-FAIL database was not fully allocated. Check the size of the mumps.dat file.
  ```

* The failure happened only when the random option `V6DB:` had a non-empty version (i.e. the
  env var `gtm_test_use_V6_DBs` had a value of 1 in `settings.csh`).

* This turned out to be a regression in commit 927380bc. It replaced many `gensub()` function
  invocations in `com/dbcreate_multi.awk` by instead storing the result in a variable `minor_ver`
  and using that in all those places.

* But it had a typo in one place where the variable `full_ver` was being used. This caused
  the above subtest to fail because the `if` line where `full_ver` was used evaluated to FALSE
  and skipped generating a `change -segment -nodefer_allocate` command in turn changing the
  allocated size of the file (which the above subtest cared about).

* The typo is fixed in this commit.

com/dbcreate_multi.awk
@@ -311,7 +311,7 @@ function print_block(segname,regname){
 	  if (value["qdbrundown"])
 		print "change -region " regname " " value["qdbrundown"]
 	  # DEFER_ALLOCATE option is available from V62002
-	  if ((6 < major_ver) || ((6 == major_ver) && (2002 <= full_ver)))
+	  if ((6 < major_ver) || ((6 == major_ver) && (2002 <= minor_ver)))
 	  {
 	      if (value["defer_allocate"])
 		  print "change -segment " segname " " value["defer_allocate"]