YottaDB/YDBTest

commit 51fbf1cd88c1ee0a3e65b264560bea6ebdf574b7

[#787] Fix various tests that don't run under docker
- Fix compilation errors due to missing include files. This was due to
  typos (extra spaces) in `docker/build_and_install_yottadb.csh`. While
  investigating this, it turned out that YDBDevOps also copied in
  `xfer_desc.i` and `gtm_threadgbl_deftypes.h`; these were added too.
  This fixes tests: `mupip/mu_jnl_extract_collated_globals` and `v63013/gtm9277`.
- Test `r136/ydb943` was probably fixed in
  ffc18917ead4bb9a2f2077a79e20f9d9179c1d6e, but wasn't re-enabled in
  `ffc18917ead4bb9a2f2077a79e20f9d9179c1d6e`. Now done.
- Add extra versions to the docker image.
	- r1.38 pro/dbg (for a V6 database for tests)
	- r2.02 dbg (as the default switching program to use a previous
	  database uses the same image (dbg) as the tests currently
	  running, and we use dbg right now for the tests).
	- `docker/build_and_install_yottadb.csh` modified to allow
	  pro/dbg to co-exist (and also r1.38 to be installed), but it's
	  inefficient right now, as it creates all the include files
	  twice, which means in the Docker they are actually stored
	  twice (Docker overlays file systems - the duplication is about
	  25MB for each overlay); so it's acceptable for now.
	- This fixes:
		- r124/ydb333
		- r124/ydb114
		- r130/ydb484
		- r130/ydb493
		- r130/ydb494
		- r130/ydb388
		- encryption/mu_extract_enc
- Add explicit path for `mupip` for `r124/ydb359`; the test currently
  assumes that `mupip` is in $PATH, which is not necessarily the case.
- Update `docker/pipeline-test-ydb.csh` to remove tests which now can
  run.
- `docker/pipeline-test-ydbtest.csh` has minor changes to tell us
  which tests are selected for running, as it isn't clear from looking
  at the log right now.
- Docker image size: 3.57GB -> 4.36 GB

docker/build_and_install_yottadb.csh
@@ -1,7 +1,7 @@
 #!/bin/tcsh -e
 #################################################################
 #								#
-# Copyright (c) 2021-2024 YottaDB LLC and/or its subsidiaries.	#
+# Copyright (c) 2021-2025 YottaDB LLC and/or its subsidiaries.	#
 # All rights reserved.						#
 #								#
 #	This source code contains the intellectual property	#
@@ -117,7 +117,7 @@ mkdir -p $gtm_root/$verno
 # $gtm_root/$verno have anything inside already just we want to clean it first
 # then we will unset nonomatch after this line is done
 set nonomatch
-rm -rf $gtm_root/$verno/*
+rm -rf $gtm_root/$verno/$dbgpro/*
 unset nonomatch
 
 mkdir -p $source_dir/$dbgpro
@@ -130,21 +130,26 @@ endif
 make -j `getconf _NPROCESSORS_ONLN` install
 if ( $is_gtm ) then
 	pushd lib/*/*
-	./gtminstall --installdir=$gtm_root/$verno/$dbgpro --utf8 --keep-obj --ucaseonly-utils --prompt-for-group
+		if ($verno =~ "*V7*") then
+			# the --utf8 option does not accept an ICU version in GT.M V7.0-000 or later
+			./gtminstall --installdir=$gtm_root/$verno/$dbgpro --utf8 --keep-obj --ucaseonly-utils --prompt-for-group
+		else
+			./gtminstall --installdir=$gtm_root/$verno/$dbgpro --utf8 default --keep-obj --ucaseonly-utils --prompt-for-group
+		endif
 	popd
 else
 	pushd yottadb_r*
-	./ydbinstall --installdir=$gtm_root/$verno/$dbgpro --utf8 --keep-obj --ucaseonly-utils --prompt-for-group
+		if ($verno =~ "*V7*") then
+			# the --utf8 option does not accept an ICU version in GT.M V7.0-000 or later
+			./ydbinstall --installdir=$gtm_root/$verno/$dbgpro --utf8 --keep-obj --ucaseonly-utils --prompt-for-group
+		else
+			./ydbinstall --installdir=$gtm_root/$verno/$dbgpro --utf8 default --keep-obj --ucaseonly-utils --prompt-for-group
+		endif
 	popd
 endif
 mkdir -p $gtm_root/$verno/$dbgpro/obj
 find . -name '*.a' -exec cp {} $gtm_root/$verno/$dbgpro/obj \;
 mkdir -p $gtm_root/$verno/tools
-
-# Sudo tests require `ydbinstall` that is part of the intermediate build process (yuck); so leave the build directory around for master commit
-if ( "$git_tag" != "master" ) then
-	rm -rf $source_dir/$dbgpro
-endif
 
 cd $source_dir
 cp sr_unix/*.awk $gtm_root/$verno/tools
@@ -155,56 +160,42 @@ rm -f $gtm_root/$verno/tools/setactive{,1}.csh
 
 set machtype=`uname -m`
 foreach ext (c s msg h si)
-        if (($ext == "h") || ($ext == "si")) then
-                set dir = "inc"
-        else
-                set dir = "src"
-        endif
+	if (($ext == "h") || ($ext == "si")) then
+		set dir = "inc"
+	else
+		set dir = "src"
+	endif
 	mkdir -p $gtm_ver/$dir
 
-	# Check if file extension exists first then do cp
-	set filecount=`find "sr_port/ "-maxdepth 1 -name "*.$ext" | wc -l`
-	if ($filecount > 0) then
-		cp -pa sr_port/*.$ext $gtm_ver/$dir/
-	endif
-
-	set filecount=`find "sr_port_cm/" -maxdepth 1 -name "*.$ext" | wc -l`
-	if ($filecount > 0) then
-		cp -pa sr_port_cm/*.$ext $gtm_ver/$dir/
-	endif
-
-        set filecount=`find "sr_unix/" -maxdepth 1 -name "*.$ext" | wc -l`
-	if ($filecount > 0) then
-		cp -pa sr_unix/*.$ext $gtm_ver/$dir/
-	endif
-
-        set filecount=`find "sr_unix_cm/" -maxdepth 1 -name "*.$ext" | wc -l`
-	if ($filecount > 0) then
-		cp -pa sr_unix_cm/*.$ext $gtm_ver/$dir/
-	endif
-
-        set filecount=`find "sr_unix_gnp/" -maxdepth 1 -name "*.$ext" | wc -l`
-	if ($filecount > 0) then
-		cp -pa sr_unix_gnp/*.$ext $gtm_ver/$dir/
-	endif
-
-        if (${machtype} == "x86_64") then
-		set filecount=`find "sr_x86_regs/ "-maxdepth 1 -name "*.$ext" | wc -l`
-		if ($filecount > 0) then
-			cp -pa sr_x86_regs/*.$ext $gtm_ver/$dir/
+	foreach sr_dir (sr_port sr_port_cm sr_unix sr_unix_cm sr_unix_gnp sr_x86_regs sr_${machtype} sr_linux)
+		set filecount=`find "${sr_dir}/" -maxdepth 1 -name "*.$ext" | wc -l`
+		if (${machtype} == "x86_64" && ${sr_dir} == "sr_x86_regs" && $filecount > 0) then
+			cp -pa ${sr_dir}/*.$ext $gtm_ver/$dir/
+		else if ($filecount > 0) then
+			cp -pa ${sr_dir}/*.$ext $gtm_ver/$dir/
 		endif
-	endif
+	end
+end
 
-	set filecount=`find "sr_${machtype}/" -maxdepth 1 -name "*.$ext" | wc -l`
-	if ($filecount > 0) then
-		cp -pa sr_${machtype}/*.$ext $gtm_ver/$dir/
-	endif
+# Copy xfer_desc.i (if it exists) to $gtm_inc
+if (-e $source_dir/$dbgpro/xfer_desc.i) then
+	cp -pa $source_dir/$dbgpro/xfer_desc.i $gtm_ver/inc
+endif
 
-	set filecount=`find "sr_linux/" -maxdepth 1 -name "*.$ext" | wc -l`
-	if ($filecount > 0) then
-		cp -pa sr_linux/*.$ext $gtm_ver/$dir/
+# gtm_threadgbl_deftypes.h used to be in "gen" directory previously. Now it is in "genused" directory.
+# Handle both cases in this script
+foreach dir (gen genused)
+	if (-e $source_dir/$dbgpro/$dir/gtm_threadgbl_deftypes.h) then
+		cp -pa $source_dir/$dbgpro/$dir/gtm_threadgbl_deftypes.h $gtm_ver/inc
 	endif
 end
+
+# Sudo tests require `ydbinstall` that is part of the intermediate build process (yuck); so leave the build directory around for master commit
+# Otherwise delete to save space
+if ( "$git_tag" != "master" ) then
+	rm -rf $source_dir/$dbgpro
+endif
+
 
 # Install gtmcrypt plugin
 setenv ydb_dist /usr/library/$verno/$dbgpro

docker/Dockerfile
@@ -116,9 +116,14 @@ COPY docker/build_and_install_yottadb.csh /usr/library/gtm_test/build_and_instal
 # build_and_install_yottadb.csh <folder in /usr/library> <git tag in YDB> <dbg/pro>
 RUN /usr/library/gtm_test/build_and_install_yottadb.csh V71000 V7.1-000 pro
 
+# v6 DB (dbg)
+RUN /usr/library/gtm_test/build_and_install_yottadb.csh V63014_R138 r1.38 pro
+RUN /usr/library/gtm_test/build_and_install_yottadb.csh V63014_R138 r1.38 dbg
+
 # gtm_curpro: bump up by changing the next three lines
 ENV gtm_curpro V70005_R202
 RUN /usr/library/gtm_test/build_and_install_yottadb.csh V70005_R202 r2.02 pro
+RUN /usr/library/gtm_test/build_and_install_yottadb.csh V70005_R202 r2.02 dbg
 RUN ln -s /usr/library/R202 /usr/library/V70005_R202
 
 # Needs to be the last one to enable the lastest source tree to be available for sudo tests

docker/pipeline-test-ydb.csh
@@ -1,7 +1,7 @@
 #!/bin/tcsh
 #################################################################
 #                                                               #
-# Copyright (c) 2024 YottaDB LLC and/or its subsidiaries.       #
+# Copyright (c) 2024-2025 YottaDB LLC and/or its subsidiaries.       #
 # All rights reserved.                                          #
 #                                                               #
 #       This source code contains the intellectual property     #
@@ -34,22 +34,8 @@ foreach test ($random_test_list)
 	if ($test == "r120") set subtest_list_to_randomize_nonreplic=`echo $subtest_list_to_randomize_nonreplic:s/ydbdist//`
 	# r122 fails in tprestart (log related)
 	if ($test == "r122") set subtest_list_to_randomize_nonreplic=`echo $subtest_list_to_randomize_nonreplic:s/tprestart//`
-	# r124 fails in ydb359 ($PATH), ydb333 (? cpu issue ?), ydb114 (lack of older version)
-	if ($test == "r124") then
-		set subtest_list_to_randomize_nonreplic=`echo $subtest_list_to_randomize_nonreplic:s/ydb359//`
-		set subtest_list_to_randomize_nonreplic=`echo $subtest_list_to_randomize_nonreplic:s/ydb333//`
-		set subtest_list_to_randomize_nonreplic=`echo $subtest_list_to_randomize_nonreplic:s/ydb114//`
-	endif
-	# r130 fails in ydb484 (lack of older version), ydb493 (?), ydb494 (compilation issue), ydb388 (lack of older version)
-	if ($test == "r130") then
-		set subtest_list_to_randomize_nonreplic=`echo $subtest_list_to_randomize_nonreplic:s/ydb484//`
-		set subtest_list_to_randomize_nonreplic=`echo $subtest_list_to_randomize_nonreplic:s/ydb493//`
-		set subtest_list_to_randomize_nonreplic=`echo $subtest_list_to_randomize_nonreplic:s/ydb494//`
-		set subtest_list_to_randomize_nonreplic=`echo $subtest_list_to_randomize_nonreplic:s/ydb388//`
-	endif
-	# r136 fails in ydb943 (plugin install config), gtm8863a (lack of older version)
+	# r136 fails in gtm8863a (v V63011_R134 is not installed)
 	if ($test == "r136") then
-		set subtest_list_to_randomize_nonreplic=`echo $subtest_list_to_randomize_nonreplic:s/ydb943//`
 		set subtest_list_to_randomize_nonreplic=`echo $subtest_list_to_randomize_nonreplic:s/gtm8863a//`
 	endif
 	# v70000 fails in ydb531_v6to7 (imptp invocation)

docker/pipeline-test-ydbtest.csh
@@ -80,6 +80,9 @@ else
 	set filelist = `git diff --name-only $basecommit`
 endif
 
+echo "Currently available versions: "
+ver
+
 set instream_invokelist = ""
 
 # Results collected in result.txt, initially empty
@@ -91,6 +94,10 @@ set heavyweights = " multisrv_crash unicode_socket rollback_B socket jnl_crash i
 
 # Our AARCH64 runner is slow, so add sudo to the list of heavyweight tests
 if (`uname -m` == "aarch64") set heavyweights = "${heavyweights}sudo "
+
+# Print file list so we can find out which tests are running in the pipeline
+echo "Filelist changed: "
+echo $filelist | sed 's/ /\n/g;'
 
 # For each file, check if it is instream.csh or is inside inref
 # If it is, add it so that we invoke the test system with "-t xxx -t yyy -t zzz"
@@ -107,7 +114,7 @@ foreach file ($filelist)
 	if ( $?test ) then
 		# Space delimiters ensure we don't accidentally exclude a test due to partial matching; it needs to be a whole word
 		if ( "$heavyweights" =~ "* $test *" ) then
-			echo "Skipping test on pipeline as it is a heavyweight test"
+			echo "Skipping test [$test] on pipeline as it is a heavyweight test"
 			continue
 		endif
 		# If test is already there, don't add it again
@@ -116,6 +123,8 @@ foreach file ($filelist)
 		endif
 	endif
 end
+
+echo "Test list: $instream_invokelist (replic/nonreplic)"
 
 # The test system has the capability of running multiple instreams at once, so let's do that.
 
@@ -145,7 +154,7 @@ foreach file ($filelist)
 	if ( ( "u_inref" == "$directory_name" ) || ( "outref" == "$directory_name" ) ) then
 		set test = `echo $file | cut -d / -f 1`
 		if ( "$heavyweights" =~ "* $test *" ) then
-			echo "Skipping test on pipeline as it is a heavyweight test"
+			echo "Skipping test [$test] on pipeline as it is a heavyweight test"
 			continue
 		endif
 		set subtest = $file:r:t
@@ -160,6 +169,7 @@ foreach file ($filelist)
 		if ( "$subtest_list_non_replic" =~ "*$subtest*" || "$subtest_list_common" =~ "*$subtest*" ) then
 			# If test was invoked as a suite from instream (non-replic), don't add it to the invoke list
 			if ( "$instream_invokelist" !~ "*-t $test*" ) then
+				echo "Running $test/$subtest"
 			        su -l gtmtest $pass_env -c "/usr/library/gtm_test/T999/com/gtmtest.csh -nomail -fg -env gtm_ipv4_only=1 -stdout 2 -t $test/$subtest"
 				if ($status) then
 					echo "$test/${subtest}: FAIL (non-replic)" >> result.txt
@@ -170,6 +180,7 @@ foreach file ($filelist)
 		else if ( "$subtest_list_replic" =~ "*$subtest*" || "$subtest_list_common" =~ "*$subtest*" ) then
 			# If test was invoked as a suite from instream (replic), don't add it to the invoke list
 			if ( "$instream_invokelist" !~ "*-t $test*" ) then
+				echo "Running $test/$subtest -replic"
 			        su -l gtmtest $pass_env -c "/usr/library/gtm_test/T999/com/gtmtest.csh -nomail -fg -env gtm_ipv4_only=1 -stdout 2 -t $test/$subtest -replic"
 				if ($status) then
 					echo "$test/${subtest}: FAIL (replic)" >> result.txt
@@ -181,12 +192,14 @@ foreach file ($filelist)
 			# Subtest in the u_inref or outref is not a known subtest. Run whole test.
 			# If test was invoked as a suite from instream (replic), don't add it to the invoke list
 			if ( "$instream_invokelist" !~ "*-t $test*" ) then
+				echo "Running $test"
 			        su -l gtmtest $pass_env -c "/usr/library/gtm_test/T999/com/gtmtest.csh -nomail -fg -env gtm_ipv4_only=1 -stdout 2 -t $test"
 				if ($status) then
 					echo "${test}: FAIL (non-replic)" >> result.txt
 				else
 					echo "${test}: PASS (non-replic)" >> result.txt
 				endif
+				echo "Running $test -replic"
 			        su -l gtmtest $pass_env -c "/usr/library/gtm_test/T999/com/gtmtest.csh -nomail -fg -env gtm_ipv4_only=1 -stdout 2 -t $test -replic"
 				if ($status) then
 					echo "${test}: FAIL (replic)" >> result.txt

r124/inref/ydb359.m
@@ -1,6 +1,6 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;								;
-; Copyright (c) 2018-2023 YottaDB LLC and/or its subsidiaries.	;
+; Copyright (c) 2018-2025 YottaDB LLC and/or its subsidiaries.	;
 ; All rights reserved.						;
 ;								;
 ;	This source code contains the intellectual property	;
@@ -12,12 +12,12 @@
 ;
 ydb359
 	set $zinterrupt="do zintr" do ^sstep
-        set x=1
-        zsystem "mupip intrpt "_$j
-        set y=2
-        set z=3
-        write "x = ",x," : y = ",y," : z = ",z,!
-        quit
+	set x=1
+	zsystem "$gtm_dist/mupip intrpt "_$j
+	set y=2
+	set z=3
+	write "x = ",x," : y = ",y," : z = ",z,!
+	quit
 
 zintr	;
 	if $ZJOBEXAM()

commit a25e7d4d253bd9ba1b751f915e5811c784ddc5c8

[#787] Fix docker-create-aarch64 job failure (regression in 51fbf1cd)
* See https://gitlab.com/YottaDB/DB/YDBTest/-/merge_requests/2229#note_2377229618 and surrounding
  discussion for more background on the issue.

* This commit implements the YDB@f88b9698 fix using a `sed` based approach and removes the `-ggdb3`
  lines from the `sr_linux/platform.cmake` file before attempting the build.

docker/build_and_install_yottadb.csh
@@ -48,6 +48,10 @@ else
 	# Discard sr_*/release_name.h changes from a previous run
 	git checkout .
 	git checkout $git_tag
+	# Remove "-ggdb3" from CMAKE_ASM_FLAGS_DEBUG else AARCH64 build fails (see commit f88b9698 for more details)
+	# The below sed command is structured to work for r1.38 (pre f88b9698) or r2.00 (post f88b9698) so we do not
+	# need to check if the git tag is less than or equal to r1.38 in order to do this removal.
+	sed -i '/-ggdb3/d' sr_linux/platform.cmake
 	popd
 endif
 