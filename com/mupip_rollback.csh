YottaDB/YDBTest

commit 5be711dae81a2a0688189f4d6df0cc62a458a76b

Fix rare rollback_A/seqno_chk subtest failure (DBPREMATEOF, due to disk full)
Background
----------
* Once in a while, the above subtest failed during in-house testing with the following diff.

  ```diff
  183,185d182
  < %YDB-S-JNLSUCCESS, Show successful
  < %YDB-S-JNLSUCCESS, Verify successful
  < %YDB-S-JNLSUCCESS, Rollback successful
  189c186,197
  < DATABASE EXTRACT PASSED
  ---
  > TEST-E-DB_EXTRACT, MUPIP extract failed
  > %YDB-E-DBPREMATEOF, Premature end of file with database file .../rollback_A_1/seqno_chk/mumps.dat
  ```

Issue
-----
* The primary failure symptom is actually the following (`No space left on device` error).

  ```sh
  $ cat rollback1.log
  .
  .
  %YDB-S-JNLSUCCESS, Verify successful
  %YDB-S-JNLSUCCESS, Rollback successful
  %YDB-I-MUJNLSTAT, End processing at Sun Mar 30 03:03:48 2025
  cp: error writing './h.mjl': No space left on device
  cp: error writing './h.mjl_2025089030238': No space left on device
  cp: error writing './mumps.mjl': No space left on device
  cp: error writing './mumps.mjl_2025089030238': No space left on device
  cp: error writing './a.dat': No space left on device
  cp: error writing './b.dat': No space left on device
  cp: error writing './c.dat': No space left on device
  cp: error writing './d.dat': No space left on device
  cp: error writing './e.dat': No space left on device
  cp: error writing './f.dat': No space left on device
  cp: error writing './g.dat': No space left on device
  cp: error writing './h.dat': No space left on device
  cp: error writing './mumps.dat': No space left on device
  ```

* This caused the `*.dat` files to be created with 0 file size and that is what later caused the
  `DBPREMATEOF` error.

* So the file system got full and caused the test failure. This was an 80Gb sized filesystem.

* This disk full situation happened while the subtest had invoked `com/mupip_rollback.csh`, a test

com/mupip_rollback.csh
@@ -4,7 +4,7 @@
 # Copyright (c) 2015-2016 Fidelity National Information		#
 # Services, Inc. and/or its subsidiaries. All rights reserved.	#
 #								#
-# Copyright (c) 2018-2024 YottaDB LLC and/or its subsidiaries.	#
+# Copyright (c) 2018-2025 YottaDB LLC and/or its subsidiaries.	#
 # All rights reserved.						#
 #								#
 #	This source code contains the intellectual property	#
@@ -424,9 +424,7 @@ if (! $forward_only_specified) then
 	endif
 endif
 
-set bakdir3 = dir_${bakdir}_mupip_rollback_3
 unset noglob
-$gtm_tst/com/backup_dbjnl.csh $bakdir3 "*.dat *.mjl*" mv nozip	# move over files for debugging test failures if any
 
 set sort = "sort -T ."	# Use sort -T . to ensure temporary files get created in current directory instead of /tmp where we might not have enough space
 
@@ -546,16 +544,19 @@ if (! $forward_only_specified) then
 	endif
 
 	echo "# Check mupip extract" >>& $misclog
-	# The second line of the extract file contains a timestamp so need to remove that before the diff.
-	foreach direction (backward forward)
-		$tail -n +3 ${bakdir}_${direction}_extract.out >& ${bakdir}_${direction}_extract.out.filtered
-	end
+	# The second line of the extract file contains a timestamp so need to skip the first 2 lines before the diff.
 	# Since extract files could be huge, use "cmp" instead of "diff" (faster).
-	echo "cmp ${bakdir}_{backward,forward}_extract.out.filtered" >>& $misclog
-	cmp ${bakdir}_{backward,forward}_extract.out.filtered >>& $misclog
+	foreach direction (backward forward)
+		set skip$direction = `$head -n 2 ${bakdir}_${direction}_extract.out | wc -c`
+	end
+	echo "cmp --ignore-initial=${skipbackward}:${skipforward} ${bakdir}_{backward,forward}_extract.out" >>& $misclog
+	cmp --ignore-initial=${skipbackward}:${skipforward} ${bakdir}_{backward,forward}_extract.out>>& $misclog
 	if ($status) then
 		echo "TEST-E-ERROR : MUPIP EXTRACT diff of backward and forward rollback failed. See above for details" >>& $misclog
 		set exit_status = 1
+	else
+		echo "rm -f ${bakdir}_*_extract.out" >>& $misclog
+		rm -f ${bakdir}_*_extract.out	# * to account for "backward" and "forward"
 	endif
 
 	echo "# Check appropriate lost transaction files got created" >>& $misclog
@@ -605,11 +606,19 @@ if (! $forward_only_specified) then
 		endif
 	endif
 
-	cp $bakdir2/*.mjl* .	# restore mjl to reflect backward rollback
-	cp $bakdir2/*.dat .	# restore dat to reflect backward rollback
+	if (0 == $exit_status) then
+		mv $bakdir2/*.mjl* .	# restore mjl to reflect backward rollback
+		mv $bakdir2/*.dat .	# restore dat to reflect backward rollback
+	endif
 else
-	# Forward rollback done. Databases are in good shape.
-	mv $bakdir3/*.dat .	# restore dat to reflect forward rollback
+	# Forward rollback done. Databases are in good shape. Preserve dat to reflect forward rollback.
+	# Move over journal files for debugging test failures if any.
+	set bakdir3 = dir_${bakdir}_mupip_rollback_3
+	if (0 != $exit_status) then
+		$gtm_tst/com/backup_dbjnl.csh $bakdir3 "*.mjl*" mv nozip
+	else
+		rm -rf *.mjl*
+	endif
 	# Move away replication instance file
 	if (-e $gtm_repl_instance) then
 		mv $gtm_repl_instance $gtm_repl_instance.orig_${n}
@@ -638,8 +647,6 @@ if (0 == $exit_status) then
 	# Cleaning them up is okay since this invocation of mupip_rollback.csh succeeded.
 	echo "rm -rf $bakdir1 $bakdir2 $bakdir3" >>& $misclog
 	rm -rf $bakdir1 $bakdir2 $bakdir3
-	echo "rm -f ${bakdir}_*_extract.out{,.filtered}" >>& $misclog
-	rm -f ${bakdir}_*_extract.out{,.filtered}	# * to account for "backward" and "forward"
 endif
 
 exit $exit_status

rollback_A/u_inref/seqno_chk.csh
@@ -4,7 +4,7 @@
 # Copyright (c) 2003-2015 Fidelity National Information		#
 # Services, Inc. and/or its subsidiaries. All rights reserved.	#
 #								#
-# Copyright (c) 2018-2024 YottaDB LLC and/or its subsidiaries.	#
+# Copyright (c) 2018-2025 YottaDB LLC and/or its subsidiaries.	#
 # All rights reserved.						#
 #								#
 #	This source code contains the intellectual property	#
@@ -50,11 +50,14 @@ $MUPIP rundown -reg "*" -override |& sort -f
 echo "Journal extract on primary side before rollback ......"
 $gtm_tst/$tst/u_inref/seqextr.csh
 echo "Verifying journals on primary side before rollback ......"
-$GTM << gtm_eof
-	d chkextr^bkgrnd(1)
-	h
-gtm_eof
-#
+$gtm_dist/mumps -run %XCMD 'do chkextr^bkgrnd(1)' >& chkextr1.out
+cat chkextr1.out
+$grep "VERIFICATION FAILED" chkextr1.out > /dev/null
+if ($status) then
+	# Verification passed. Remove extract files as they occupy disk space otherwise.
+	rm -f *.mjf
+endif
+
 echo "mupip rollback on primary side ..."
 echo "#mupip journal /rollback /back /losttrans=lost.glo *"
 setenv start_time `date +%H_%M_%S`
@@ -104,8 +107,11 @@ echo "Journal extract on primary side ......"
 $gtm_tst/$tst/u_inref/seqextr.csh
 #
 echo "Verifying journals on primary side ......"
-$GTM << gtm_eof
-	d chkextr^bkgrnd(0)
-	h
-gtm_eof
+$gtm_dist/mumps -run %XCMD 'do chkextr^bkgrnd(0)' >& chkextr0.out
+cat chkextr0.out
+$grep "VERIFICATION FAILED" chkextr0.out > /dev/null
+if ($status) then
+	# Verification passed. Remove extract files as they occupy disk space otherwise.
+	rm -f *.mjf
+endif
 #

commit 7179be6371dd8ce0d76b417594158f238e676f2c

Fix various test failures (bakdir3: Undefined variable) due to com/mupip_rollback.csh regression
* A few tests failed in a D_ALL and all of them had the following error.

  ```
  bakdir3: Undefined variable.
  ```

* After some analysis, realized it is a regression in 5be711da where as part of reducing disk space
  utilization, logic that set the `bakdir3` variable in `com/mupip_rollback.csh` was moved inside
  an `else` clause but later code that was outside of the `else` did a `rm -rf $bakdir3` resulting
  in the `bakdir3: Undefined variable` error.

* The fix is straightforward and is to move the line where `bakdir3` variable is set to be outside
  the `else` block. That way it is defined even for the `if` block and for later code (that uses
  the variable for a different `if` condition).

com/mupip_rollback.csh
@@ -428,6 +428,7 @@ unset noglob
 
 set sort = "sort -T ."	# Use sort -T . to ensure temporary files get created in current directory instead of /tmp where we might not have enough space
 
+set bakdir3 = dir_${bakdir}_mupip_rollback_3
 if (! $forward_only_specified) then
 	echo "#################################################################" >>& $misclog
 	echo "# Check BACKWARD and FORWARD rollback produced the same database state" >>& $misclog
@@ -613,7 +614,6 @@ if (! $forward_only_specified) then
 else
 	# Forward rollback done. Databases are in good shape. Preserve dat to reflect forward rollback.
 	# Move over journal files for debugging test failures if any.
-	set bakdir3 = dir_${bakdir}_mupip_rollback_3
 	if (0 != $exit_status) then
 		$gtm_tst/com/backup_dbjnl.csh $bakdir3 "*.mjl*" mv nozip
 	else