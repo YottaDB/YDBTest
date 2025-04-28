YottaDB/YDBTest

commit 554abb0ecf6d0de1d6a2a35bd3b7f0d8f65c824b

Disable YDBPython random choice in com/imptp.csh if YottaDB is built with GCC and ASAN (segmentation fault)
Background
----------
* In a Ubuntu 22.04 system that had been running in-house tests fine for a while, I noticed that this
  past weekend a lot of tests that use `com/imptp.csh` failed with core files when they chose `YDBPython`
  as the random language wrapper choice and when YottaDB was built with GCC and ASAN.

* Below are the subtests that failed.
  - rundown_argless_1/ipcsnoreset
  - rundown_argless_1/mu_rundown_no_ipcrm1
  - v43001e_0/D9C03002051
  - v54000_0/D9I09002699
  - v60000_0/gtm4525b
  - v60000_1/gtm4525b
  - v60003_0/gtm7528
  - v62001_1/gtm7862
  - v63000_0/gtm8137
  - v63000_1/gtm8340
  - v70000_0/ydb531_v6tov7
  - v70000_1/gtm9368
  - set_jnl_1/unicode_dir_autoswitch_replic

* The failure symptom was the following.

  ```diff
  > ERRORS.CSH-E-FINDEXE : Could not determine executable of core file [./python/core.76272]
  > Core was generated by `~/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/bin/rustc --versi'.
  > Program terminated with signal SIGSEGV, Segmentation fault.
  ```

Issue
-----
* The failure was easily reproducible by running the following commands on the Ubuntu 22.04 system
  in a `tcsh` shell session.

  ```sh
  $ setenv ASAN_OPTIONS "detect_leaks=0:abort_on_error=1:disable_coredump=0"
  $ setenv LD_PRELOAD "/usr/lib/gcc/x86_64-linux-gnu/11/libasan.so"

  $ ~/.cargo/bin/rustc --version
  Segmentation fault (core dumped)

  $ /usr/bin/rustc --version
  rustc 1.75.0 (82e1608df 2023-12-21) (built from a source tarball)

  $ unsetenv LD_PRELOAD
  $ ~/.cargo/bin/rustc --version
  rustc 1.86.0 (05f9846f8 2025-03-31)
  ```

* As can be seen above, invoking `/usr/bin/rustc --version` with `LD_PRELOAD` set runs fine but
  invoking `~/.cargo/bin/rustc --version` with `LD_PRELOAD` set causes a `Segmentation fault` error.

* The `~/.cargo/bin/rustc` version is 1.86 and was installed under the user's home directory using

com/imptp.csh
@@ -131,19 +131,20 @@ if ($gtm_test_dbfill == "IMPTP" || $gtm_test_dbfill == "IMPZTP") then
 				echo "# Disabling ydb_imptp_flavor=3 (YDBPython) due to python3 3.11.* or 3.12.*"
 				set disable_imptp_flavor_list = "$disable_imptp_flavor_list 3"
 			endif
-			# Disable Python testing if ASAN is enabled and Rust version is 1.58.* or 1.59.*
-			# to prevent erroneous core files from `rustc --version`.
+			# Disable YDBPython testing if YottaDB is built with ASAN and GCC.
+			# This is to prevent erroneous core files from `rustc --version`.
 			# This command is run by YDBPython's `setup.py`, during the setting of
 			# LD_PRELOAD, e.g.:
 			#	LD_PRELOAD=$(gcc -print-file-name=libasan.so) rustc --version
-			# On systems with Rust version 1.58.*, 1.59.* or 1.60.* or 1.61.* (all versions only on an Arch
-			# Linux system, not on a Ubuntu/Debian/RHEL system), this has been seen to result in a
-			# segmentation fault. So, disable YDBPython random choice on Arch Linux.
-			# See https://gitlab.com/YottaDB/DB/YDBTest/-/merge_requests/1299#note_839072462 and
-			# https://gitlab.com/YottaDB/DB/YDBTest/-/merge_requests/1385#note_972287548 for more details.
-			if (($gtm_test_libyottadb_asan_enabled) && ("arch" == $gtm_test_linux_distrib)) then
-				echo "# Disabling ydb_imptp_flavor=3 (YDBPython) due to ASAN + Arch Linux"
-				set disable_imptp_flavor_list = "$disable_imptp_flavor_list 3"
+			# On systems with Rust version 1.58 onwards (and even as high as 1.86.0) we have seen this
+			# result in a segmentation fault. This seems to happen only when rustc is installed using
+			# rustup in a user's home directory (~/.cargo/bin/rustc) and does not seem to happen when rustc
+			# is installed system-wide (/usr/bin/rustc). No idea why. So, disable YDBPython random choice for now.
+			if ($gtm_test_libyottadb_asan_enabled)
+				if ("gcc" == $gtm_test_asan_compiler) then
+					echo "# Disabling ydb_imptp_flavor=3 (YDBPython) due to GCC + ASAN (rustc --version core dumps)"
+					set disable_imptp_flavor_list = "$disable_imptp_flavor_list 3"
+				endif
 			endif
 			# Disable Python testing if YottaDB is built with ASAN and CLANG.
 			# In that case, setting the LD_PRELOAD env var (which the YDBPython wrapper sets whenever it

commit 4b0ac0c60094f7174b0ed5c07cf34e4d5a29aa56

Fix missing [then] in [if] check in com/imptp.csh (regression in 554abb0e)

com/imptp.csh
@@ -140,7 +140,7 @@ if ($gtm_test_dbfill == "IMPTP" || $gtm_test_dbfill == "IMPZTP") then
 			# result in a segmentation fault. This seems to happen only when rustc is installed using
 			# rustup in a user's home directory (~/.cargo/bin/rustc) and does not seem to happen when rustc
 			# is installed system-wide (/usr/bin/rustc). No idea why. So, disable YDBPython random choice for now.
-			if ($gtm_test_libyottadb_asan_enabled)
+			if ($gtm_test_libyottadb_asan_enabled) then
 				if ("gcc" == $gtm_test_asan_compiler) then
 					echo "# Disabling ydb_imptp_flavor=3 (YDBPython) due to GCC + ASAN (rustc --version core dumps)"
 					set disable_imptp_flavor_list = "$disable_imptp_flavor_list 3"