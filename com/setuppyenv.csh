YottaDB/YDBTest

commit 2ff039684925db617c59d766bcfc52cecd8aa03e

Fixed r200/ydbpython32 and r136/ydb935 subtests failures on Ubuntu 24.04
Failures in the r200/ydbpython32 and r136/ydb935 subtests were reported on Ubuntu 24.04 due to the following error:

```python
NotImplementedError: Support for egg-based install has been removed.
```

This is due to the deprecation of the `.egg` binary format used for Python module installs, per the [Python packaging documentation](https://packaging.python.org/en/latest/discussions/package-formats/#what-about-eggs):

> "Egg" is an old package format that has been replaced with the wheel format. It should not be used anymore. Since August 2023, PyPI rejects egg uploads.

The fix is to use the new wheel format instead of the deprecated `.egg` format by replacing calls to `python setup.py install` with `python -m pip install .`.

com/setuppyenv.csh
@@ -1,6 +1,6 @@
 #################################################################
 #								#
-# Copyright (c) 2022-2024 YottaDB LLC and/or its subsidiaries.	#
+# Copyright (c) 2022-2025 YottaDB LLC and/or its subsidiaries.	#
 # All rights reserved.						#
 #								#
 #	This source code contains the intellectual property	#
@@ -34,7 +34,7 @@ source .venv/bin/activate.csh
 # following in the activated virtual environment. See https://docs.python.org/3/whatsnew/3.12.html
 # for more details (search for "gh-95299").
 pip install setuptools
-# Install into venv: `python setup.py install`
-python setup.py install
+# Install into venv: `python -m -pip install .`
+python -m -pip install .
 cd ..
 exit 0

r136/u_inref/ydb935.csh
@@ -1,6 +1,6 @@
 #################################################################
 #								#
-# Copyright (c) 2022-2024 YottaDB LLC and/or its subsidiaries.	#
+# Copyright (c) 2022-2025 YottaDB LLC and/or its subsidiaries.	#
 # All rights reserved.						#
 #								#
 #	This source code contains the intellectual property	#
@@ -23,8 +23,8 @@ source .venv/bin/activate.csh
 # See comment in com/setuppyenv.csh for why this step is needed
 pip install setuptools >& setup.txt
 
-# Install into venv: `python setup.py install`
-python setup.py install >>& setup.txt
+# Install into venv: `python -m pip install .`
+python -m pip install . >>& setup.txt
 pip3 install flask      >>& setup.txt
 
 # Make sure that the ldd linkage is right

r200/u_inref/ydbpython32.csh
@@ -1,7 +1,7 @@
 #!/usr/local/bin/tcsh -f
 #################################################################
 #								#
-# Copyright (c) 2023-2024 YottaDB LLC and/or its subsidiaries.	#
+# Copyright (c) 2023-2025 YottaDB LLC and/or its subsidiaries.	#
 # All rights reserved.						#
 #								#
 #	This source code contains the intellectual property	#
@@ -25,8 +25,8 @@ source .venv/bin/activate.csh
 # See comment in com/setuppyenv.csh for why this step is needed
 pip install setuptools >& setup.txt
 
-# Install into venv: `python setup.py install`
-python setup.py install >>&  setup.txt
+# Install into venv: `python -m pip install .`
+python -m pip install . >>&  setup.txt
 pip3 install flask      >>& setup.txt
 
 # Make sure that the ldd linkage is right

commit 5f2fb7444f6a4f46182f94b3be9b609cd22621d4

Print SETUPPYENV-E-FAILED if any step in com/setuppyenv.csh fails; Also fix typo (regression in 2ff03968)
* See https://gitlab.com/YottaDB/DB/YDBTest/-/merge_requests/2310#note_2480057356 for more details
  on the typo in `com/setuppyenv.csh`. This is a regression in 2ff03968.

* Additionally, I noticed in the same D_ALL that there were a lot of test failures but all of them
  had various failure symptoms none of which easily led to the error inside `com/imptp.csh` while
  installing the YDBPython wrapper. This is because we are not checking the error status of various
  commands run inside the script (in this case it was the `python -m -pip install .` command. That
  is now taken care of by printing a `SETUPPYENV-E-FAILED` string in case of an error. This way the
  caller script will detect the error (because it will usually run `com/imptp_check_error.csh` which
  will scan the output for `-E-` strings and signal an error and stop the test right away leading to
  a much clearer cause of failure.

com/setuppyenv.csh
@@ -29,12 +29,30 @@ cd $pypath
 python3 -m venv .venv
 # Activate virtual environment
 source .venv/bin/activate.csh
+set status1 = $status
+if ($status1) then
+	echo "SETUPPYENV-E-FAILED : [source .venv/bin/activate.csh] failed with status [$status1]"
+	cd ..
+	exit 1
+endif
 # setuptools is no longer pre-installed in virtual environments created with venv from Python 3.12
 # This means that it is no longer available by default. But since setup.py needs it, run the
 # following in the activated virtual environment. See https://docs.python.org/3/whatsnew/3.12.html
 # for more details (search for "gh-95299").
 pip install setuptools
-# Install into venv: `python -m -pip install .`
-python -m -pip install .
+set status1 = $status
+if ($status1) then
+	echo "SETUPPYENV-E-FAILED : [pip install setuptools] failed with status [$status1]"
+	cd ..
+	exit 1
+endif
+# Install into venv: `python -m pip install .`
+python -m pip install .
+set status1 = $status
+if ($status1) then
+	echo "SETUPPYENV-E-FAILED : [python -m pip install .] failed with status [$status1]"
+	cd ..
+	exit 1
+endif
 cd ..
 exit 0