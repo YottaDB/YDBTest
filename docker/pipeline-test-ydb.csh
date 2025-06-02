YottaDB/YDBTest

commit 0235bf2c5bf39baee1cc99874d4d03b954d544c6

[YDB#1105][#807][#829] Speed up YDB pipeline and share script with YDBTest
This commit re-does how the YDB pipeline runs, and it also moves all the
shared code between three paths (docker run, YDBTest pipeline, and YDB
pipeline) into a single file.

NB: dind = Docker inside of Docker

- YDB pipeline previously ran using dind (as a docker run) (i.e. the
  Gitlab runner ran a dind image, and then ran a docker run inside the
  dind image). Now, we do the "docker run" internally in the Gitlab
  runner. The advantage to do doing this is that we don't have to
  download the image each time, which is what previously happened.
  This commit is a pre-requisite for the YDB change. Doing this will
  help us reuse code for #807, as right now the different running
  methodology prevents us from re-using code between the YDB and YDBTest
  pipelines. The reason it was done using the previous way is that it
  was easy to reproduce problems locally as it was an identical set-up;
  but since YDBTest image now is nearly 10 GB, minimize downloads is best.
- The shared code used by `docker/pipeline-test-ydb.csh`,
  `docker/pipeline-test-ydbtest.csh`, and `docker/run.csh` was
  consolidated into a single file `docker/shared-setup.csh`. The 3 files
  were all developed separately, and it's realized now that there is a
  lot of common code that can be re-used. The common code is quite
  complex, which means that there were bugs in the pipeline that were
  fixed already in `run.csh` but were not fixed in the pipeline.
- Removed the objects left behind in the master layer as they took
  around 5GB, and also removed the *.a files, as they took up a lot of
  space. This brought down the size of the image from 10.4G to 4.2G. The
  objects in the master layer were there for the use of the `sudo` test,
  which only used a single file, `ydbinstall`.

List of regression testing done:

run.csh still works:
```

docker/pipeline-test-ydb.csh
@@ -10,6 +10,20 @@
 #       the license, please stop and do not read further.       #
 #                                                               #
 #################################################################
+source /usr/library/gtm_test/T999/docker/shared-setup.csh
+
+set echo
+set verbose
+
+# This block is active when we are inside of a YDB pipeline
+if ( $?CI_PIPELINE_ID ) then
+	git config --global --add safe.directory `pwd`
+	# Link the source code to the directory build_and_install_yottadb.csh is aware of
+	ln -s `pwd` /Distrib/YottaDB/V999_R999
+	ls -lrt /Distrib/YottaDB/V999_R999/
+	/usr/library/gtm_test/T999/docker/build_and_install_yottadb.csh V999_R999 master dbg
+endif
+
 pushd $gtm_tst
 set test_list=`echo basic sudo r1* r2* v7*`
 echo "test_list: $test_list"
@@ -40,10 +54,10 @@ foreach test ($random_test_list)
 	# Note that we use "fg", because if we use "bg", the shell does not know that there are child jobs to "wait" for.
 	# using fg with & gives us what we want: run multiple tests concurrently and wait for all of them to finish
 	if ( $random_nonreplic_subtest_list_with_commas != "" ) then
-		/usr/library/gtm_test/T999/com/gtmtest.csh -nomail -env gtm_ipv4_only=1 -stdout 0 -fg -t $test -st $random_nonreplic_subtest_list_with_commas >>& /tmp/test-${test}.txt &
+		su -l gtmtest $pass_env -c "/usr/library/gtm_test/T999/com/gtmtest.csh -nomail -env gtm_ipv4_only=1 -stdout 0 -fg -t $test -st $random_nonreplic_subtest_list_with_commas >>& /tmp/test-${test}.txt" &
 	endif
 	if ( $random_replic_subtest_list_with_commas != "" ) then
-		/usr/library/gtm_test/T999/com/gtmtest.csh -nomail -env gtm_ipv4_only=1 -stdout 0 -fg -t $test -st $random_replic_subtest_list_with_commas -replic >>& /tmp/test-${test}.txt &
+		su -l gtmtest $pass_env -c "/usr/library/gtm_test/T999/com/gtmtest.csh -nomail -env gtm_ipv4_only=1 -stdout 0 -fg -t $test -st $random_replic_subtest_list_with_commas -replic >>& /tmp/test-${test}.txt" &
 	endif
 end
 

docker/build_and_install_yottadb.csh
@@ -160,8 +160,6 @@ else
 		endif
 	popd
 endif
-mkdir -p $gtm_root/$verno/$dbgpro/obj
-find . -name '*.a' -exec cp {} $gtm_root/$verno/$dbgpro/obj \;
 mkdir -p $gtm_root/$verno/tools
 
 cd $source_dir
@@ -203,12 +201,19 @@ foreach dir (gen genused)
 	endif
 end
 
-# Sudo tests require `ydbinstall` that is part of the intermediate build process (yuck); so leave the build directory around for master commit
-# Otherwise delete to save space
-if ( "$git_tag" != "master" ) then
+# Sudo tests require `ydbinstall` that is part of the intermediate build process (yuck)
+# But the intermediate objects take up about 5GB
+# So try to preserve `ydbinstall`, but delete the rest
+# /Distrib/YottaDB/${gtm_verno}/${tst_image}/yottadb_r*/ydbinstall
+if ( "$git_tag" == "master" ) then
+	set ydbinstall = `find ${source_dir}/${dbgpro} -name 'ydbinstall' -not -path '*utf8*'`
+	mv $source_dir/$dbgpro/yottadb_r*/ydbinstall /tmp/
+	rm -rf $source_dir/$dbgpro
+	mkdir -p $ydbinstall:h
+	mv /tmp/ydbinstall $ydbinstall:h
+else
 	rm -rf $source_dir/$dbgpro
 endif
-
 
 # Install gtmcrypt plugin
 setenv ydb_dist /usr/library/$verno/$dbgpro

docker/pipeline-test-ydbtest.csh
@@ -10,45 +10,7 @@
 #       the license, please stop and do not read further.       #
 #                                                               #
 #################################################################
-
-# Ensure that our hostname does not have dashes as that crashes multiple parts of the test system
-# (it converts hostnames into variables)
-echo "HOSTNAME: $HOST"
-
-# Preamble
-# Start log server; needed by test framework
-rsyslogd
-# needed to create the /var/log/syslog file; as otherwise it won't exist at start
-logger test
-
-# The file is created asynchronously, so this ensures we don't proceed till it's created
-while ( ! -e /var/log/syslog)
-	sleep .01
-end
-
-# Next two seds, fix the serverconf.txt file
-## Correct the host; as it differs each time we start docker
-sed -i "s/HOST/$HOST/" /usr/library/gtm_test/serverconf.txt
-sed -i 's|LOG|/var/log/syslog|' /usr/library/gtm_test/serverconf.txt
-
-# Fix HOST on tstdirs.csh file
-sed -i "s/HOST/$HOST/" /usr/library/gtm_test/tstdirs.csh
-
-# Runner job does not set TERM
-setenv TERM xterm
-
-if ( ! -f /YDBTest/com/gtmtest.csh ) then
-	# Copy over the test system to $gtm_tst
-	# This is ineffecient, but not worth optimizing. It copies everything over rather than only changed files.
-	setenv gtm_tst "/usr/library/gtm_test/T999"
-	rsync . -ar --delete $gtm_tst
-	chown -R gtmtest:gtc $gtm_tst
-	git config --global --add safe.directory $gtm_tst
-else
-	# Test system passed in /YDBTest, use that
-	rm -r /usr/library/gtm_test/T999
-	ln -s /YDBTest /usr/library/gtm_test/T999
-endif
+source /usr/library/gtm_test/T999/docker/shared-setup.csh
 
 # See if we need to build a branch version of YottaDB to match the current branch
 pushd /usr/library/gtm_test/T999
@@ -77,19 +39,8 @@ endif
 rm ydb_open_mrs.json
 popd
 
-# Set-up testareas to be in the current directory so we can upload the artifacts
-mkdir testarea{1,2,3}
-# tst_dir can only be one test area, the main one
-echo "setenv tst_dir ${PWD}/testarea1" >> ~gtmtest/.cshrc
-chmod 777 ${PWD}/testarea{1,2,3}
-sed -i "s|/testarea|$PWD/testarea|g" /usr/library/gtm_test/tstdirs.csh
-
 # Sudo tests rely on the source code for ydbinstall to be in a specific location
 ln -s /Distrib/YottaDB /Distrib/YottaDB/V999_R999
-
-# Set-up some environment variables to pass to the test system
-setenv ydb_test_inside_docker 1
-set pass_env = "-w CI_PIPELINE_ID -w CI_COMMIT_BRANCH -w ydb_test_inside_docker"
 
 if ( ! -f /YDBTest/com/gtmtest.csh ) then
 	# Get list of changed files

docker/run.csh
@@ -10,55 +10,7 @@
 #	the license, please stop and do not read further.	#
 #								#
 #################################################################
-# Start log server; needed by test framework
-rsyslogd
-# needed to create the /var/log/syslog file; as otherwise it won't exist at start
-logger test
-
-# The file is created asynchronously, so this ensures we don't proceed till it's created
-while ( ! -e /var/log/syslog)
-	sleep .01
-end
-
-# Start sshd
-/sbin/sshd
-
-# Add an extra hostname for remote tests
-echo "127.0.0.1 ydbtest" >> /etc/hosts
-
-# Trust localhost
-su - gtmtest -c "ssh-keyscan -t rsa localhost >>& ~/.ssh/known_hosts"
-su - gtmtest -c "ssh-keyscan -t rsa `hostname -I` >>& ~/.ssh/known_hosts"
-su - gtmtest -c "ssh-keyscan -t rsa $HOST >>& ~/.ssh/known_hosts"
-su - gtmtest -c "ssh-keyscan -t rsa ydbtest >>& ~/.ssh/known_hosts"
-
-# Next seds, fix the serverconf.txt file
-## Correct the host; as it differs each time we start docker
-sed -i "s/HOST/$HOST/" /usr/library/gtm_test/serverconf.txt
-sed -i 's|LOG|/var/log/syslog|' /usr/library/gtm_test/serverconf.txt
-sed -i "s/SE1/localhost/" /usr/library/gtm_test/serverconf.txt
-sed -i "s/SE2/ydbtest/" /usr/library/gtm_test/serverconf.txt
-
-# Set hosts on tstdirs.csh file
-sed -i "s/HOST/$HOST/" /usr/library/gtm_test/tstdirs.csh
-sed -i "s/SE1/localhost/" /usr/library/gtm_test/tstdirs.csh
-sed -i "s/SE2/ydbtest/" /usr/library/gtm_test/tstdirs.csh
-
-# Make sure /testarea[n] is writeable, as it can be redirected from the host
-chmod 777 /testarea1
-chmod 777 /testarea2
-chmod 777 /testarea3
-
-# User passed in the Test system as a volume
-if ( -f /YDBTest/com/gtmtest.csh ) then
-	# Symlink to allow user to experiment and get the test to pass
-	echo "Test System passed in as a volume... symlinking test system"
-	rm -r /usr/library/gtm_test/T999
-	ln -s /YDBTest /usr/library/gtm_test/T999
-endif
-
-setenv ydb_test_inside_docker 1
-set pass_env = "-w CI_PIPELINE_ID -w CI_COMMIT_BRANCH -w ydb_test_inside_docker -w gtm_curpro"
+source /usr/library/gtm_test/T999/docker/shared-setup.csh
 
 if ( $#argv == 0 ) then
 	exec su -l gtmtest $pass_env -c "/usr/library/gtm_test/T999/com/gtmtest.csh -h"
@@ -76,10 +28,6 @@ if ( "$argv[1]" == "-shell") then
 	echo "Try running gtmtest -t r134"
 	echo "Type 'ver' to switch versions"
 	exec su -l gtmtest $pass_env
-endif
-
-if ( "$argv[1]" == "-pipelineydb") then
-	exec su -l gtmtest $pass_env -c "/usr/library/gtm_test/T999/docker/pipeline-test-ydb.csh"
 endif
 
 # Run the tests as the test user

docker/shared-setup.csh
@@ -1,0 +1,88 @@
+#!/bin/tcsh
+#################################################################
+#                                                               #
+# Copyright (c) 2025 YottaDB LLC and/or its subsidiaries.       #
+# All rights reserved.                                          #
+#                                                               #
+#       This source code contains the intellectual property     #
+#       of its copyright holder(s), and is made available       #
+#       under a license.  If you do not know the terms of       #
+#       the license, please stop and do not read further.       #
+#                                                               #
+#################################################################
+# Ensure that our hostname does not have dashes as that crashes multiple parts of the test system
+# (it converts hostnames into variables)
+echo "HOSTNAME: $HOST"
+
+# Preamble
+# Start log server; needed by test framework
+rsyslogd
+# needed to create the /var/log/syslog file; as otherwise it won't exist at start
+logger test
+
+# The file is created asynchronously, so this ensures we don't proceed till it's created
+while ( ! -e /var/log/syslog)
+	sleep .01
+end
+
+# Start sshd
+/sbin/sshd
+
+# Add an extra hostname for remote tests
+echo "127.0.0.1 ydbtest" >> /etc/hosts
+
+# Trust localhost
+su - gtmtest -c "ssh-keyscan -t rsa localhost >>& ~/.ssh/known_hosts"
+su - gtmtest -c "ssh-keyscan -t rsa `hostname -I` >>& ~/.ssh/known_hosts"
+su - gtmtest -c "ssh-keyscan -t rsa $HOST >>& ~/.ssh/known_hosts"
+su - gtmtest -c "ssh-keyscan -t rsa ydbtest >>& ~/.ssh/known_hosts"
+
+# Next seds, fix the serverconf.txt file
+## Correct the host; as it differs each time we start docker
+sed -i "s/HOST/$HOST/" /usr/library/gtm_test/serverconf.txt
+sed -i 's|LOG|/var/log/syslog|' /usr/library/gtm_test/serverconf.txt
+sed -i "s/SE1/localhost/" /usr/library/gtm_test/serverconf.txt
+sed -i "s/SE2/ydbtest/" /usr/library/gtm_test/serverconf.txt
+
+# Set hosts on tstdirs.csh file
+sed -i "s/HOST/$HOST/" /usr/library/gtm_test/tstdirs.csh
+sed -i "s/SE1/localhost/" /usr/library/gtm_test/tstdirs.csh
+sed -i "s/SE2/ydbtest/" /usr/library/gtm_test/tstdirs.csh
+
+# Make sure /testarea[n] is writeable, as it can be redirected from the host
+chmod 777 /testarea1
+chmod 777 /testarea2
+chmod 777 /testarea3
+
+# Runner job does not set TERM
+setenv TERM xterm
+
+if ( ! -f /YDBTest/com/gtmtest.csh && -f ${PWD}/com/gtmtest.csh ) then
+	# Copy over the test system to $gtm_tst
+	# This is ineffecient, but not worth optimizing. It copies everything over rather than only changed files.
+	setenv gtm_tst "/usr/library/gtm_test/T999"
+	rsync . -ar --delete $gtm_tst
+	chown -R gtmtest:gtc $gtm_tst
+	git config --global --add safe.directory $gtm_tst
+endif
+
+if ( -f /YDBTest/com/gtmtest.csh ) then
+	# Test system passed in /YDBTest, use that
+	rm -r /usr/library/gtm_test/T999
+	ln -s /YDBTest /usr/library/gtm_test/T999
+	git config --global --add safe.directory /YDBTest
+endif
+
+# Special pipeline set-up for testareas
+# Set-up testareas to be in the current directory so we can upload the artifacts
+if ( $?CI_PIPELINE_ID ) then
+	mkdir -p testarea{1,2,3}
+	# tst_dir can only be one test area, the main one
+	echo "setenv tst_dir ${PWD}/testarea1" >> ~gtmtest/.cshrc
+	chmod 777 ${PWD}/testarea{1,2,3}
+	sed -i "s|/testarea|$PWD/testarea|g" /usr/library/gtm_test/tstdirs.csh
+endif
+
+# Set-up some environment variables to pass to the test system
+setenv ydb_test_inside_docker 1
+setenv pass_env "-w CI_PIPELINE_ID -w CI_COMMIT_BRANCH -w ydb_test_inside_docker"

commit 91e6415a0020c2984e56db5688ee5d44338d2169

[YDB#1105] Sudo tests fail on YDB pipeline
Due to a regression in 0235bf2c5bf39baee1cc99874d4d03b954d544c6, sudo
tests requiring ydbinstall stopped working only inside the YDB pipeline
(YDBTest pipeline was still okay). See
https://gitlab.com/nars1/YDB/-/jobs/10224676723 for a sample failure.

That commit changed the how the pipeline runs, from running as a script
inside of a docker image after some set-up (which happened in
`docker/run.csh`) to running in a script directly invoked by the gitlab
runner. One of the steps that `docker/run.csh` does is to set-up a link
for an imaginary build directory for the use of sudo tests, as the sudo
tests are the only tests that use a file from the build directory. That
step is already in `docker/pipeline-test-ydbtest.csh`, but was forgotten
to be added to the modified file `docker/pipeline-test-ydb.csh`.

docker/pipeline-test-ydb.csh
@@ -62,6 +62,9 @@ if ( $?CI_COMMIT_BRANCH ) then
 	rm ydbtest_open_mrs.json
 endif
 
+# Sudo tests rely on the source code for ydbinstall to be in a specific location
+ln -s /Distrib/YottaDB /Distrib/YottaDB/V999_R999
+
 if ( $?filelist ) then
 	exec $gtm_tst/docker/pipeline-run-changed-tests.csh
 else