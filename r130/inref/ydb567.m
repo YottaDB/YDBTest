76f6f4c9d86497ff7a7e7dd998fed29639cc22ee
Narayanan Iyer <nars@yottadb.com>
2025-09-30 15:52:26 -0400

    [YDB#1181] Fix r130/ydb567 subtest failure due to JOB command now returning lot faster than before
    * After the YDB#1181 code fixes, the above subtest started failing with the following diff.
    
      ```diff
      4d3
      < Hang test finished successfully
      ```
    
    * And the reason turned out to be that the M program `r130/inref/ydb567.m`, does a `job child`
      and then immediately sends a lot of `SIGUSR1` signals to it until it dies and expects it to
      still execute the M program and eventually print a `Hang test finished successfully` message.
    
    * But this does not happen in the test because the child process that has been jobbed off by the
      `job child` has not yet done its initialization of signal handlers etc. This means a SIGUSR1
      signal will `terminate` the process instead of acting like a `mupip intrpt`.
    
    * And the reason this happens now is because the JOB command returned a lot faster due to the
      YDB#1181 code fixes. And so we are not guaranteed that the child process has done its signal
      handler initialization at that point in time.
    
    * The fix is simple and is to wait for the child process to start executing the M program so we
      add a global set in the first line of the `child` label and wait for this global to be set
      in the parent process before we do the SIGUSR1 signal send.
    
    * This meant adding `dbcreate.csh` and `dbcheck.csh` calls to `r130/u_inref/ydb567.csh`.

--- r130/inref/ydb567.m
+++ r130/inref/ydb567.m
@@ -1,6 +1,6 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;								;
-; Copyright (c) 2020-2021 YottaDB LLC and/or its subsidiaries.	;
+; Copyright (c) 2020-2025 YottaDB LLC and/or its subsidiaries.	;
 ; All rights reserved.						;
 ;								;
 ;	This source code contains the intellectual property	;
@@ -10,7 +10,12 @@
 ;								;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 test    ;
+	set ^ready=0
 	job child
+	; Wait for child to be in a position to receive SIGUSR1 signal. Not waiting can cause the child process
+	; to not yet have established its SIGUSR1 signal handler resulting in the process terminating on the
+	; very first signal which would defeat the purpose of the test (to send a lot of SIGUSR1s).
+	for  quit:^ready=1  hang 0.001
 	for i=1:1 quit:$zsigproc($zjob,"SIGUSR1")  hang 0.001
 	do ^waitforproctodie($zjob,300)
 	quit
@@ -18,6 +23,7 @@ test    ;
 child   ;
 	; The performance part of this test is disabled for both pro and dbg on single CPU machines
 	; and for dbg on ARM as it has failed in the past due to slowness.
+	set ^ready=1
 	set perfdisabled=$ztrnlnm("gtm_test_singlecpu")
 	set:(("dbg"=$ztrnlnm("tst_image"))&("HOST_LINUX_ARMVXL"=$ztrnlnm("gtm_test_os_machtype"))) perfdisabled=1
 	set $zinterrupt="if $increment(intrptcnt)"
--- r130/u_inref/ydb567.csh
+++ r130/u_inref/ydb567.csh
@@ -1,7 +1,7 @@
 #!/usr/local/bin/tcsh -f
 #################################################################
 #								#
-# Copyright (c) 2020 YottaDB LLC and/or its subsidiaries.	#
+# Copyright (c) 2020-2025 YottaDB LLC and/or its subsidiaries.	#
 # All rights reserved.						#
 #								#
 #	This source code contains the intellectual property	#
@@ -11,8 +11,22 @@
 #								#
 #################################################################
 
+$gtm_tst/com/dbcreate.csh mumps >& dbcreate.out
+if ($status) then
+	echo "dbcreate.csh failed. [cat dbcreate.out] output follows."
+	cat dbreate.out
+	exit -1
+endif
+
 echo '# This test ensures that a MUPIP INTRPT during a hang in a for loop does not'
 echo '# result in an assert failure. Previously, this would result in an assert'
 echo '# failure when done on a debug build.'
 $ydb_dist/yottadb -run test^ydb567
 cat ydb567.mjo
+
+$gtm_tst/com/dbcheck.csh >& dbcheck.out
+if ($status) then
+	echo "dbcheck.csh failed. [cat dbcheck.out] output follows."
+	cat dbcheck.out
+	exit -1
+endif
