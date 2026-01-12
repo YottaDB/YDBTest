YottaDB/YDBTest

commit c578e5f0191b6228d9beb85ca37736ad8eac0e8e

Fix multi-host imptp.csh test failures due to clang compiler and gcc linker (LVNSET-E-LINKFAIL)
Background
----------
* During in-house testing, we noticed a few multi-host tests (e.g. `dual_fail1/dual_fail2` subtest)
  fail with the following symptom.

  ```diff
  > IMPTP-E-FAIL : imptp.csh invocation had errors in file [.../dual_fail_1_1/dual_fail2/imptp.out]. Error output follows.
  > LVNSET-E-LINKFAIL : Linking simplethreadapi_imptp failed. See simplethreadapi_imptp.map for details
  ```

* Looking at `simplethreadapi_imptp.map` showed lines of the following form.

  ```
    OUTPUT(simplethreadapi_imptp ...ld: .../libyottadb.so: undefined reference to `__asan_stack_free_7'
  ```

Issue
-----
* This failure symptom was already fixed as part of commit 52968521 where the cause was using a clang
  compiler with a gcc linker.

* But in this failure case, the local and the remote hosts both had YottaDB built with CLANG.

* And therefore, we should have used `clang` as the linker. But we instead used `gcc` as the linker and
  this is why we ended up with the above error.

* The cause is a recent change commit 1e14592d which added a call to `gtm_env.csh` at the end of
  `com/remote_getenv.csh`.

* This meant that the call to `set_ydb_build_env_vars.csh` done in `remote_getenv.csh` by commit 52968521
  happened BEFORE the call to the recently added `gtm_env.csh`. And this meant that the `gt_cc_compiler`
  env var set by `set_ydb_build_env_vars.csh` would get incorrectly overwritten by the later call to
  `gtm_env.csh`.

* Commit 1e14592d should have added the `gtm_env.csh` call BEFORE the `set_ydb_build_env_vars.csh` call.
  That is where the issue is.

Fix
---
* The `set_ydb_build_env_vars.csh` call is now moved to AFTER the `gtm_env.csh` call in `remote_getenv.csh`.

* While at this, I also enhanced `com/imptp.csh` to print the actual compiler and linker commands that it

com/remote_getenv.csh
@@ -4,7 +4,7 @@
 # Copyright (c) 2002-2015 Fidelity National Information		#
 # Services, Inc. and/or its subsidiaries. All rights reserved.	#
 #								#
-# Copyright (c) 2017-2025 YottaDB LLC and/or its subsidiaries.	#
+# Copyright (c) 2017-2026 YottaDB LLC and/or its subsidiaries.	#
 # All rights reserved.						#
 #								#
 #	This source code contains the intellectual property	#
@@ -66,12 +66,6 @@ source $gtm_tst/com/set_specific.csh
 # this will take care of gtmroutines and locale set up defintions along with other regular setups
 source $gtm_tst/com/getenv.csh
 
-# Check if the YottaDB build has ASAN enabled on the remote side of a multi-host test. If so, need to set
-# ASAN_OPTIONS env var (among other things) on the remote side to disable leak check (LSAN) as that can cause failures.
-# Note: Do this BEFORE the "source $gtm_tst/com/set_gtm_machtype.csh" step as that invokes YottaDB and would otherwise
-# fail (due to memory leaks identified) in case ASAN is enabled in the current build.
-source $gtm_tst/com/set_ydb_build_env_vars.csh
-
 # override machtype environment from primary
 source $gtm_tst/com/set_gtm_machtype.csh
 
@@ -93,4 +87,10 @@ if (-e $gtm_tools/gtm_env.csh) then
 	source $gtm_tools/gtm_env.csh		# this sets "gt_cc_options_common" etc. correctly to reflect remote host
 	source $gtm_tst/com/set_specific.csh	# this sets "gtt_cc_shl_options" etc. correctly to reflect the remote host
 endif
+
+# Check if the YottaDB build has ASAN enabled on the remote side of a multi-host test. If so, need to set
+# ASAN_OPTIONS env var (among other things) on the remote side to disable leak check (LSAN) as that can cause failures.
+# Note: Do this BEFORE the "source $gtm_tst/com/set_gtm_machtype.csh" step as that invokes YottaDB and would otherwise
+# fail (due to memory leaks identified) in case ASAN is enabled in the current build.
+source $gtm_tst/com/set_ydb_build_env_vars.csh
 

com/imptp.csh
@@ -4,7 +4,7 @@
 # Copyright (c) 2002-2015 Fidelity National Information 	#
 # Services, Inc. and/or its subsidiaries. All rights reserved.	#
 #								#
-# Copyright (c) 2018-2025 YottaDB LLC and/or its subsidiaries.	#
+# Copyright (c) 2018-2026 YottaDB LLC and/or its subsidiaries.	#
 # All rights reserved.						#
 #								#
 #	This source code contains the intellectual property	#
@@ -201,8 +201,18 @@ xyz
 		# from a previous "imptp.csh" invocation in the same subtest, skip rebuilding it.
 		if (! -e $exefile) then
 			cp $gtm_tst/com/$file .
-			$gt_cc_compiler $gtt_cc_shl_options -I$gtm_tst/com -I$ydb_dist $file
-			$gt_ld_linker $gt_ld_option_output $exefile $gt_ld_options_common $exefile.o $gt_ld_sysrtns $ci_ldpath$ydb_dist -L$ydb_dist $tst_ld_yottadb $gt_ld_syslibs >& $exefile.map
+			set cmd = "$gt_cc_compiler $gtt_cc_shl_options -I$gtm_tst/com -I$ydb_dist $file"
+			echo "-----------"
+			echo "# Compiling"
+			echo "-----------"
+			echo $cmd
+			$cmd
+			set cmd = "$gt_ld_linker $gt_ld_option_output $exefile $gt_ld_options_common $exefile.o $gt_ld_sysrtns $ci_ldpath$ydb_dist -L$ydb_dist $tst_ld_yottadb $gt_ld_syslibs"
+			echo "-----------"
+			echo "# Linking"
+			echo "-----------"
+			echo "$cmd >& $exefile.map"
+			$cmd >& $exefile.map
 			if (0 != $status) then
 				echo "LVNSET-E-LINKFAIL : Linking $exefile failed. See $exefile.map for details"
 				exit -1

commit 00068868ba051d446964ff16e823e886d34740f4

Fix various multi-host test failures (ERROR: LeakSanitizer: detected memory leaks)
Background
----------
* Various multi-host tests started failing with the following symptom.

  ```diff
  $ cat increment_16/incrbas/incrbas.diff
  2a3,37
  >
  > =================================================================
  > ==36259==ERROR: LeakSanitizer: detected memory leaks
  > .
  > .
  ```

Issue
-----
* This turned out to be a regression in c578e5f0. As part of that commit, the call to
  `com/set_ydb_build_env_vars.csh` was moved from earlier to the end of `com/remote_getenv.csh`.

* As a side effect of the above change, the invocation of `com/set_gtm_machtype.csh`, which was
  previously immediately AFTER the `com/set_ydb_build_env_vars.csh` call ended up happening BEFORE.

* But inside `com/set_gtm_machtype.csh`, we have an invocation of `mumps -direct`.

  **com/set_gtm_machtype.csh**
  ```sh
     31 set gtm_platform=`echo 'write $piece($zversion," ",4),! halt' | $gtm_exe/mumps -direct | tail -n 1`
  ```

* It is this invocation which started spewing the `ERROR: LeakSanitizer: detected memory leaks` output.

* And this is because the `ASAN_OPTIONS` env var (which would be set to disable such output) was being
  set in the `com/set_ydb_build_env_vars.csh` call. And this call now happens AFTER resulting in the
  `ASAN_OPTIONS` env var not being set at line 31 above.

Fix
---
* The fix is simple and is to move the `com/set_gtm_machtype.csh` call to immediately AFTER the new
  location of the `com/set_ydb_build_env_vars.csh` call (after the c578e5f0 changes). This way we
  continue to maintain the two calls one after the other both before and after c578e5f0.

com/remote_getenv.csh
@@ -66,9 +66,6 @@ source $gtm_tst/com/set_specific.csh
 # this will take care of gtmroutines and locale set up defintions along with other regular setups
 source $gtm_tst/com/getenv.csh
 
-# override machtype environment from primary
-source $gtm_tst/com/set_gtm_machtype.csh
-
 # This script is called to set env vars on the remote side of a multi-host test. But for multi-host tests,
 # "com/do_random_settings.csh" disables "gtm_statshare" (see reasoning in a comment there). So do the same
 # here now that we are setting up the remote host env vars. Note that in addition to setting "gtm_statshare" to 0,
@@ -93,4 +90,7 @@ endif
 # Note: Do this BEFORE the "source $gtm_tst/com/set_gtm_machtype.csh" step as that invokes YottaDB and would otherwise
 # fail (due to memory leaks identified) in case ASAN is enabled in the current build.
 source $gtm_tst/com/set_ydb_build_env_vars.csh
+
+# Override machtype environment from primary.
+source $gtm_tst/com/set_gtm_machtype.csh
 