c8be07a3aab995d14f06e14cb7e403349e42dcff
Narayanan Iyer <nars@yottadb.com>
2026-01-12 15:20:50 -0500

    Fix sudo/ydb910 subtest hang on slow systems (sudo timeout due to multiple ydbinstall --from-source)
    * After the `sudo timeout` in in-house systems were made consistent and set to the default of 15 minutes
      the `sudo_0/ydb910` subtest started hanging on systems which took a while to do a YottaDB source build.
      The hang symptom was the following.
    
      ```
      sudo/ydb910 has been running for 18000 seconds. Check if it is hung
      ```
    
    * This was because `sudo/u_inref/ydb910.sh` invocation took more than 15 minutes (due to 2 YottaDB
      source builds) and that caused the default sudo timeout of 15 minutes to expire causing a `$sudostr`
      call in `sudo/u_inref/ydb910.csh` to prompt for the sudo password after `sudo/u_inref/ydb910.sh`
      returned.
    
    * Fortunately, the maximum time that YottaDB builds take is around 8 minutes on the slowest in-house
      system. Therefore this is fixed by doing a `sudo -v` in between the 2 `ydbinstall --from-source`
      calls in `sudo/u_inref/ydb910.sh`.

--- sudo/u_inref/ydb910.sh
+++ sudo/u_inref/ydb910.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #################################################################
 #								#
-# Copyright (c) 2022-2025 YottaDB LLC and/or its subsidiaries.	#
+# Copyright (c) 2022-2026 YottaDB LLC and/or its subsidiaries.	#
 # All rights reserved.						#
 #								#
 #	This source code contains the intellectual property	#
@@ -15,7 +15,6 @@ mkdir ydb_build
 echo "# Building YottaDB with Default Compiler"
 echo '# Running [/Distrib/YottaDB/$1/$2/yottadb_r*/ydbinstall --installdir $3 --from-source --source-build-dir $PWD/ydb_build --overwrite-existing --utf8 --user $USER $4 > ydbinstall_fromsource.txt 2>&1]'
 /Distrib/YottaDB/$1/$2/yottadb_r*/ydbinstall --installdir $3 --from-source --source-build-dir $PWD/ydb_build --overwrite-existing --utf8 --user $USER $4 > ydbinstall_fromsource.txt 2>&1
-
 status=$?
 if [ 0 != $status ]; then
 	echo "ydbinstall returned a non-zero status: $status"
@@ -27,10 +26,16 @@ echo ""
 echo "# YottaDB installed with Default Compiler"
 tail -2 ydbinstall_fromsource.txt
 
+# This script does 2 invocations of [ydbinstall --from-source]. A "--from-source" invocation does a full build of YottaDB
+# from source and could take 8 or more minutes on slow systems. In that case, the 2 invocations would end up taking more
+# than the default sudo timeout of 15 minutes. And this would mean that a later "$sudostr" invocation in the caller script
+# ("sudo/u_inref/ydb910.csh") would prompt the user for the sudo password effectively resulting in a test hang (as the test
+# would be running in the background). To avoid this, make sure sudo timeout gets extended/reset between the 2 invocations.
+sudo -v
+
 echo "# Building YottaDB with Clang"
 echo '# Running [/Distrib/YottaDB/$1/$2/yottadb_r*/ydbinstall --installdir $3 --from-source --compiler clang --source-build-dir $PWD/ydb_build --overwrite-existing --utf8 --user $USER $4 > ydbinstall_fromsource2.txt 2>&1]'
 /Distrib/YottaDB/$1/$2/yottadb_r*/ydbinstall --installdir $3 --from-source --compiler clang --source-build-dir $PWD/ydb_build --overwrite-existing --utf8 --user $USER $4 > ydbinstall_fromsource2.txt 2>&1
-
 status=$?
 if [ 0 != $status ]; then
 	echo "ydbinstall returned a non-zero status: $status"
