YottaDB/YDBTest

commit 2ab1b145389c2bc0ce548d4bd0476f5aa3db356e

Version of imptp for YDBGo v2 and also make go/unit_tests run v2 unit tests
Create a version of imptpgo.go for YDBGo v2.
Also made go/unit_tests run v2 unit tests.

Improvements to imptpgo2 as compared with imptpgo:
- Added significant amounts of documentation, even to the M version as it was poorly documented which made it difficult to decipher.
  - Document defaults on inputs environment variables and other docs clarity improvements
  - Document the need to set environment variable gtm_badchar=no or jobs bomb out.
  - Document pseudo-random index generator and fill algorithms.
  - Document a handy command for a clean debug run of imptp.m (somewhat obscure) and imptpgo2
- Merged the 3 .go files of v1 (imptpgo.go) back into one go file for v2 (imptpgo2.go). This makes it easier to build/test.
- Add --clean option to clean database from previous run first: eases running standalone
- Add --verbose option for debugging purposes
- Remove requirement for gtm8086 subtest to run via imptp.m
- Allow <jobs_to_start> parameter on the command line to ease debugging -- handy to specify 1 or 2 with --verbose
- Convert helper2 to Go for clarity and to test triggers via M call-in
- Factor spawn() function out of main parent() process function.
- Add xecute() call-in function to more easily call M VIEW command, etc.
- Remove ztrcmd M local since I can now set ztrigger directly with xecute() function
- Simplify setup logic that sets isTP and tpNoIso
- Remove redundant test of locks
- Updated Dockerfile to install go version 1.25.4 since it needs to be updated to at least 1.24 for YDBGo v2
- Added go.mod file to YDBTest/com so you can easily build or run go files from this directory with 'go build <file1.go> [<file2.go> ...]'.
- Add comment about why 300s database open timeout is necessary per [this discussion](https://gitlab.com/YottaDB/DB/YDBTest/-/merge_requests/2480#note_3031241642).

com/setupgoenv.csh
@@ -1,6 +1,6 @@
 #################################################################
 #								#
-# Copyright (c) 2019-2025 YottaDB LLC and/or its subsidiaries.	#
+# Copyright (c) 2019-2026 YottaDB LLC and/or its subsidiaries.	#
 # All rights reserved.						#
 #								#
 #	This source code contains the intellectual property	#
@@ -12,135 +12,94 @@
 #
 # This script is meant to be source'd by golang tests to both setup the golang environment and
 # fetch the yottadb package into it before building/running the specific golang test.
-#
+# However, set the following defaults so that the user can run it from the shell for testing purposes
+if ! $?test_encryption setenv test_encryption "NON_ENCRYPT"
+if ! $?test_awk setenv tst_awk awk
+if ! $?grep setenv grep grep
+if ! $?gtm_test_os_machtype source set_gtm_machtype.csh
+
 set tstpath = `pwd`
 setenv PKG_CONFIG_PATH $ydb_dist
-setenv GOPATH $tstpath/go/
-setenv GOPROXY https://proxy.golang.org/cached-only
-setenv GO111MODULE on	# Need this for "go get" to work. Starting "go 1.22", go get fails to work if GO111MODULE=off
-set go_repo="lang.yottadb.com/go/yottadb"
-mkdir go
+# set a GOPATH outside current directory to avoid it being subsequently zipped. This prevents permission
+# errors when zipping test results since GOPATH contains subdirectories with read-only permissions.
+# This particular path makes it common to all subtests run this instance of gtmtest: avoids multiple downloads per gtmtest run.
+setenv GOPATH $tst_dir/$gtm_tst_out/gopath
+# Ensure go toolchains are not downloaded by the test as this creates huge artifacts
+# in GOPATH (up to gigabytes) every time imptp.csh is run.
+# Ensure the machine has a Go version at least as recent as required in go.mod and by YDBGo's go.mod files.
+setenv GOTOOLCHAIN local
+
+# Set cannonical url of YDBGo when properly referenced by 'go get'
+set ydbgo_url="lang.yottadb.com/go/yottadb"
 
 # Now that "go get" is only supported inside of a module, create a go.mod file first.
 # This step was not needed until "go 1.21" as long as "GO111MODULE" env var was set to "off".
 # But starting "go 1.22", that did not work (see YDBTest#387 for more details).
-go mod init gitlab.com/YottaDB/Lang/YDBGo >& go_mod.out
+cp $gtm_tst/com/go.{mod,sum} . >& go_mod.out || \
+	echo "[cp $gtm_tst/com/go.{mod,sum} .] failed with status [$status]:" && cat go_mod.out && exit 1
 
-# Retrieve yottadb package from the repository using "go get".
-set cmdtorunprefix = "go get"
-set cmdtorunsuffix = "-d -v -x -t $go_repo"
-set cmdtorun = "$cmdtorunprefix $cmdtorunsuffix"
-echo "# Running : $cmdtorun"
-# Occasionally we have seen "TLS handshake timeout" or "i/o timeout" failures on slow boxes if the "go get" takes
-# approximately more than 15 seconds to finish (which can happen on the slow ARMV6L boxes or over less than ideal
-# internet connections). That timeout does not seem to be user configurable either so we handle that by retrying
-# the "go get" for a few times before signaling failure.
-set retry = 0
-set maxretry = 5
-while ($retry < $maxretry)
-	$cmdtorun |& $tst_awk '{print strftime("%T"),":",$0}' >& go_get.log
-	set status1 = $status
-	mv go_get.log go_get_$retry.log
-	if (! $status1) then
-		# "go get" succeeded. Break out of retry loop.
-		break
-	endif
-	$grep -q -e "net/http: TLS handshake timeout" go_get_$retry.log
-	if (! $status) then
-		# It was a TLS handshake timeout error. Try with "-insecure" to see if that helps.
-		# This has been seen to really make a difference at least on 1-CPU systems that are otherwise loaded.
-		set cmdtorun = "$cmdtorunprefix -insecure $cmdtorunsuffix"
-	else
-		# It was some other error (e.g. "i/o timeout" or "The requested URL returned error: 500")
-		# We retry on all types of errors.
-		set cmdtorun = "$cmdtorunprefix $cmdtorunsuffix"
-	endif
-	@ retry = $retry + 1
-end
-if ($status1) then
-	echo "TEST-E-FAILED : [$cmdtorun] returned failure status of $status1. Total of $retry retries. Output in go_get*.log"
-	foreach file (go_get*.log)
-		echo " --> Output of $file"
-		cat $file
-	end
-	exit 1
-endif
-
-# go/pkg directory contains subdirectories with read-only permissions after a "go get" so give read-write permissions
-# as later the test framework would try to gzip the files in case of a test failure and that would require creating a
-# new file and fail with a "Permission denied" error.
-chmod -R +w go/pkg
-
-# go get with GO111MODULE=on no longer downloads the git repository. But later parts of this script and the caller test
-# rely on that directory existing. So download the git repo using "git clone".
-mkdir -p go/src/lang.yottadb.com/go/yottadb/
-git clone https://gitlab.com/YottaDB/Lang/YDBGo go/src/lang.yottadb.com/go/yottadb >& gitclone.log
-set status1 = $status
-if ($status1) then
-	echo "TEST-E-FAILED : [git clone] returned failure status of $status1. Output below and in gitclone.log"
-	cat gitclone.log
-	exit 1
-endif
-
-cd go/src/$go_repo
-if ($?ydb_test_go_repo_dir) then
-	# If env var "ydb_test_go_repo_dir" is defined, use this as the path of the go repo instead of the go repo on gitlab.
-	git remote add tmp $ydb_test_go_repo_dir
-	set status1 = $status
-	if ($status1) then
-		echo "TEST-E-FAILED : [git remote add tmp $ydb_test_go_repo_dir] returned failure status of $status1"
-		exit 1
-	endif
-	git fetch tmp >& git_fetch.log
-	set status1 = $status
-	if ($status1) then
-		echo "TEST-E-FAILED : [git fetch tmp] returned failure status of $status1. Output below and in git_fetch.log"
-		cat git_fetch.log
-		exit 1
-	endif
-	if ($?ydb_test_go_repo_branch) then
-		# If env var "ydb_test_go_repo_branch" is defined, use that as the branch
-		git checkout -b tmp tmp/$ydb_test_go_repo_branch >& git_checkout.log
-		set status1 = $status
-		if ($status1) then
-			echo "TEST-E-FAILED : [git checkout -b tmp tmp/$ydb_test_go_repo_branch] returned failure status of $status1. Output below and in git_checkout.log"
-			cat git_checkout.log
-			exit 1
-		endif
-	else
-		# Else use "master" branch as the latest developed code
-		git checkout -b tmp tmp/master >& git_checkout.log
-		set status1 = $status
-		if ($status1) then
-			echo "TEST-E-FAILED : [git checkout -b tmp tmp/master] returned failure status of $status1. Output below and in git_checkout.log"
-			cat git_checkout.log
-			exit 1
-		endif
-	endif
+# If $ydbgo_repo points to a URL, check out YDBGo from a git repo instead of using 'go get'.
+# If $ydbgo_repo points to a local filesystem git repo, use that, including any uncommitted changes.
+# If testing with docker, you can mount your local YDBGo repo as a docker volume and point to that.
+# The first half of the if statement would work by itself, but the second 'go get' version to test what users will mostly do.
+if ( $?ydbgo_repo ) then
+	# If it's a pathname, convert it to file:// URL format
+	set ydbgo_repo_path = "$ydbgo_repo"
+	if ( "$ydbgo_repo" !~ "*://*" ) setenv ydbgo_repo "file://$ydbgo_repo"
+	echo "# Cloning $ydbgo_repo to $tstpath/YDBGo"
+	# clone -depth 1 gets only the latest version for testing (faster)
+	rm $tstpath/YDBGo go.work -rf # remove first in case setupgoenv.csh gets run twice
+	git clone -q --depth 1 $ydbgo_repo $tstpath/YDBGo >>& ydbgo_clone.out || \
+		echo "[git clone --depth 1 $ydbgo_repo:q] failed with status [$status]:" && cat ydbgo_clone.out && exit 1
+	# If it's a local file repository diff + apply includes any uncommitted working files from the local YDBGo
+	if ( "$ydbgo_repo" =~ "file://*" ) git -C "$ydbgo_repo_path" diff | git -C $tstpath/YDBGo apply --allow-empty -
+	# include '.' below for several tests that assume it's included
+	go work init . $tstpath/YDBGo $tstpath/YDBGo/v2 |& tee go_work.out
+	set goget = "true" # needn't download ydbgo since it's found via ../go.work (e.g. in pseudoBank.csh)
 else
-	# We used the go repo on gitlab. Check out the "master" branch.
-	git checkout master >& git_checkout.log
-	set status1 = $status
+	# Retrieve yottadb package from the repository using "go get".
+	set goget = "go get -x -t $ydbgo_url $ydbgo_url/v2"
+	echo "# Running : $goget"
+	# Occasionally we have seen "TLS handshake timeout" or "i/o timeout" failures on slow boxes if the "go get" takes
+	# approximately more than 15 seconds to finish (which can happen on the slow ARMV6L boxes or over less than ideal
+	# internet connections). That timeout does not seem to be user configurable either so we handle that by retrying
+	# the "go get" for a few times before signaling failure.
+	set retry = 0
+	set maxretry = 5
+	while ($retry < $maxretry)
+		$goget |& $tst_awk '{print strftime("%T"),":",$0}' >& go_get.log
+		set status1 = $status
+		mv go_get.log go_get_$retry.log
+		if (! $status1) then
+			# "go get" succeeded. Break out of retry loop.
+			break
+		endif
+		$grep -q -e "net/http: TLS handshake timeout" go_get_$retry.log
+		if (! $status) then
+			# It was a TLS handshake timeout error. Try with "-insecure" to see if that helps.
+			# This has been seen to really make a difference at least on 1-CPU systems that are otherwise loaded.
+			setenv GOINSECURE $ydbgo_url
+		endif
+		@ retry = $retry + 1
+	end
 	if ($status1) then
-		echo "TEST-E-FAILED : [git checkout master] returned failure status of $status1. Output below and in git_checkout.log"
-		cat git_checkout.log
+		echo "TEST-E-FAILED : [$goget] returned failure status of $status1. Total of $retry retries. Output in go_get*.log"
+		foreach file (go_get*.log)
+			echo " --> Output of $file"
+			cat $file
+		end
 		exit 1
 	endif
 endif
-cd -
 
 if ("ENCRYPT" == "$test_encryption" ) then
 	# Set env var to absolute path (not relative path) since go processes will start from subdirectories
 	setenv gtmcrypt_config `pwd`/gtmcrypt.cfg
 endif
 
-# When using a pure Go application, one can do 'go run xxx.go' and it will do that. This does not work when cgo is involved -
-# which is 100% of our Go applications currently since they all test the Go wrapper.
-set gobuild = "go build"
-setenv GO111MODULE off	# Keep this off as we rely on this for "go build" commands to work with GOPATH programs in YDBTest
-			# like com/imptpgo.go and com/impjobgo.go. Note that starting go 1.22, "go get" runs as if
-			# GO111MODULE=on irrespective of the env var setting but we handle that separately later.
-set gotest = "go test"
+# Historically, `go run xxx.go` did not work with CGo, so we use go build
+# -buildvcs=false prevents Go from trying to contact the git repository to get version information
+set goflags = "-buildvcs=false"
 
 # If ASAN enabled and Go version < 1.18, using go build flag `-fsanitize=address`
 # But if ASAN enabled and Go version >= 1.18, using `-asan` flag instead
@@ -148,11 +107,10 @@ source $gtm_tst/com/is_libyottadb_asan_enabled.csh
 if ($gtm_test_libyottadb_asan_enabled) then
 	# libyottadb.so was built with asan enabled. Do the same with the go executables.
 	set asanflags = "-asan"
-	set gobuild = "$gobuild $asanflags"
-	set gotest = "$gotest $asanflags"
+	set goflags = "$goflags -asan"
 
 	# ------------------------------------------------------------------------------------
-	# The below comment and code is similar to that in sudo/u_inref/setinstalloptions.csh
+	# The following comment and code is similar to that in sudo/u_inref/setinstalloptions.csh
 	# ------------------------------------------------------------------------------------
 	# If libyottadb.so has been built with CLANG + ASAN, we started seeing link time errors like the following
 	# from a "go build" when run on a SUSE SLED SP7 system or a RHEL 9 system.
@@ -165,7 +123,7 @@ endif
 
 # Random Go environment settings
 # ydb_go_race_detector - determines if the go -race flag should be used
-# GOGC - The rate Go does garbage collection; a smaller value increases frequency; default is 100
+# GOGC is the rate at which Go does garbage collection; a smaller value increases frequency; default is 100
 if (! $?gtm_test_replay || ! $?ydb_go_race_detector_on) then
 	echo "# Go environment variables" >> settings.csh
 	setenv ydb_go_race_detector_on `$gtm_tst/com/genrandnumbers.csh 1 0 1`
@@ -186,11 +144,14 @@ if (! $?gtm_test_replay || ! $?ydb_go_race_detector_on) then
 	endif
 endif
 
-if ($ydb_go_race_detector_on) then
-	# Randomly enable go race detector
-	set gobuild = "$gobuild -race"
-	set gotest = "$gotest -race"
-endif
+# Randomly enable go race detector
+if ($ydb_go_race_detector_on) set goflags = "$goflags -race"
+
+# These may be useful but if user needs to use Go "-C" flag they will need to use $goflags directly since -C must be first flag
+set gobuild = "go build $goflags"
+set gotest = "go test $goflags"
+
 # Capture random setting in file for later analysis in case of test failures
 set | $grep ^go >& govars.txt
+# prevent exit code of grep above from being the exit code of this script
 exit 0

.gitignore
@@ -1,6 +1,6 @@
 ###########################################################
 #
-# Copyright (c) 2024 YottaDB LLC and/or its subsidiaries.
+# Copyright (c) 2024-2026 YottaDB LLC and/or its subsidiaries.
 # All rights reserved.
 #
 #	This source code contains the intellectual property
@@ -12,3 +12,10 @@
 
 # ignore some of the common test artifacts created when running test scripts directly
 *.o
+com/go.sum
+com/go.work
+com/go.work.sum
+com/impjob_imptp*.mj*
+com/imptpgo
+com/impjobgo
+com/imptpgo2

com/go.mod
@@ -1,0 +1,10 @@
+module gitlab.com/YottaDB/DB/YDBTest/com
+
+go 1.24.0
+
+require (
+	lang.yottadb.com/go/yottadb v1.2.9
+	lang.yottadb.com/go/yottadb/v2 v2.0.4
+)
+
+require github.com/outrigdev/goid v0.3.0 // indirect

com/go.sum
@@ -1,0 +1,14 @@
+github.com/davecgh/go-spew v1.1.1 h1:vj9j/u1bqnvCEfJOwUhtlOARqs3+rkHYY13jYWTU97c=
+github.com/davecgh/go-spew v1.1.1/go.mod h1:J7Y8YcW2NihsgmVo/mv3lAwl/skON4iLHjSsI+c5H38=
+github.com/outrigdev/goid v0.3.0 h1:t/otQD3EXc45cLtQVPUnNgEyRaTQA4cPeu3qVcrsIws=
+github.com/outrigdev/goid v0.3.0/go.mod h1:hEH7f27ypN/GHWt/7gvkRoFYR0LZizfUBIAbak4neVE=
+github.com/pmezard/go-difflib v1.0.0 h1:4DBwDE0NGyQoBHbLQYPwSUPoCMWR5BEzIk/f1lZbAQM=
+github.com/pmezard/go-difflib v1.0.0/go.mod h1:iKH77koFhYxTK1pcRnkKkqfTogsbg7gZNVY4sRDYZ/4=
+github.com/stretchr/testify v1.8.2 h1:+h33VjcLVPDHtOdpUCuF+7gSuG3yGIftsP1YvFihtJ8=
+github.com/stretchr/testify v1.8.2/go.mod h1:w2LPCIKwWwSfY2zedu0+kehJoqGctiVI29o6fzry7u4=
+gopkg.in/yaml.v3 v3.0.1 h1:fxVm/GzAzEWqLHuvctI91KS9hhNmmWOoWu0XTYJS7CA=
+gopkg.in/yaml.v3 v3.0.1/go.mod h1:K4uyk7z7BCEPqu6E+C64Yfv1cQ7kz7rIZviUmN+EgEM=
+lang.yottadb.com/go/yottadb v1.2.9 h1:oCJIF7HEdFqmuD6YiA/Xg7l7dEn65xNbObLyEUAHsnw=
+lang.yottadb.com/go/yottadb v1.2.9/go.mod h1:cZBFvm9RZ64GAHn4ix0MwqVkFedVrQVtUf7bCkXNvew=
+lang.yottadb.com/go/yottadb/v2 v2.0.4 h1:sIGDJBKfhPkAnSs9Uejl73L6xTvB8zsdUNqLRGaJicg=
+lang.yottadb.com/go/yottadb/v2 v2.0.4/go.mod h1:sfivszJf55NHAUbtaTRw5jnDIOdlNPpWZZr4gIQGFUs=

com/imp.go
@@ -1,6 +1,6 @@
 //////////////////////////////////////////////////////////////////
 //                                                              //
-// Copyright (c) 2019 YottaDB LLC. and/or its subsidiaries.     //
+// Copyright (c) 2019-2026 YottaDB LLC. and/or its subsidiaries.//
 // All rights reserved.                                         //
 //                                                              //
 //      This source code contains the intellectual property     //
@@ -10,7 +10,7 @@
 //                                                              //
 //////////////////////////////////////////////////////////////////
 
-package imp
+package main
 
 import (
 	"fmt"

com/imptp.csh
@@ -37,6 +37,7 @@ if (-e $gtm_tools/check_trigger_support.csh) then
 	endif
 endif
 
+if !($?gtm_test_trigger) setenv gtm_test_trigger 0
 if (($gtm_test_trigger) && !($?test_specific_trig_file)) set trigger_load=1
 \pwd
 if ($gtm_test_crash == 1)	echo "This is a crash test"
@@ -59,19 +60,19 @@ if ($gtm_test_dbfill == "IMPTP" || $gtm_test_dbfill == "IMPZTP") then
 		# If rand = 1 (i.e. ydb_app_ensures_isolation env var is not set) -> do VIEW "NOISOLATION" command later in imptp.m
 	endif
 	if (($?trigger_load) && ($gtm_test_dbfill == "IMPTP")) then
-		# Test has specified -trigger and we are going to use imptp.m.
+		# Test has specified -trigger and we are going to use the imptp implementation of dbfill
 		# Load triggers in the database.
 		$gtm_exe/mumps -run dollarztrigger $gtm_tst/com/imptp.trg
 		$gtm_exe/mumps -run ztrsupport 'if $ztrigger("file",$ztrnlnm("gtm_tst")_"/com/imptpztr.trg")'
 	endif
 	setenv gtm_badchar "no"
 	set rust_supported = `tcsh $gtm_tst/com/is_rust_supported.csh`
-	# Randomly choose to run M (rand=0), C simpleAPI (rand=1), C simpleThreadedAPI (rand=2), Python (rand=3), Golang (rand=4), or Rust (rand=5) version of imptp
+	# Randomly choose to run M (rand=0), C simpleAPI (rand=1), C simpleThreadedAPI (rand=2), Python (rand=3), YDBGo v1 (rand=4), Rust (rand=5) version of imptp, YDBGo v2 (rand=6)
 	# Note that if a new choice gets added above, "imptpflavor" entryref in "com/imptp.m" needs to be correspondingly updated.
 	if (! $?gtm_test_replay || ! $?ydb_imptp_flavor) then
 		if ($?ydb_imptp_flavor) then
-			if ((0 > $ydb_imptp_flavor) || (5 < $ydb_imptp_flavor)) then
-				echo "TEST-E-FAILED : Invalid flavor of imptp specified: $ydb_imptp_flavor - allowed values, 0, 1, 2, 3, 4, or 5"
+			if ((0 > $ydb_imptp_flavor) || (6 < $ydb_imptp_flavor)) then
+				echo "TEST-E-FAILED : Invalid flavor of imptp specified: $ydb_imptp_flavor - allowed values, 0 to 6"
 				exit 1
 			else if (5 == $ydb_imptp_flavor && true != $rust_supported) then
 				echo "# TEST-E-FAILED : rust flavor was explicitly specified, but Rust is not supported on this platform"
@@ -234,7 +235,7 @@ xyz
 			source python/.venv/bin/activate.csh
 		endif
 		# Using YDBPython with ASAN requires 2 env vars to be set.
-		# ASAN_OPTIONS and LD_PRELOAD. See YottaDB/Lang/YDBPython@479e80a2 for details.
+		# ASAN_OPTIONS and LD_PRELOAD. For details see: https://gitlab.com/YottaDB/Lang/YDBPython/-/commit/479e80a2
 		# YDBTest test framework already sets ASAN_OPTIONS. So only need to set LD_PRELOAD.
 		if ($gtm_test_libyottadb_asan_enabled) then
 			setenv LD_PRELOAD `gcc -print-file-name=libasan.so`
@@ -244,55 +245,12 @@ xyz
 		set exefile = "imptp.py"
 		breaksw
 	case 4: # Run Golang wrapper (uses SimpleThreadAPI) flavor of imptp
-		if (! -e go) then # if no go environment setup yet, do it
-			source $gtm_tst/com/setupgoenv.csh # Do our golang setup (sets $tstpath)
-			set status1 = $status
-			if ($status1) then
-				echo "TEST-E-FAILED : [source $gtm_tst/com/setupgoenv.csh] failed with status [$status1]"
-				exit 1
-			endif
-		endif
-		if (! -e imptpgo) then # if imptpgo hasn't been built yet, get it and impjobgo built
-			cd go/src
-			mkdir imp
-			mkdir imptpgo
-			mkdir impjobgo
-			ln -s $gtm_tst/com/imp.go imp/imp.go
-			set status1 = $status
-			if ($status1) then
-				echo "TEST-E-FAILED : Unable to soft link imp.go to current directory ($PWD)"
-				exit 1
-			endif
-			ln -s $gtm_tst/com/imptpgo.go imptpgo/imptpgo.go
-			if ($status1) then
-				echo "TEST-E-FAILED : Unable to soft link imptpgo.go to current directory ($PWD)"
-				exit 1
-			endif
-			ln -s $gtm_tst/com/impjobgo.go impjobgo/impjobgo.go
-			if ($status1) then
-				echo "TEST-E-FAILED : Unable to soft link impjobgo.go to current directory ($PWD)"
-				exit 1
-			endif
-			cd imptpgo
-			$gobuild
-			if (0 != $status) then
-				echo "TEST-E-FAILED : Unable to build imptpgo.go"
-				exit 1
-			endif
-			cd ../impjobgo
-			$gobuild
-			if (0 != $status) then
-				echo "TEST-E-FAILED : Unable to build impjobgo.go"
-				exit 1
-			endif
-			cd ../../.. # back to main test directory
-			# Now make a link to imptpgo from main test directory
-			ln -s go/src/imptpgo/imptpgo .
-			if ($status1) then
-				echo "TEST-E-FAILED : Unable to soft link imptpgo.go to main test directory ($PWD)"
-				exit 1
-			endif
-		endif
+		source $gtm_tst/com/setupgoenv.csh || \
+			echo "TEST-E-FAILED : [source $gtm_tst/com/setupgoenv.csh] failed with status [$status]" && exit 1
+		echo "# Running : $gobuild -o imptpgo $gtm_tst/com/{imp,imptpgo}.go && $gobuild -o impjobgo $gtm_tst/com/{imp,impjobgo}.go"
+		$gobuild -o imptpgo  $gtm_tst/com/{imp,imptpgo}.go   >! go.out && \
+		$gobuild -o impjobgo $gtm_tst/com/{imp,impjobgo}.go >>& go.out || \
+			echo "TEST-E-FAILED : Unable to build {imp,imptpgo}.go and {imp,impjobgo}.go with status [$status]:" && cat go.out && exit 1
 		set exefile = "imptpgo"
 		breaksw
 	case 5: # Run Rust wrapper using SimpleThreadAPI flavor of imptp
@@ -357,6 +315,15 @@ xyz
 			exit 1
 		endif
 		set exefile = "imptprust"
+		breaksw
+	case 6: # Run Golang wrapper v2 flavor of imptp
+		source $gtm_tst/com/setupgoenv.csh || \
+			echo "TEST-E-FAILED : [source $gtm_tst/com/setupgoenv.csh] failed with status [$status]" && exit 1
+		cp $gtm_tst/com/imptpgo2.go imptpgo2.go
+		echo "# Running : $gobuild imptpgo2.go"
+		$gobuild imptpgo2.go >! go.out || \
+			echo "TEST-E-FAILED : Unable to build imptpgo2.go with status [$status]:" && cat go.out && exit 1
+		set exefile = "imptpgo2"
 		breaksw
 	endsw
 	# Setup call-in table : Need call-ins to do a few tasks that are not easily done through simpleAPI

commit 55c8a2dcd19bd5dba221d7572817cbaa476c59e5

Fix com/setupgoenv.csh to work with multi-host tests (regression in 2ab1b145)
Background
----------
* The `dualfail_ms/dual_fail_multisite` subtest failed during in-house testing with the following diff.

  ```diff
  --- dual_fail_multisite/dual_fail_multisite.diff ---
  49a50
  > TEST-E-MULTISITE replic action failed.Pls. check msr_execute_##FILTERED##NO.csh and all logs related to ##FILTERED## ##TIMESTAMP##
  96a98,100
  > ##REMOTE_HOST##:.../dualfail_ms_1_1/dual_fail_multisite/instance2/imptp_10:17:44.out
  > TEST-E-FAILED : [go get -x -t lang.yottadb.com/go/yottadb lang.yottadb.com/go/yottadb/v2] returned failure status of 1. Total of 5 retries. Output in go_get*.log
  > TEST-E-FAILED : [source /usr/library/gtm_test/T998/com/setupgoenv.csh] failed with status [1]
  ```

* The above was a multi-host test run. Below is the `go_get*.log` output on the remote host.

  ```
  .
  .
  10:17:46 : # get https://sum.golang.org/lookup/lang.yottadb.com/go/yottadb/v2@v2.0.5: 200 OK (0.048s)
  10:17:46 : go: could not create module cache: mkdir /extra: permission denied
  ```

Issue
-----
* The `permission denied` error came from running a `mkdir /extra/...` command. This was done by
  a `go get` or `go build` command but the cause is that `GOPATH` was set to a directory that did
  not exist. The line that sets `GOPATH` is line 26 below.

  **com/setupgoenv.csh**
  ```sh
     23 # set a GOPATH outside current directory to avoid it being subsequently zipped. This prevents permission
     24 # errors when zipping test results since GOPATH contains subdirectories with read-only permissions.
     25 # This particular path makes it common to all subtests run this instance of gtmtest: avoids multiple downloads per gtmtest run.
     26 setenv GOPATH $tst_dir/$gtm_tst_out/gopath
  ```

* Using `$tst_dir` in line 26 can have issues with multi-host test runs as `$tst_dir` need not exist

com/setupgoenv.csh
@@ -23,7 +23,28 @@ setenv PKG_CONFIG_PATH $ydb_dist
 # set a GOPATH outside current directory to avoid it being subsequently zipped. This prevents permission
 # errors when zipping test results since GOPATH contains subdirectories with read-only permissions.
 # This particular path makes it common to all subtests run this instance of gtmtest: avoids multiple downloads per gtmtest run.
-setenv GOPATH $tst_dir/$gtm_tst_out/gopath
+if (-e $tst_dir/$gtm_tst_out) then
+	# The above directory exists. That means this is a single host test run. Use that directory for GOPATH
+	setenv GOPATH $tst_dir/$gtm_tst_out/gopath
+else
+	# The above directory does not exist. Possible in case this is a multi host test run where the current directory
+	# is on a remote host ("$tst_dir" only exists on the originating host, not the current/remote host).
+	# In that case, go up the parent levels until we find the file "primary_dir". That is the top level remote
+	# host directory. Use that for GOPATH.
+	# See https://gitlab.com/YottaDB/DB/YDBTest/-/merge_requests/2480#note_3024710451 for more details.
+	set curdir = $PWD
+	while (1)
+		if (-e primary_dir) then
+			break
+		endif
+		cd ..
+		if ("/" == $PWD) then
+			echo "[SETUPGOENV-E-GOPATH : $curdir does not have primary_dir in parent levels" && exit 1
+		endif
+	end
+	setenv GOPATH $PWD/gopath
+	cd $curdir
+endif
 # Ensure go toolchains are not downloaded by the test as this creates huge artifacts
 # in GOPATH (up to gigabytes) every time imptp.csh is run.
 # Ensure the machine has a Go version at least as recent as required in go.mod and by YDBGo's go.mod files.