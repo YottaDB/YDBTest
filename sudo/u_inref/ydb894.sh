34625713073fb21b0d68500039c9c78aa42d9691
Jon Badiali <jon@yottadb.com>
2026-04-22 16:46:29 -0400

    Enhanced sudo/ydb894 to include additional output in case of failure to aid in diagnosing rare pipeline failures
    This was done to facilitate analysis of rare, difficult to reproduce pipeline failures such as https://gitlab.com/nars1/YDB/-/jobs/14025152357.

--- sudo/u_inref/ydb894.sh
+++ sudo/u_inref/ydb894.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 #################################################################
-# Copyright (c) 2022-2023 YottaDB LLC and/or its subsidiaries.	#
+# Copyright (c) 2022-2026 YottaDB LLC and/or its subsidiaries.	#
 # All rights reserved.						#
-#								#
 #								#
 #	This source code contains the intellectual property	#
 #	of its copyright holder(s), and is made available	#
@@ -16,31 +15,32 @@ EXISTS=false
 # If yottadb.pc already exists change value of EXISTS flag and
 # get its current timestamp
 if test -f "$FILE"; then
-    EXISTS=true
-    OLD_TIMESTAMP=`stat -c %y $FILE`
+	EXISTS=true
+	OLD_TIMESTAMP=`stat -c %y $FILE`
 fi
 
 /Distrib/YottaDB/$1/$2/yottadb_r*/ydbinstall --installdir $3 --nopkg-config --overwrite-existing --utf8 --user $USER $4
 
 status=$?
 if [ 0 != $status ]; then
-    echo "ydbinstall returned a non-zero status: $status"
-    exit $status
+	echo "ydbinstall returned a non-zero status: $status"
+	exit $status
 else
-    # Test errors out if timestamp of yottadb.pc has changed, indicating
-    # that the file has been updated with the --nopkg-config option
-    if $EXISTS; then
-	NEW_TIMESTAMP=`stat -c %y $FILE`
-	if [ "$OLD_TIMESTAMP" != "$NEW_TIMESTAMP" ]; then
-	    echo "Error: $FILE has been updated with --nopkg-config option"
+	# Test errors out if timestamp of yottadb.pc has changed, indicating
+	# that the file has been updated with the --nopkg-config option
+	if $EXISTS; then
+		NEW_TIMESTAMP=`stat -c %y $FILE`
+		if [ "$OLD_TIMESTAMP" != "$NEW_TIMESTAMP" ]; then
+			echo "Error: $FILE has been updated with --nopkg-config option ($OLD_TIMESTAMP != $NEW_TIMESTAMP)"
+			stat /usr/share/pkgconfig/yottadb.pc
+		fi
+	# Test errors out if yottadb.pc gets created with the --nopkg-config
+	# option
+	else
+		if [ -f "$FILE" ]; then
+			echo "Error: $FILE has been created with the --nopkg-config option"
+		fi
 	fi
-    # Test errors out if yottadb.pc gets created with the --nopkg-config
-    # option
-    else
-	if [ -f "$FILE" ]; then
-	    echo "Error: $FILE has been created with the --nopkg-config option"
-	fi
-    fi
 fi
 
 exit 0
